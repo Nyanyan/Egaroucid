b2f1143919ca36a4002cca01cf49a40d0ff635d8
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2021-12-22 07:36:08 +0900

    update book

--- book/book.py
+++ book/book.py
@@ -3,7 +3,10 @@ from copy import deepcopy
 from collections import deque
 from othello_py import *
 
-ai_exe = subprocess.Popen('./../src/egaroucid5.out'.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+
+n_parallel = 14
+
+ai_exe = [subprocess.Popen('./../src/egaroucid5.out'.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL) for _ in range(n_parallel)]
 
 book = {}
 
@@ -19,26 +22,35 @@ move_threshold = 0
 
 que = deque([])
 
-def calc_value(o):
+def start_calc_value(o, ai_num):
+    grid_str = ''
+    for i in range(hw):
+        for j in range(hw):
+            grid_str += '0' if o.grid[i][j] == 0 else '1' if o.grid[i][j] == 1 else '.'
+        grid_str += '\n'
+    if grid_str.replace('\n', '') in book:
+        return False
+    ai_exe[ai_num].stdin.write((str(o.player) + '\n' + grid_str).encode('utf-8'))
+    ai_exe[ai_num].stdin.flush()
+    return True
+
+def calc_value(o, ai_num, flag):
     o.print_info()
     grid_str = ''
     for i in range(hw):
         for j in range(hw):
             grid_str += '0' if o.grid[i][j] == 0 else '1' if o.grid[i][j] == 1 else '.'
         grid_str += '\n'
-    #print(grid_str)
-    if grid_str.replace('\n', '') in book:
-        val = book[grid_str.replace('\n', '')]
-    else:
-        ai_exe.stdin.write((str(o.player) + '\n' + grid_str).encode('utf-8'))
-        ai_exe.stdin.flush()
-        _, _, val = [float(elem) for elem in ai_exe.stdout.readline().decode().split()]
+    if flag:
+        _, _, val = [float(elem) for elem in ai_exe[ai_num].stdout.readline().decode().split()]
         if o.player == white:
             val = -val
         print(val)
         book[grid_str.replace('\n', '')] = val
         with open('learned_data/book.txt', 'a') as f:
             f.write(grid_str.replace('\n', '') + ' ' + str(val) + '\n')
+    else:
+        val = book[grid_str.replace('\n', '')]
     if sum(o.n_stones) > move_threshold + 4 and abs(val) > abs(val_threshold):
         print('out of range')
         return
@@ -59,4 +71,13 @@ o.move(4, 5)
 o.check_legal()
 que.append(o)
 while que:
-    calc_value(que.popleft())
+    lst = []
+    num = min(n_parallel, len(que))
+    for i in range(num):
+        tmp = que.popleft()
+        lst.append([tmp, start_calc_value(tmp, i)])
+    for i in range(num):
+        calc_value(lst[i][0], i, lst[i][1])
+
+for i in range(n_parallel):
+    ai_exe[i].kill()
