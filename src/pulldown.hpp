827f13cbe5c7edc6376671d7a34e5e6c5f46d835
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2021-12-22 14:30:00 +0900

    pulldown

---
+++ src/pulldown.hpp
@@ -0,0 +1,143 @@
+﻿#pragma once
+# include <Siv3D.hpp>
+
+class Pulldown
+{
+public:
+
+	Pulldown() = default;
+
+	Pulldown(const Array<String>& items, const Font& font, const Point& pos = { 0,0 }, int default_idx = 0)
+		: m_font{ font }
+		, m_items{ items }
+		, m_rect{ pos, 0, (m_font.height() + m_padding.y * 2) }
+	{
+		for (const auto& item : m_items)
+		{
+			m_rect.w = Max(m_rect.w, static_cast<int32>(m_font(item).region().w));
+		}
+
+		m_rect.w += (m_padding.x * 2 + m_downButtonSize);
+		m_index = default_idx;
+	}
+
+	bool isEmpty() const
+	{
+		return m_items.empty();
+	}
+
+	void update()
+	{
+		if (isEmpty())
+		{
+			return;
+		}
+
+		if (m_rect.leftClicked())
+		{
+			m_isOpen = (not m_isOpen);
+		}
+
+		Point pos = m_rect.pos.movedBy(0, m_rect.h);
+
+		if (m_isOpen)
+		{
+			for (int i = 0; i < (int)m_items.size(); ++i)
+			{
+				if (const Rect rect{ pos, m_rect.w, m_rect.h };
+					rect.leftClicked())
+				{
+					m_index = i;
+					m_isOpen = false;
+					break;
+				}
+
+				pos.y += m_rect.h;
+			}
+		}
+	}
+
+	void draw() const
+	{
+		m_rect.draw();
+
+		if (isEmpty())
+		{
+			return;
+		}
+
+		m_rect.drawFrame(1, 0, m_isOpen ? Palette::Orange : Palette::Gray);
+
+		Point pos = m_rect.pos;
+
+		m_font(m_items[m_index]).draw(pos + m_padding, Palette::Black);
+
+		Triangle{ (m_rect.x + m_rect.w - m_downButtonSize / 2.0 - m_padding.x), (m_rect.y + m_rect.h / 2.0),
+			(m_downButtonSize * 0.5), 180_deg }.draw(Palette::Black);
+
+		pos.y += m_rect.h;
+
+		if (m_isOpen)
+		{
+			const Rect backRect{ pos, m_rect.w, (m_rect.h * m_items.size()) };
+
+			backRect.drawShadow({ 1, 1 }, 4, 1).draw();
+
+			for (const auto& item : m_items)
+			{
+				if (const Rect rect{ pos, m_rect.size };
+					rect.mouseOver())
+				{
+					rect.draw(Palette::Skyblue);
+				}
+
+				m_font(item).draw((pos + m_padding), Palette::Black);
+
+				pos.y += m_rect.h;
+			}
+
+			backRect.drawFrame(1, 0, Palette::Gray);
+		}
+	}
+
+	void setPos(const Point& pos)
+	{
+		m_rect.setPos(pos);
+	}
+
+	const Rect& getRect() const
+	{
+		return m_rect;
+	}
+
+	size_t getIndex() const
+	{
+		return m_index;
+	}
+
+	String getItem() const
+	{
+		if (isEmpty())
+		{
+			return{};
+		}
+
+		return m_items[m_index];
+	}
+
+private:
+
+	Font m_font;
+
+	Array<String> m_items;
+
+	size_t m_index = 0;
+
+	Size m_padding{ 6, 2 };
+
+	Rect m_rect;
+
+	int32 m_downButtonSize = 16;
+
+	bool m_isOpen = false;
+};
