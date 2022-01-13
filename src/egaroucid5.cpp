e735bf2ec390ee7da09d0bca8efa6c54c1d2a38e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-01-13 19:29:40 +0900

    update

--- src/egaroucid5.cpp
+++ src/egaroucid5.cpp
@@ -80,8 +80,8 @@ int main(){
         int depth, end_depth;
         //bool pre_searched = false;
         book_value book_result;
-        depth = 1;
-        end_depth = 2;
+        depth = 10;
+        end_depth = 20;
     #endif
     int ai_player;
     //cin >> ai_player;
