7f0bee191425adfb9c0fe9ec6443f6e9e9102c05
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-01-15 21:25:27 +0900

    update evaluation function

--- evaluation/adjust_param.cpp
+++ evaluation/adjust_param.cpp
@@ -49,9 +49,9 @@ int sa_phase, sa_player;
 
 #define n_raw_params 86
 
-#define beta 0.0001
+#define beta 0.00001
 unsigned long long hour = 0;
-unsigned long long minute = 10;
+unsigned long long minute = 2;
 unsigned long long second = 0;
 
 double alpha[n_eval][max_evaluate_idx];
