426d3c8451fd0c1a76f057edf2ef01f3d1aa5a95
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-01-15 16:47:13 +0900

    update

--- evaluation/adjust_param.cpp
+++ evaluation/adjust_param.cpp
@@ -42,9 +42,9 @@ int sa_phase, sa_player;
 
 #define n_raw_params 102
 
-#define beta 0.0001
+#define beta 0.00001
 unsigned long long hour = 0;
-unsigned long long minute = 10;
+unsigned long long minute = 5;
 unsigned long long second = 0;
 
 double alpha[n_eval][max_evaluate_idx];
@@ -581,7 +581,7 @@ int main(int argc, char *argv[]){
     init();
     initialize_param();
     //output_param_onephase();
-    //input_param_onephase((string)(argv[3]));
+    input_param_onephase((string)(argv[3]));
     input_test_data(0);
 
     sd(second * 1000);
