a9cd05397e0fbdc01ffc219ba1f284342c5ada55
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-01-21 11:04:02 +0900

    import book

--- src/Main.cpp
+++ src/Main.cpp
@@ -208,7 +208,7 @@ inline String coord_translate(int coord) {
 
 inline void import_book(string file) {
 	cerr << "book import" << endl;
-	bool result = book.import_file(file.c_str());
+	bool result = book.import_file_bin(file);
 	if (result)
 		cerr << "book imported" << endl;
 	else
