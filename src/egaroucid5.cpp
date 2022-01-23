7cf375a77fac5b3720e16a286509f78e2ab48911
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-01-24 03:18:05 +0900

    updating evaluation function

--- src/egaroucid5.cpp
+++ src/egaroucid5.cpp
@@ -84,8 +84,8 @@ int main(){
         int depth, end_depth;
         //bool pre_searched = false;
         book_value book_result;
-        depth = 17;
-        end_depth = 40;
+        depth = 1;
+        end_depth = 2;
     #endif
     int ai_player;
     //cin >> ai_player;
