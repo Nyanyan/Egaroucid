05683185f743f5732823d013668deecc6ac7e474
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-01-30 21:14:04 +0900

    update evaluation speed

--- src/evaluate.hpp
+++ src/evaluate.hpp
@@ -308,6 +308,10 @@ inline void calc_stability(board *b, int *stab0, int *stab1){
     *stab1 = pop_count_ull(white_stability);
 }
 
+inline int pop_digit(unsigned long long x, int place){
+    return 1 & (x >> place);
+}
+
 inline int pick_pattern(const int phase_idx, const int p, const int pattern_idx, const int b_arr[], const int p0, const int p1, const int p2, const int p3, const int p4){
     return pattern_arr[phase_idx][p][pattern_idx][b_arr[p0] * p34 + b_arr[p1] * p33 + b_arr[p2] * p32 + b_arr[p3] * p31 + b_arr[p4]];
 }
@@ -352,17 +356,73 @@ inline int calc_pattern(const int phase_idx, board *b, const int b_arr[]){
         pick_pattern(phase_idx, b->p, 15, b_arr, 0, 1, 8, 9, 10, 11, 12, 17, 25, 33) + pick_pattern(phase_idx, b->p, 15, b_arr, 7, 6, 15, 14, 13, 12, 11, 22, 30, 38) + pick_pattern(phase_idx, b->p, 15, b_arr, 56, 57, 48, 49, 50, 51, 52, 41, 33, 25) + pick_pattern(phase_idx, b->p, 15, b_arr, 63, 62, 55, 54, 53, 52, 51, 46, 38, 30);
 }
 
+inline int pick_pattern(const int phase_idx, const int p, const int pattern_idx, const unsigned long long bk, const unsigned long long wt, const int p0, const int p1, const int p2, const int p3, const int p4){
+    return pattern_arr[phase_idx][p][pattern_idx][(pop_digit(wt, p0) * 2 + pop_digit(bk, p0)) * p34 +(pop_digit(wt, p1) * 2 + pop_digit(bk, p1)) * p33 + (pop_digit(wt, p2) * 2 + pop_digit(bk, p2)) * p32 + (pop_digit(wt, p3) * 2 + pop_digit(bk, p3)) * p31 + (pop_digit(wt, p4) * 2 + pop_digit(bk, p4))];
+}
+
+inline int pick_pattern(const int phase_idx, const int p, const int pattern_idx, const unsigned long long bk, const unsigned long long wt, const int p0, const int p1, const int p2, const int p3, const int p4, const int p5){
+    return pattern_arr[phase_idx][p][pattern_idx][(pop_digit(wt, p0) * 2 + pop_digit(bk, p0)) * p35 + (pop_digit(wt, p1) * 2 + pop_digit(bk, p1)) * p34 + (pop_digit(wt, p2) * 2 + pop_digit(bk, p2)) * p33 + (pop_digit(wt, p3) * 2 + pop_digit(bk, p3)) * p32 + (pop_digit(wt, p4) * 2 + pop_digit(bk, p4)) * p31 + (pop_digit(wt, p5) * 2 + pop_digit(bk, p5))];
+}
+
+inline int pick_pattern(const int phase_idx, const int p, const int pattern_idx, const unsigned long long bk, const unsigned long long wt, const int p0, const int p1, const int p2, const int p3, const int p4, const int p5, const int p6){
+    return pattern_arr[phase_idx][p][pattern_idx][(pop_digit(wt, p0) * 2 + pop_digit(bk, p0)) * p36 + (pop_digit(wt, p1) * 2 + pop_digit(bk, p1)) * p35 + (pop_digit(wt, p2) * 2 + pop_digit(bk, p2)) * p34 + (pop_digit(wt, p3) * 2 + pop_digit(bk, p3)) * p33 + (pop_digit(wt, p4) * 2 + pop_digit(bk, p4)) * p32 + (pop_digit(wt, p5) * 2 + pop_digit(bk, p5)) * p31 + (pop_digit(wt, p6) * 2 + pop_digit(bk, p6))];
+}
+
+inline int pick_pattern(const int phase_idx, const int p, const int pattern_idx, const unsigned long long bk, const unsigned long long wt, const int p0, const int p1, const int p2, const int p3, const int p4, const int p5, const int p6, const int p7){
+    return pattern_arr[phase_idx][p][pattern_idx][(pop_digit(wt, p0) * 2 + pop_digit(bk, p0)) * p37 + (pop_digit(wt, p1) * 2 + pop_digit(bk, p1)) * p36 + (pop_digit(wt, p2) * 2 + pop_digit(bk, p2)) * p35 + (pop_digit(wt, p3) * 2 + pop_digit(bk, p3)) * p34 + (pop_digit(wt, p4) * 2 + pop_digit(bk, p4)) * p33 + (pop_digit(wt, p5) * 2 + pop_digit(bk, p5)) * p32 + (pop_digit(wt, p6) * 2 + pop_digit(bk, p6)) * p31 + (pop_digit(wt, p7) * 2 + pop_digit(bk, p7))];
+}
+
+inline int pick_pattern(const int phase_idx, const int p, const int pattern_idx, const unsigned long long bk, const unsigned long long wt, const int p0, const int p1, const int p2, const int p3, const int p4, const int p5, const int p6, const int p7, const int p8){
+    return pattern_arr[phase_idx][p][pattern_idx][(pop_digit(wt, p0) * 2 + pop_digit(bk, p0)) * p38 + (pop_digit(wt, p1) * 2 + pop_digit(bk, p1)) * p37 + (pop_digit(wt, p2) * 2 + pop_digit(bk, p2)) * p36 + (pop_digit(wt, p3) * 2 + pop_digit(bk, p3)) * p35 + (pop_digit(wt, p4) * 2 + pop_digit(bk, p4)) * p34 + (pop_digit(wt, p5) * 2 + pop_digit(bk, p5)) * p33 + (pop_digit(wt, p6) * 2 + pop_digit(bk, p6)) * p32 + (pop_digit(wt, p7) * 2 + pop_digit(bk, p7)) * p31 + (pop_digit(wt, p8) * 2 + pop_digit(bk, p8))];
+}
+
+inline int pick_pattern(const int phase_idx, const int p, const int pattern_idx, const unsigned long long bk, const unsigned long long wt, const int p0, const int p1, const int p2, const int p3, const int p4, const int p5, const int p6, const int p7, const int p8, const int p9){
+    return pattern_arr[phase_idx][p][pattern_idx][(pop_digit(wt, p0) * 2 + pop_digit(bk, p0)) * p39 + (pop_digit(wt, p1) * 2 + pop_digit(bk, p1)) * p38 + (pop_digit(wt, p2) * 2 + pop_digit(bk, p2)) * p37 + (pop_digit(wt, p3) * 2 + pop_digit(bk, p3)) * p36 + (pop_digit(wt, p4) * 2 + pop_digit(bk, p4)) * p35 + (pop_digit(wt, p5) * 2 + pop_digit(bk, p5)) * p34 + (pop_digit(wt, p6) * 2 + pop_digit(bk, p6)) * p33 + (pop_digit(wt, p7) * 2 + pop_digit(bk, p7)) * p32 + (pop_digit(wt, p8) * 2 + pop_digit(bk, p8)) * p31 + (pop_digit(wt, p9) * 2 + pop_digit(bk, p9))];
+}
+
+inline int calc_pattern(const int phase_idx, board *b, const unsigned long long bk, const unsigned long long wt){
+    return 
+        pick_pattern(phase_idx, b->p, 0, bk, wt, 8, 9, 10, 11, 12, 13, 14, 15) + pick_pattern(phase_idx, b->p, 0, bk, wt, 1, 9, 17, 25, 33, 41, 49, 57) + pick_pattern(phase_idx, b->p, 0, bk, wt, 48, 49, 50, 51, 52, 53, 54, 55) + pick_pattern(phase_idx, b->p, 0, bk, wt, 6, 14, 22, 30, 38, 46, 54, 62) + 
+        pick_pattern(phase_idx, b->p, 1, bk, wt, 16, 17, 18, 19, 20, 21, 22, 23) + pick_pattern(phase_idx, b->p, 1, bk, wt, 2, 10, 18, 26, 34, 42, 50, 58) + pick_pattern(phase_idx, b->p, 1, bk, wt, 40, 41, 42, 43, 44, 45, 46, 47) + pick_pattern(phase_idx, b->p, 1, bk, wt, 5, 13, 21, 29, 37, 45, 53, 61) + 
+        pick_pattern(phase_idx, b->p, 2, bk, wt, 24, 25, 26, 27, 28, 29, 30, 31) + pick_pattern(phase_idx, b->p, 2, bk, wt, 3, 11, 19, 27, 35, 43, 51, 59) + pick_pattern(phase_idx, b->p, 2, bk, wt, 32, 33, 34, 35, 36, 37, 38, 39) + pick_pattern(phase_idx, b->p, 2, bk, wt, 4, 12, 20, 28, 36, 44, 52, 60) + 
+        pick_pattern(phase_idx, b->p, 3, bk, wt, 3, 12, 21, 30, 39) + pick_pattern(phase_idx, b->p, 3, bk, wt, 4, 11, 18, 25, 32) + pick_pattern(phase_idx, b->p, 3, bk, wt, 24, 33, 42, 51, 60) + pick_pattern(phase_idx, b->p, 3, bk, wt, 59, 52, 45, 38, 31) + 
+        pick_pattern(phase_idx, b->p, 4, bk, wt, 2, 11, 20, 29, 38, 47) + pick_pattern(phase_idx, b->p, 4, bk, wt, 5, 12, 19, 26, 33, 40) + pick_pattern(phase_idx, b->p, 4, bk, wt, 16, 25, 34, 43, 52, 61) + pick_pattern(phase_idx, b->p, 4, bk, wt, 58, 51, 44, 37, 30, 23) + 
+        pick_pattern(phase_idx, b->p, 5, bk, wt, 1, 10, 19, 28, 37, 46, 55) + pick_pattern(phase_idx, b->p, 5, bk, wt, 6, 13, 20, 27, 34, 41, 48) + pick_pattern(phase_idx, b->p, 5, bk, wt, 8, 17, 26, 35, 44, 53, 62) + pick_pattern(phase_idx, b->p, 5, bk, wt, 57, 50, 43, 36, 29, 22, 15) + 
+        pick_pattern(phase_idx, b->p, 6, bk, wt, 0, 9, 18, 27, 36, 45, 54, 63) + pick_pattern(phase_idx, b->p, 6, bk, wt, 7, 14, 21, 28, 35, 42, 49, 56) + 
+        pick_pattern(phase_idx, b->p, 7, bk, wt, 9, 0, 1, 2, 3, 4, 5, 6, 7, 14) + pick_pattern(phase_idx, b->p, 7, bk, wt, 9, 0, 8, 16, 24, 32, 40, 48, 56, 49) + pick_pattern(phase_idx, b->p, 7, bk, wt, 49, 56, 57, 58, 59, 60, 61, 62, 63, 54) + pick_pattern(phase_idx, b->p, 7, bk, wt, 54, 63, 55, 47, 39, 31, 23, 15, 7) + 
+        pick_pattern(phase_idx, b->p, 8, bk, wt, 0, 1, 2, 3, 8, 9, 10, 16, 17, 24) + pick_pattern(phase_idx, b->p, 8, bk, wt, 7, 6, 5, 4, 15, 14, 13, 23, 22, 31) + pick_pattern(phase_idx, b->p, 8, bk, wt, 63, 62, 61, 60, 55, 54, 53, 47, 46, 39) + pick_pattern(phase_idx, b->p, 8, bk, wt, 56, 57, 58, 59, 48, 49, 50, 40, 41, 32) + 
+        pick_pattern(phase_idx, b->p, 9, bk, wt, 0, 2, 3, 4, 5, 7, 10, 11, 12, 13) + pick_pattern(phase_idx, b->p, 9, bk, wt, 0, 16, 24, 32, 40, 56, 17, 25, 33, 41) + pick_pattern(phase_idx, b->p, 9, bk, wt, 56, 58, 59, 60, 61, 63, 50, 51, 52, 53) + pick_pattern(phase_idx, b->p, 9, bk, wt, 7, 23, 31, 39, 47, 63, 22, 30, 38, 46) + 
+        pick_pattern(phase_idx, b->p, 10, bk, wt, 0, 9, 18, 27, 1, 10, 19, 8, 17, 26) + pick_pattern(phase_idx, b->p, 10, bk, wt, 7, 14, 21, 28, 6, 13, 20, 15, 22, 29) + pick_pattern(phase_idx, b->p, 10, bk, wt, 56, 49, 42, 35, 57, 50, 43, 48, 41, 34) + pick_pattern(phase_idx, b->p, 10, bk, wt, 63, 54, 45, 36, 62, 53, 44, 55, 46, 37) + 
+        pick_pattern(phase_idx, b->p, 11, bk, wt, 0, 1, 2, 8, 9, 10, 16, 17, 18) + pick_pattern(phase_idx, b->p, 11, bk, wt, 7, 6, 5, 15, 14, 13, 23, 22, 21) + pick_pattern(phase_idx, b->p, 11, bk, wt, 56, 57, 58, 48, 49, 50, 40, 41, 42) + pick_pattern(phase_idx, b->p, 11, bk, wt, 63, 62, 61, 55, 54, 53, 47, 46, 45) + 
+        pick_pattern(phase_idx, b->p, 12, bk, wt, 10, 0, 1, 2, 3, 4, 5, 6, 7, 13) + pick_pattern(phase_idx, b->p, 12, bk, wt, 17, 0, 8, 16, 24, 32, 40, 48, 56, 41) + pick_pattern(phase_idx, b->p, 12, bk, wt, 50, 56, 57, 58, 59, 60, 61, 62, 63, 53) + pick_pattern(phase_idx, b->p, 12, bk, wt, 46, 63, 55, 47, 39, 31, 23, 15, 7, 22) + 
+        pick_pattern(phase_idx, b->p, 13, bk, wt, 0, 1, 2, 3, 4, 8, 9, 16, 24, 32) + pick_pattern(phase_idx, b->p, 13, bk, wt, 7, 6, 5, 4, 3, 15, 14, 23, 31, 39) + pick_pattern(phase_idx, b->p, 13, bk, wt, 63, 62, 61, 60, 59, 55, 54, 47, 39, 31) + pick_pattern(phase_idx, b->p, 13, bk, wt, 56, 57, 58, 59, 60, 48, 49, 40, 32, 24) + 
+        pick_pattern(phase_idx, b->p, 14, bk, wt, 0, 1, 8, 9, 10, 11, 17, 18, 25, 27) + pick_pattern(phase_idx, b->p, 14, bk, wt, 7, 6, 15, 14, 13, 12, 22, 21, 30, 28) + pick_pattern(phase_idx, b->p, 14, bk, wt, 56, 57, 48, 49, 50, 51, 41, 42, 33, 35) + pick_pattern(phase_idx, b->p, 14, bk, wt, 63, 62, 55, 54, 53, 52, 46, 45, 38, 36) + 
+        pick_pattern(phase_idx, b->p, 15, bk, wt, 0, 1, 8, 9, 10, 11, 12, 17, 25, 33) + pick_pattern(phase_idx, b->p, 15, bk, wt, 7, 6, 15, 14, 13, 12, 11, 22, 30, 38) + pick_pattern(phase_idx, b->p, 15, bk, wt, 56, 57, 48, 49, 50, 51, 52, 41, 33, 25) + pick_pattern(phase_idx, b->p, 15, bk, wt, 63, 62, 55, 54, 53, 52, 51, 46, 38, 30);
+}
+
 inline int create_canput_line(const int canput_arr[], const int a, const int b, const int c, const int d, const int e, const int f, const int g, const int h){
     return 
         canput_arr[a] * p47 + canput_arr[b] * p46 + canput_arr[c] * p45 + canput_arr[d] * p44 + 
         canput_arr[e] * p43 + canput_arr[f] * p42 + canput_arr[g] * p41 + canput_arr[h];
 }
 
+inline int create_canput_line(unsigned long long bk, unsigned long long wt, const int a, const int b, const int c, const int d, const int e, const int f, const int g, const int h){
+    return 
+        (pop_digit(wt, a) * 2 + pop_digit(bk, a)) * p47 + 
+        (pop_digit(wt, b) * 2 + pop_digit(bk, b)) * p46 + 
+        (pop_digit(wt, c) * 2 + pop_digit(bk, c)) * p45 + 
+        (pop_digit(wt, d) * 2 + pop_digit(bk, d)) * p44 + 
+        (pop_digit(wt, e) * 2 + pop_digit(bk, e)) * p43 + 
+        (pop_digit(wt, f) * 2 + pop_digit(bk, f)) * p42 + 
+        (pop_digit(wt, g) * 2 + pop_digit(bk, g)) * p41 + 
+        (pop_digit(wt, h) * 2 + pop_digit(bk, h));
+}
+
 inline int calc_canput_pattern(const int phase_idx, board *b, const unsigned long long black_mobility, const unsigned long long white_mobility){
-    int res;
+    /*
     int canput_arr[hw2];
     b->board_canput(canput_arr, black_mobility, white_mobility);
-    res = 
+    return
         eval_canput_pattern[phase_idx][b->p][0][create_canput_line(canput_arr, 0, 1, 2, 3, 4, 5, 6, 7)] + 
         eval_canput_pattern[phase_idx][b->p][0][create_canput_line(canput_arr, 0, 8, 16, 24, 32, 40, 48, 56)] + 
         eval_canput_pattern[phase_idx][b->p][0][create_canput_line(canput_arr, 7, 15, 23, 31, 39, 47, 55, 63)] + 
@@ -379,7 +439,24 @@ inline int calc_canput_pattern(const int phase_idx, board *b, const unsigned lon
         eval_canput_pattern[phase_idx][b->p][3][create_canput_line(canput_arr, 3, 11, 19, 27, 35, 43, 51, 59)] + 
         eval_canput_pattern[phase_idx][b->p][3][create_canput_line(canput_arr, 4, 12, 20, 28, 36, 44, 52, 60)] + 
         eval_canput_pattern[phase_idx][b->p][3][create_canput_line(canput_arr, 32, 33, 34, 35, 36, 37, 38, 39)];
-    return res;
+    */
+    return
+        eval_canput_pattern[phase_idx][b->p][0][create_canput_line(black_mobility, white_mobility, 0, 1, 2, 3, 4, 5, 6, 7)] + 
+        eval_canput_pattern[phase_idx][b->p][0][create_canput_line(black_mobility, white_mobility, 0, 8, 16, 24, 32, 40, 48, 56)] + 
+        eval_canput_pattern[phase_idx][b->p][0][create_canput_line(black_mobility, white_mobility, 7, 15, 23, 31, 39, 47, 55, 63)] + 
+        eval_canput_pattern[phase_idx][b->p][0][create_canput_line(black_mobility, white_mobility, 56, 57, 58, 59, 60, 61, 62, 63)] + 
+        eval_canput_pattern[phase_idx][b->p][1][create_canput_line(black_mobility, white_mobility, 8, 9, 10, 11, 12, 13, 14, 15)] + 
+        eval_canput_pattern[phase_idx][b->p][1][create_canput_line(black_mobility, white_mobility, 1, 9, 17, 25, 33, 41, 49, 57)] + 
+        eval_canput_pattern[phase_idx][b->p][1][create_canput_line(black_mobility, white_mobility, 6, 14, 22, 30, 38, 46, 54, 62)] + 
+        eval_canput_pattern[phase_idx][b->p][1][create_canput_line(black_mobility, white_mobility, 48, 49, 50, 51, 52, 53, 54, 55)] + 
+        eval_canput_pattern[phase_idx][b->p][2][create_canput_line(black_mobility, white_mobility, 16, 17, 18, 19, 20, 21, 22, 23)] + 
+        eval_canput_pattern[phase_idx][b->p][2][create_canput_line(black_mobility, white_mobility, 2, 10, 18, 26, 34, 42, 50, 58)] + 
+        eval_canput_pattern[phase_idx][b->p][2][create_canput_line(black_mobility, white_mobility, 5, 13, 21, 29, 37, 45, 53, 61)] + 
+        eval_canput_pattern[phase_idx][b->p][2][create_canput_line(black_mobility, white_mobility, 40, 41, 42, 43, 44, 45, 46, 47)] + 
+        eval_canput_pattern[phase_idx][b->p][3][create_canput_line(black_mobility, white_mobility, 24, 25, 26, 27, 28, 29, 30, 31)] + 
+        eval_canput_pattern[phase_idx][b->p][3][create_canput_line(black_mobility, white_mobility, 3, 11, 19, 27, 35, 43, 51, 59)] + 
+        eval_canput_pattern[phase_idx][b->p][3][create_canput_line(black_mobility, white_mobility, 4, 12, 20, 28, 36, 44, 52, 60)] + 
+        eval_canput_pattern[phase_idx][b->p][3][create_canput_line(black_mobility, white_mobility, 32, 33, 34, 35, 36, 37, 38, 39)];
 }
 
 inline int end_evaluate(board *b){
@@ -389,8 +466,8 @@ inline int end_evaluate(board *b){
 inline int mid_evaluate(board *b){
     int phase_idx, sur0, sur1, canput0, canput1, stab0, stab1, num0, num1;
     unsigned long long black_mobility, white_mobility, empties;
-    int b_arr[hw2];
-    b->translate_to_arr(b_arr);
+    //int b_arr[hw2];
+    //b->translate_to_arr(b_arr);
     black_mobility = get_mobility(b->b, b->w);
     white_mobility = get_mobility(b->w, b->b);
     empties = ~(b->b | b->w);
@@ -406,7 +483,7 @@ inline int mid_evaluate(board *b){
     num1 = pop_count_ull(b->w);
     //cerr << sur0 << " " << sur1 << " " << canput0 << " " << canput1 << " " << stab0 << " " << stab1 << " " << num0 << " " << num1 << endl;
     int res = (b->p ? -1 : 1) * (
-        calc_pattern(phase_idx, b, b_arr) + 
+        calc_pattern(phase_idx, b, b->b, b->w) + 
         eval_sur0_sur1_arr[phase_idx][b->p][sur0][sur1] + 
         eval_canput0_canput1_arr[phase_idx][b->p][canput0][canput1] + 
         eval_stab0_stab1_arr[phase_idx][b->p][stab0][stab1] + 
