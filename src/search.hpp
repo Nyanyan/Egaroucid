1fc1edf0bc3ee7e15c38eaebf34609327ea0bd65
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-02-09 16:36:49 +0900

    udpate evaluation function

--- src/search.hpp
+++ src/search.hpp
@@ -33,9 +33,9 @@ using namespace std;
 #define mpc_max_depth_final 40
 
 #define simple_mid_threshold 2
-#define simple_end_threshold 7
+#define simple_end_threshold 6
+#define simple_end_threshold2 11
 #define simple_end_mpc_threshold 5
-#define simple_end_threshold2 13
 #define end_mpc_depth_threshold 5
 
 #define po_max_depth 15
