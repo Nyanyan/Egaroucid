4404ef9a2efd78c81429440a1b8bd78411cef8e4
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-02-14 00:38:44 +0900

    creating move ordering model

---
+++ move_ordering/calc_idx.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include "common.hpp"
+
+#define p40 1
+#define p41 4
+#define p42 16
+#define p43 64
+#define p44 256
+#define p45 1024
+#define p46 4096
+#define p47 16384
+#define p48 65536
+
+using namespace std;
+
+inline int pop_digit(unsigned long long x, int place){
+    return 1 & (x >> place);
+}
+
+inline int pick_pattern(unsigned long long p, unsigned long long o, unsigned long long v, unsigned long long f, const int p0, const int p1, const int p2){
+    return 
+        ((pop_digit(f, p0) * 3 + pop_digit(o, p0) * 2 + pop_digit(p, p0)) * p40) + 
+        ((pop_digit(f, p1) * 3 + pop_digit(o, p1) * 2 + pop_digit(p, p1)) * p41) + 
+        ((pop_digit(f, p2) * 3 + pop_digit(o, p2) * 2 + pop_digit(p, p2)) * p42);
+}
+
+inline int pick_pattern(unsigned long long p, unsigned long long o, unsigned long long v, unsigned long long f, const int p0, const int p1, const int p2, const int p3){
+    return 
+        ((pop_digit(f, p0) * 3 + pop_digit(o, p0) * 2 + pop_digit(p, p0)) * p40) + 
+        ((pop_digit(f, p1) * 3 + pop_digit(o, p1) * 2 + pop_digit(p, p1)) * p41) + 
+        ((pop_digit(f, p2) * 3 + pop_digit(o, p2) * 2 + pop_digit(p, p2)) * p42) + 
+        ((pop_digit(f, p3) * 3 + pop_digit(o, p3) * 2 + pop_digit(p, p3)) * p43);
+}
+
+inline int pick_pattern(unsigned long long p, unsigned long long o, unsigned long long v, unsigned long long f, const int p0, const int p1, const int p2, const int p3, const int p4){
+    return 
+        ((pop_digit(f, p0) * 3 + pop_digit(o, p0) * 2 + pop_digit(p, p0)) * p40) + 
+        ((pop_digit(f, p1) * 3 + pop_digit(o, p1) * 2 + pop_digit(p, p1)) * p41) + 
+        ((pop_digit(f, p2) * 3 + pop_digit(o, p2) * 2 + pop_digit(p, p2)) * p42) + 
+        ((pop_digit(f, p3) * 3 + pop_digit(o, p3) * 2 + pop_digit(p, p3)) * p43) + 
+        ((pop_digit(f, p4) * 3 + pop_digit(o, p4) * 2 + pop_digit(p, p4)) * p44);
+}
+
+
+inline int pick_pattern(unsigned long long p, unsigned long long o, unsigned long long v, unsigned long long f, const int p0, const int p1, const int p2, const int p3, const int p4, const int p5){
+    return 
+        ((pop_digit(f, p0) * 3 + pop_digit(o, p0) * 2 + pop_digit(p, p0)) * p40) + 
+        ((pop_digit(f, p1) * 3 + pop_digit(o, p1) * 2 + pop_digit(p, p1)) * p41) + 
+        ((pop_digit(f, p2) * 3 + pop_digit(o, p2) * 2 + pop_digit(p, p2)) * p42) + 
+        ((pop_digit(f, p3) * 3 + pop_digit(o, p3) * 2 + pop_digit(p, p3)) * p43) + 
+        ((pop_digit(f, p4) * 3 + pop_digit(o, p4) * 2 + pop_digit(p, p4)) * p44) + 
+        ((pop_digit(f, p5) * 3 + pop_digit(o, p5) * 2 + pop_digit(p, p5)) * p45);
+}
+
+inline int pick_pattern(unsigned long long p, unsigned long long o, unsigned long long v, unsigned long long f, const int p0, const int p1, const int p2, const int p3, const int p4, const int p5, const int p6){
+    return 
+        ((pop_digit(f, p0) * 3 + pop_digit(o, p0) * 2 + pop_digit(p, p0)) * p40) + 
+        ((pop_digit(f, p1) * 3 + pop_digit(o, p1) * 2 + pop_digit(p, p1)) * p41) + 
+        ((pop_digit(f, p2) * 3 + pop_digit(o, p2) * 2 + pop_digit(p, p2)) * p42) + 
+        ((pop_digit(f, p3) * 3 + pop_digit(o, p3) * 2 + pop_digit(p, p3)) * p43) + 
+        ((pop_digit(f, p4) * 3 + pop_digit(o, p4) * 2 + pop_digit(p, p4)) * p44) + 
+        ((pop_digit(f, p5) * 3 + pop_digit(o, p5) * 2 + pop_digit(p, p5)) * p45) + 
+        ((pop_digit(f, p6) * 3 + pop_digit(o, p6) * 2 + pop_digit(p, p6)) * p46);
+}
+
+inline int pick_pattern(unsigned long long p, unsigned long long o, unsigned long long v, unsigned long long f, const int p0, const int p1, const int p2, const int p3, const int p4, const int p5, const int p6, const int p7){
+    return 
+        ((pop_digit(f, p0) * 3 + pop_digit(o, p0) * 2 + pop_digit(p, p0)) * p40) + 
+        ((pop_digit(f, p1) * 3 + pop_digit(o, p1) * 2 + pop_digit(p, p1)) * p41) + 
+        ((pop_digit(f, p2) * 3 + pop_digit(o, p2) * 2 + pop_digit(p, p2)) * p42) + 
+        ((pop_digit(f, p3) * 3 + pop_digit(o, p3) * 2 + pop_digit(p, p3)) * p43) + 
+        ((pop_digit(f, p4) * 3 + pop_digit(o, p4) * 2 + pop_digit(p, p4)) * p44) + 
+        ((pop_digit(f, p5) * 3 + pop_digit(o, p5) * 2 + pop_digit(p, p5)) * p45) + 
+        ((pop_digit(f, p6) * 3 + pop_digit(o, p6) * 2 + pop_digit(p, p6)) * p46) + 
+        ((pop_digit(f, p7) * 3 + pop_digit(o, p7) * 2 + pop_digit(p, p7)) * p47);
+}
+
+inline void calc_idx(unsigned long long p, unsigned long long o, unsigned long long v, unsigned long long f, char res){
+    cout << pick_pattern(p, o, v, f, 0, 1, 2, 3, 4, 5, 6, 7) << " ";
+    cout << pick_pattern(p, o, v, f, 0, 8, 16, 24, 32, 40, 48, 56) << " ";
+    cout << pick_pattern(p, o, v, f, 7, 15, 23, 31, 39, 47, 55, 63) << " ";
+    cout << pick_pattern(p, o, v, f, 56, 57, 58, 59, 60, 61, 62, 63) << " ";
+
+    cout << pick_pattern(p, o, v, f, 8, 9, 10, 11, 12, 13, 14, 15) << " ";
+    cout << pick_pattern(p, o, v, f, 1, 9, 17, 25, 33, 41, 49, 57) << " ";
+    cout << pick_pattern(p, o, v, f, 6, 14, 22, 30, 38, 46, 54, 62) << " ";
+    cout << pick_pattern(p, o, v, f, 48, 49, 50, 51, 52, 53, 54, 55) << " ";
+
+    cout << pick_pattern(p, o, v, f, 16, 17, 18, 19, 20, 21, 22, 23) << " ";
+    cout << pick_pattern(p, o, v, f, 2, 10, 18, 26, 34, 42, 50, 58) << " ";
+    cout << pick_pattern(p, o, v, f, 5, 13, 21, 29, 37, 45, 53, 61) << " ";
+    cout << pick_pattern(p, o, v, f, 40, 41, 42, 43, 44, 45, 46, 47) << " ";
+
+    cout << pick_pattern(p, o, v, f, 24, 25, 26, 27, 28, 29, 30, 31) << " ";
+    cout << pick_pattern(p, o, v, f, 3, 11, 19, 27, 35, 43, 51, 59) << " ";
+    cout << pick_pattern(p, o, v, f, 4, 12, 20, 28, 36, 44, 52, 60) << " ";
+    cout << pick_pattern(p, o, v, f, 32, 33, 34, 35, 36, 37, 38, 39) << " ";
+    
+    cout << pick_pattern(p, o, v, f, 5, 14, 23) << " ";
+    cout << pick_pattern(p, o, v, f, 2, 9, 16) << " ";
+    cout << pick_pattern(p, o, v, f, 40, 49, 58) << " ";
+    cout << pick_pattern(p, o, v, f, 61, 54, 47) << " ";
+
+    cout << pick_pattern(p, o, v, f, 4, 13, 22, 31) << " ";
+    cout << pick_pattern(p, o, v, f, 3, 10, 17, 24) << " ";
+    cout << pick_pattern(p, o, v, f, 32, 41, 50, 59) << " ";
+    cout << pick_pattern(p, o, v, f, 60, 53, 46, 39) << " ";
+
+    cout << pick_pattern(p, o, v, f, 3, 12, 21, 30, 39) << " ";
+    cout << pick_pattern(p, o, v, f, 4, 11, 18, 25, 32) << " ";
+    cout << pick_pattern(p, o, v, f, 24, 33, 42, 51, 60) << " ";
+    cout << pick_pattern(p, o, v, f, 59, 52, 45, 38, 31) << " ";
+
+    cout << pick_pattern(p, o, v, f, 2, 11, 20, 29, 38, 47) << " ";
+    cout << pick_pattern(p, o, v, f, 5, 12, 19, 26, 33, 40) << " ";
+    cout << pick_pattern(p, o, v, f, 16, 25, 34, 43, 52, 61) << " ";
+    cout << pick_pattern(p, o, v, f, 58, 51, 44, 37, 30, 23) << " ";
+
+    cout << pick_pattern(p, o, v, f, 1, 10, 19, 28, 37, 46, 55) << " ";
+    cout << pick_pattern(p, o, v, f, 6, 13, 20, 27, 34, 41, 48) << " ";
+    cout << pick_pattern(p, o, v, f, 8, 17, 26, 35, 44, 53, 62) << " ";
+    cout << pick_pattern(p, o, v, f, 57, 50, 43, 36, 29, 22, 15) << " ";
+
+    cout << pick_pattern(p, o, v, f, 0, 9, 18, 27, 36, 45, 54, 63) << " ";
+    cout << pick_pattern(p, o, v, f, 7, 14, 21, 28, 35, 42, 49, 56) << " ";
+    cout << res << endl;
+}
+
+void solve(string line){
+    unsigned long long p = 0, o = 0, v = 0, f = 0;
+    for (int i = 0; i < 64; ++i){
+        if (line[i] == '0')
+            p |= 1ULL << i;
+        else if (line[i] == '1')
+            o |= 1ULL << i;
+        else if (line[i] == '2')
+            v |= 1ULL << i;
+        else if (line[i] == '3')
+            f |= 1ULL << i;
+    }
+    char ans = line[65];
+    calc_idx(p, o, v, f, ans);
+}
+
+#define start_file 0
+#define n_files 60
+
+int main(){
+    int t = 0;
+
+    for (int i = start_file; i < n_files; ++i){
+        cerr << "=";
+        ostringstream sout;
+        sout << setfill('0') << setw(7) << i;
+        string file_name = sout.str();
+        ifstream ifs("data/" + file_name + ".txt");
+        if (ifs.fail()){
+            cerr << "file not exist" << endl;
+            return 0;
+        }
+        string line;
+        while (getline(ifs, line)){
+            ++t;
+            solve(line);
+        }
+        if (i % 25 == 24)
+            cerr << endl;
+    }
+    cerr << t << endl;
+
+    return 0;
+
+}
---
+++ move_ordering/common.hpp
@@ -0,0 +1,145 @@
+#pragma once
+#include <chrono>
+#include <random>
+#ifdef _MSC_VER
+    #include <intrin.h>
+#else
+    #include <x86intrin.h>
+#endif
+
+using namespace std;
+
+#define INF 100000000
+#define N_PHASES 15
+#define PHASE_N_STONES 4
+
+#define HW 8
+#define HW_M1 7
+#define HW_P1 9
+#define HW2 64
+#define HW22 128
+#define HW2_M1 63
+#define HW2_MHW 56
+#define HW2_P1 65
+#define BLACK 0
+#define WHITE 1
+#define VACANT 2
+
+inline long long tim(){
+    return chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
+
+mt19937 raw_myrandom(tim());
+bool global_searching = true;
+
+inline double myrandom(){
+    return (double)raw_myrandom() / mt19937::max();
+}
+
+inline int myrandrange(int s, int e){
+    return s +(int)((e - s) * myrandom());
+}
+
+inline unsigned long long myrand_ull(){
+    return (unsigned long long)(myrandom() * 18446744073709551615ULL);
+}
+
+/*
+inline int pop_count_ull(unsigned long long x){
+    unsigned long long a = x & 0b0101010101010101010101010101010101010101010101010101010101010101ULL;
+    unsigned long long b = x & 0b1010101010101010101010101010101010101010101010101010101010101010ULL;
+    x = a + (b >> 1);
+    a = x & 0b0011001100110011001100110011001100110011001100110011001100110011ULL;
+    b = x & 0b1100110011001100110011001100110011001100110011001100110011001100ULL;
+    x = a + (b >> 2);
+    a = x & 0b0000111100001111000011110000111100001111000011110000111100001111ULL;
+    b = x & 0b1111000011110000111100001111000011110000111100001111000011110000ULL;
+    x = a + (b >> 4);
+    a = x & 0b0000000011111111000000001111111100000000111111110000000011111111ULL;
+    b = x & 0b1111111100000000111111110000000011111111000000001111111100000000ULL;
+    x = a + (b >> 8);
+    a = x & 0b0000000000000000111111111111111100000000000000001111111111111111ULL;
+    b = x & 0b1111111111111111000000000000000011111111111111110000000000000000ULL;
+    x = a + (b >> 16);
+    a = x & 0b0000000000000000000000000000000011111111111111111111111111111111ULL;
+    b = x & 0b1111111111111111111111111111111100000000000000000000000000000000ULL;
+    return (int)(a + (b >> 32));
+}
+*/
+
+inline int pop_count_ull(unsigned long long x){
+    x = x - ((x >> 1) & 0x5555555555555555ULL);
+	x = (x & 0x3333333333333333ULL) + ((x >> 2) & 0x3333333333333333ULL);
+	x = (x + (x >> 4)) & 0x0F0F0F0F0F0F0F0FULL;
+	x = (x * 0x0101010101010101ULL) >> 56;
+    return (int)x;
+}
+
+inline int pop_count_uchar(unsigned char x){
+    x = (x & 0b01010101) + ((x & 0b10101010) >> 1);
+    x = (x & 0b00110011) + ((x & 0b11001100) >> 2);
+    return (x & 0b00001111) + ((x & 11110000) >> 4);
+}
+/*
+inline unsigned long long mirror_v(unsigned long long x){
+    unsigned long long a = x & 0b0101010101010101010101010101010101010101010101010101010101010101ULL;
+    unsigned long long b = x & 0b1010101010101010101010101010101010101010101010101010101010101010ULL;
+    x = (a << 1) | (b >> 1);
+    a = x & 0b0011001100110011001100110011001100110011001100110011001100110011ULL;
+    b = x & 0b1100110011001100110011001100110011001100110011001100110011001100ULL;
+    x = (a << 2) | (b >> 2);
+    a = x & 0b0000111100001111000011110000111100001111000011110000111100001111ULL;
+    b = x & 0b1111000011110000111100001111000011110000111100001111000011110000ULL;
+    x = (a << 4) | (b >> 4);
+    a = x & 0b0000000011111111000000001111111100000000111111110000000011111111ULL;
+    b = x & 0b1111111100000000111111110000000011111111000000001111111100000000ULL;
+    x = (a << 8) | (b >> 8);
+    a = x & 0b0000000000000000111111111111111100000000000000001111111111111111ULL;
+    b = x & 0b1111111111111111000000000000000011111111111111110000000000000000ULL;
+    x = (a << 16) | (b >> 16);
+    a = x & 0b0000000000000000000000000000000011111111111111111111111111111111ULL;
+    b = x & 0b1111111111111111111111111111111100000000000000000000000000000000ULL;
+    return (a << 32) | (b >> 32);
+}
+*/
+
+#ifdef _MSC_VER
+	#define	mirror_v(x)	_byteswap_uint64(x)
+#else
+	#define	mirror_v(x)	__builtin_bswap64(x)
+#endif
+
+inline unsigned long long white_line(unsigned long long x){
+    unsigned long long res = 0;
+    int i, j;
+    for (i = 0; i < HW; ++i){
+        for (j = 0; j < HW; ++j){
+            res |= (1 & (x >> (i * HW + j))) << (j * HW + i);
+        }
+    }
+    return res;
+}
+
+/*
+inline unsigned long long white_line(unsigned long long x){
+    unsigned long long t;
+	t = (x ^ (x >> 7)) & 0x00aa00aa00aa00aaULL;
+	x = x ^ t ^ (t << 7);
+	t = (x ^ (x >> 14)) & 0x0000cccc0000ccccULL;
+	x = x ^ t ^ (t << 14);
+	t = (x ^ (x >> 28)) & 0x00000000f0f0f0f0ULL;
+	x = x ^ t ^ (t << 28);
+	return x;
+}
+*/
+
+inline unsigned long long black_line(unsigned long long x){
+    unsigned long long res = 0;
+    int i, j;
+    for (i = 0; i < HW; ++i){
+        for (j = 0; j < HW; ++j){
+            res |= (1 & (x >> (i * HW + j))) << ((HW_M1 - j) * HW + HW_M1 - i);
+        }
+    }
+    return res;
+}
