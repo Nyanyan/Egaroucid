a23b66c94f1d5f11d324ff2aceb50a3c11d19eb4
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-03-05 16:34:13 +0900

    updating evaluation function

--- evaluation/load_record.py
+++ evaluation/load_record.py
@@ -212,7 +212,7 @@ for year in reversed(range(1977, 2019 + 1)):
         raw_data = f.read()
     games.extend([i for i in raw_data.splitlines()])
 '''
-for i in range(6, 7):
+for i in range(7, 8):
     raw_data = ''
     with open('third_party/records9/' + digit(i, 7) + '.txt', 'r') as f:
         raw_data = f.read()
@@ -223,7 +223,7 @@ idx = 0
 for i in trange(len(games)):
     if len(games[i]) == 0:
         continue
-    collect_data(91 + idx // 1000, games[i])
+    collect_data(119 + idx // 1000, games[i])
     idx += 1
 print(idx)
 
