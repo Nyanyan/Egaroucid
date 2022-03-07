471d20e5abf98da4ffb7c29f1f971a996100210d
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-03-07 16:03:09 +0900

    udpate legal calculation

--- new_src/mobility.hpp
+++ new_src/mobility.hpp
@@ -47,7 +47,7 @@ inline uint64_t calc_some_mobility(uint64_t p, uint64_t o){
             p1 = _mm256_and_si256(_mm256_set_epi64x(ph, pv, phr, pvr), mask);
             p1 = _mm256_slli_epi64(p1, 1);
             o = _mm256_set_epi64x(oh, ov, ohr, ovr);
-            res = _mm256_and_si256(~_mm256_or_si256(p1, o), _mm256_add_epi64(p1, o & mask));
+            res = _mm256_andnot_si256(_mm256_or_si256(p1, o), _mm256_add_epi64(p1, o & mask));
             
             uint64_t a, b;
             a = _mm256_extract_epi64(res, 1);
@@ -93,7 +93,7 @@ inline uint64_t calc_some_mobility(uint64_t p, uint64_t o){
         __m256i p1 = _mm256_and_si256(_mm256_set_epi64x(p45, p135, p45r, p135r), mask);
         p1 = _mm256_slli_epi64(p1, 1);
         __m256i oo = _mm256_set_epi64x(o45, o135, o45r, o135r);
-        __m256i res = _mm256_and_si256(~_mm256_or_si256(p1, oo), _mm256_add_epi64(p1, _mm256_and_si256(oo, mask)));
+        __m256i res = _mm256_andnot_si256(_mm256_or_si256(p1, oo), _mm256_add_epi64(p1, _mm256_and_si256(oo, mask)));
         uint64_t a, b;
         a = _mm256_extract_epi64(res, 1);
         b = _mm256_extract_epi64(res, 0);
@@ -116,7 +116,7 @@ inline uint64_t calc_some_mobility(uint64_t p, uint64_t o){
         return res & ~(p | o);
     }
 
-#else
+#elif LEGAL_CALCULATION_MODE == 2
 
     inline uint64_t calc_legal(const uint64_t P, const uint64_t O){
         uint64_t moves, mO, flip1, pre1, flip8, pre8;
@@ -138,6 +138,79 @@ inline uint64_t calc_some_mobility(uint64_t p, uint64_t o){
         MM = _mm_or_si128(MM, _mm_slli_epi64(flip, 9));                             moves |= flip1 >> 1;                moves |= flip8 >> 8;
         moves |= _mm_cvtsi128_si64(MM) | vertical_mirror(_mm_cvtsi128_si64(_mm_unpackhi_epi64(MM, MM)));
         return moves & ~(P|O);
+    }
+
+#elif LEGAL_CALCULATION_MODE == 3
+
+    inline uint64_t calc_mobility_left(uint64_t p, uint64_t o){
+        uint64_t p1 = (p & 0x7F7F7F7F7F7F7F7FULL) << 1;
+        return ~(p1 | o) & (p1 + (o & 0x7F7F7F7F7F7F7F7FULL));
+    }
+
+    inline uint64_t calc_legal(const uint64_t P, const uint64_t O){
+        uint64_t moves, mO, flip1, pre1, flip8, pre8;
+        __m128i    PP, mOO, MM, flip, pre;
+        mO = O & 0x7E7E7E7E7E7E7E7EULL;
+        PP  = _mm_set_epi64x(vertical_mirror(P), P);
+        mOO = _mm_set_epi64x(vertical_mirror(mO), mO);
+        flip = _mm_and_si128(mOO, _mm_slli_epi64(PP, 7));                                                               flip8  = O & (P << 8);
+        flip = _mm_or_si128(flip, _mm_and_si128(mOO, _mm_slli_epi64(flip, 7)));                                         flip8 |= O & (flip8 << 8);
+        pre  = _mm_and_si128(mOO, _mm_slli_epi64(mOO, 7));                                                              pre8   = O & (O << 8);
+        flip = _mm_or_si128(flip, _mm_and_si128(pre, _mm_slli_epi64(flip, 14)));                                        flip8 |= pre8 & (flip8 << 16);
+        flip = _mm_or_si128(flip, _mm_and_si128(pre, _mm_slli_epi64(flip, 14)));                                        flip8 |= pre8 & (flip8 << 16);
+        MM = _mm_slli_epi64(flip, 7);                                               moves = calc_mobility_left(P, O);   moves |= flip8 << 8;
+        flip = _mm_and_si128(mOO, _mm_slli_epi64(PP, 9));                           flip1  = mO & (P >> 1);             flip8  = O & (P >> 8);
+        flip = _mm_or_si128(flip, _mm_and_si128(mOO, _mm_slli_epi64(flip, 9)));     flip1 |= mO & (flip1 >> 1);         flip8 |= O & (flip8 >> 8);
+        pre = _mm_and_si128(mOO, _mm_slli_epi64(mOO, 9));                           pre1   = mO & (mO >> 1);            pre8 >>= 8;
+        flip = _mm_or_si128(flip, _mm_and_si128(pre, _mm_slli_epi64(flip, 18)));    flip1 |= pre1 & (flip1 >> 2);       flip8 |= pre8 & (flip8 >> 16);
+        flip = _mm_or_si128(flip, _mm_and_si128(pre, _mm_slli_epi64(flip, 18)));    flip1 |= pre1 & (flip1 >> 2);       flip8 |= pre8 & (flip8 >> 16);
+        MM = _mm_or_si128(MM, _mm_slli_epi64(flip, 9));                             moves |= flip1 >> 1;                moves |= flip8 >> 8;
+        moves |= _mm_cvtsi128_si64(MM) | vertical_mirror(_mm_cvtsi128_si64(_mm_unpackhi_epi64(MM, MM)));
+        return moves & ~(P | O);
+    }
+
+#elif LEGAL_CALCULATION_MODE == 4
+
+    inline uint64_t calc_some_mobility_hv(uint64_t ph, uint64_t oh){
+        uint64_t pv, ov, phr, ohr, pvr, ovr;
+        black_line_mirror_double(ph, oh, &pv, &ov);
+        horizontal_mirror_quad(ph, oh, pv, ov, &phr, &ohr, &pvr, &ovr);
+        __m256i p1, o, res, mask;
+        mask = _mm256_set1_epi64x(0x7F7F7F7F7F7F7F7FULL);
+        p1 = _mm256_and_si256(_mm256_set_epi64x(ph, pv, phr, pvr), mask);
+        p1 = _mm256_slli_epi64(p1, 1);
+        o = _mm256_set_epi64x(oh, ov, ohr, ovr);
+        res = _mm256_andnot_si256(_mm256_or_si256(p1, o), _mm256_add_epi64(p1, o & mask));
+        
+        uint64_t a, b;
+        a = _mm256_extract_epi64(res, 1);
+        b = _mm256_extract_epi64(res, 0);
+        horizontal_mirror_double(&a, &b);
+        a |= _mm256_extract_epi64(res, 3);
+        b |= _mm256_extract_epi64(res, 2);
+        return a | black_line_mirror(b);
+    }
+
+    inline uint64_t calc_legal(const uint64_t P, const uint64_t O){
+        uint64_t moves, mO;
+        __m128i    PP, mOO, MM, flip, pre;
+        mO = O & 0x7E7E7E7E7E7E7E7EULL;
+        PP  = _mm_set_epi64x(vertical_mirror(P), P);
+        mOO = _mm_set_epi64x(vertical_mirror(mO), mO);
+        flip = _mm_and_si128(mOO, _mm_slli_epi64(PP, 7));
+        flip = _mm_or_si128(flip, _mm_and_si128(mOO, _mm_slli_epi64(flip, 7)));
+        pre  = _mm_and_si128(mOO, _mm_slli_epi64(mOO, 7));
+        flip = _mm_or_si128(flip, _mm_and_si128(pre, _mm_slli_epi64(flip, 14)));
+        flip = _mm_or_si128(flip, _mm_and_si128(pre, _mm_slli_epi64(flip, 14)));
+        MM = _mm_slli_epi64(flip, 7);
+        flip = _mm_and_si128(mOO, _mm_slli_epi64(PP, 9));
+        flip = _mm_or_si128(flip, _mm_and_si128(mOO, _mm_slli_epi64(flip, 9)));
+        pre = _mm_and_si128(mOO, _mm_slli_epi64(mOO, 9));
+        flip = _mm_or_si128(flip, _mm_and_si128(pre, _mm_slli_epi64(flip, 18)));
+        flip = _mm_or_si128(flip, _mm_and_si128(pre, _mm_slli_epi64(flip, 18)));
+        MM = _mm_or_si128(MM, _mm_slli_epi64(flip, 9));
+        moves = calc_some_mobility_hv(P, O) | _mm_cvtsi128_si64(MM) | vertical_mirror(_mm_cvtsi128_si64(_mm_unpackhi_epi64(MM, MM)));
+        return moves & ~(P | O);
     }
 
 #endif
