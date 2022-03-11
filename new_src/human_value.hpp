f60e59dad30f04bd96f0fe4393044ffedb4f9eaf
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-03-12 04:06:00 +0900

    add gui files

---
+++ new_src/human_value.hpp
@@ -0,0 +1,79 @@
+﻿#pragma once
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include "setting.hpp"
+#include "common.hpp"
+#include "board.hpp"
+#include "book.hpp"
+#include "evaluate.hpp"
+#include "midsearch.hpp"
+
+double calc_human_value(Board *b, int depth, bool passed, double a){
+    if (!global_searching)
+        return 0.0;
+    if (depth == 0){
+        int val = -book.get(b);
+        if (val == INF){
+            val = mid_evaluate(b);
+            //unsigned long long n_nodes = 0;
+            //val = nega_alpha(b, false, 3, -HW2, HW2, &n_nodes);
+        }
+        double v = tanh(a * (double)val / (double)HW2);
+        return v;
+        //return val;
+    }
+    double res = 0.0;
+	unsigned long long legal = b->get_legal ();
+    if (legal == 0){
+        if (passed){
+            int val = -book.get(b);
+            if (val == INF){
+                val = mid_evaluate(b);
+                //unsigned long long n_nodes = 0;
+                //val = nega_alpha(b, false, 3, -HW2, HW2, &n_nodes);
+            }
+            double v = tanh(a * (double)val / (double)HW2);
+            return v;
+            //return val;
+        }
+        b->p = 1 - b->p;
+        res = -calc_human_value(b, depth, true, a);
+        b->p = 1 - b->p;
+        return res;
+    }
+	Flip flip;
+    int cell;
+    for (cell = 0; cell < HW2; ++cell) {
+        if (1 & (legal >> cell)){
+            calc_flip(&flip, b, cell);
+            b->move(&flip);
+            //res = max(res, -calc_human_value(b, depth - 1, false, a));
+				res += -calc_human_value(b, depth - 1, false, a);
+            b->undo(&flip);
+        }
+    }
+    //return res; // / (double)pop_count_ull(legal);
+    return res / (double)pop_count_ull(legal);
+}
+
+void calc_all_human_value(Board b, int depth, double a, int res[]) {
+    unsigned long long legal = b.get_legal();
+    double double_res[HW2];
+	Flip flip;
+    int cell;
+    for (cell = 0; cell < HW2; ++cell) {
+        if (1 & (legal >> cell)) {
+            calc_flip(&flip, &b, cell);
+            b.move(&flip);
+            double_res[cell] = -calc_human_value(&b, depth, false, a);
+            b.undo(&flip);
+            //cerr << cell << " " << double_res[cell] << endl;
+        }
+    }
+    for (cell = 0; cell < HW2; ++cell) {
+        if (1 & (legal >> cell)) {
+            res[cell] = min(99, (int)((double_res[cell] + 1.0) * 50.0));
+        }
+    }
+}
---
+++ new_src/joseki.hpp
@@ -0,0 +1,68 @@
+﻿#pragma once
+#include <Siv3D.hpp> // OpenSiv3D v0.6.3
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "board.hpp"
+
+using namespace std;
+
+
+
+class Joseki {
+private:
+	vector<pair<Board, string>> arr;
+public:
+	bool init() {
+		ifstream ifs("resources/joseki.txt");
+		if (ifs.fail()) {
+			cerr << "joseki file not found" << endl;
+			return false;
+		}
+		string line;
+		string name;
+		int board_arr[HW2];
+		int i;
+		Board b;
+		while (getline(ifs, line)) {
+			for (i = 0; i < HW2; ++i) {
+				if (line[i] == '0')
+					board_arr[i] = BLACK;
+				else if (line[i] == '1')
+					board_arr[i] = WHITE;
+				else
+					board_arr[i] = VACANT;
+			}
+			b.translate_from_arr(board_arr, BLACK);
+			name = line.substr(65, line.size());
+			arr.push_back(make_pair(b, name));
+			b.board_white_line_mirror();
+			arr.push_back(make_pair(b, name));
+			b.board_vertical_mirror();
+			arr.push_back(make_pair(b, name));
+			b.board_black_line_mirror();
+			arr.push_back(make_pair(b, name));
+		}
+		return true;
+	}
+
+	inline string get(Board b) {
+		int i, j;
+		bool flag;
+		for (i = 0; i < (int)arr.size(); ++i) {
+			if (b.player == BLACK && arr[i].first.player == b.player && arr[i].first.opponent == b.opponent)
+				return arr[i].second;
+			if (b.player == WHITE && arr[i].first.player == b.opponent && arr[i].first.opponent == b.player)
+				return arr[i].second;
+		}
+		return "";
+	}
+
+};
+
+Joseki joseki;
+
+bool joseki_init() {
+	return joseki.init();
+}
