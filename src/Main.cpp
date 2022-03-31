9912641f76f07daed15b529af07e5892a4ba2166
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-03-31 10:52:46 +0900

    change button layout

--- src/Main.cpp
+++ src/Main.cpp
@@ -46,7 +46,7 @@ constexpr int graph_sx = 40, graph_sy = 530, graph_width = 424, graph_height = 1
 constexpr int human_sense_graph_sx = 520, human_sense_graph_sy = 275, human_sense_graph_width = 405, human_sense_graph_height = 405, humnan_sense_graph_resolution = 10;
 constexpr Color green = Color(36, 153, 114, 100);
 constexpr int start_game_how_to_use_width = 120, start_game_how_to_use_height = 30;
-constexpr int start_game_button_x = 20, start_game_button_y = 50, start_game_button_w = start_game_how_to_use_width, start_game_button_h = start_game_how_to_use_height, start_game_button_r = 5;
+constexpr int start_game_button_x = 600, start_game_button_y = 49, start_game_button_w = start_game_how_to_use_width, start_game_button_h = start_game_how_to_use_height, start_game_button_r = 5;
 constexpr Color button_color = Palette::White, button_font_color = Palette::Black;
 constexpr int popup_width = 500, popup_height = 300, popup_r = 20, popup_circle_r = 30;
 constexpr Color popup_color = Palette::White, popup_font_color = Palette::Black, popup_frame_color = Palette::Black, textbox_active_color = Palette::Lightcyan;
@@ -1310,7 +1310,7 @@ void learn_book(Board bd, int level, int depth, int book_learn_accept, Board* bd
 }
 
 void show_change_book(int change_book_cell, String changed_book_value_str, Font font) {
-	font(language.get("book", "changed_value") + U"(" + str_record(change_book_cell) + U"): " + changed_book_value_str).draw(left_left, 650, font_color);
+	font(language.get("book", "changed_value") + U"(" + str_record(change_book_cell) + U"): " + changed_book_value_str).draw(600, 90, font_color);
 }
 
 bool import_book_p(string file) {
