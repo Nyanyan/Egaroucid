2984a29f61d65f568422cec7662f919e719c39fd
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-05-10 15:55:56 +0900

    fixed inside flipping bug, tested

--- src/test/several_test.cpp
+++ src/test/several_test.cpp
@@ -6,25 +6,21 @@
 
 using namespace std;
 
+inline uint64_t or_dismiss_1(const uint64_t a, const uint64_t b, const uint64_t c, const uint64_t d){
+    return (a | b | c) & (b | c | d) & (d | a | b);
+}
+
 inline uint64_t calc_legal_flip_inside(const uint64_t player, const uint64_t opponent){
     const uint64_t empty = ~(player | opponent);
-    uint64_t masked = empty & 0x007E7E7E7E7E7E00ULL;
-    uint64_t outside_stones_mask = (masked << HW_M1) | (masked >> HW_M1);
-    uint64_t shifted = (masked << HW_P1) | (masked >> HW_P1);
-    uint64_t dismiss_tmp = outside_stones_mask ^ shifted;
-    uint64_t dismiss = dismiss_tmp;
-    outside_stones_mask |= shifted;
-    masked = empty & 0x7E7E7E7E7E7E7E7EULL;
-    shifted = (masked << 1) | (masked >> 1);
-    outside_stones_mask |= shifted;
-    dismiss_tmp ^= shifted;
-    dismiss &= dismiss_tmp;
+    uint64_t masked = empty & 0x7E7E7E7E7E7E7E7EULL;
+    const uint64_t shift1 = (masked << 1) | (masked >> 1);
     masked = empty & 0x00FFFFFFFFFFFF00ULL;
-    shifted = (masked << HW) | (masked >> HW);
-    outside_stones_mask |= shifted;
-    dismiss_tmp ^= shifted;
-    dismiss &= dismiss_tmp;
-    outside_stones_mask &= ~dismiss;
+    const uint64_t shift8 = (masked << HW) | (masked >> HW);
+    masked = empty & 0x007E7E7E7E7E7E00ULL;
+    const uint64_t shift7 = (masked << HW_M1) | (masked >> HW_M1);
+    const uint64_t shift9 = (masked << HW_P1) | (masked >> HW_P1);
+    uint64_t outside_stones_mask = shift1 | shift8 | shift7 | shift9;
+    outside_stones_mask &= or_dismiss_1(shift1, shift8, shift7, shift9);
     if ((opponent & ~outside_stones_mask) == 0ULL)
         return 0ULL;
     uint64_t legal = calc_legal(player, opponent & ~outside_stones_mask) & empty;
