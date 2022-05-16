25a9d1150f5accb75b54a11d473495bacbe89f1c
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-05-17 01:35:53 +0900

    learn written

--- evaluation_test/learn.py
+++ evaluation_test/learn.py
@@ -13,7 +13,7 @@ from tensorflow.keras.callbacks import ReduceLROnPlateau
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import trange
-from random import randrange, sample, shuffle
+from random import random, randrange, sample, shuffle
 import subprocess
 import datetime
 import os
@@ -41,7 +41,8 @@ n_patterns = 16
 n_additional_features = 4
 n_mobility = 4
 
-n_raw_data = 86
+n_raw_data = 82
+n_raw_data_input = 86
 
 
 pattern_nums = [
@@ -67,14 +68,63 @@ n_raw_patterns = 62
 n_raw_mobility = 16
 
 feature_sizes = [8, 8, 8, 5, 6, 7, 8, 10, 10, 10, 10, 9, 10, 10, 10, 10, 0, 0, 0, 0, 8, 8, 8, 8]
-n_pattern_varieties = [4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+n_pattern_varieties = [4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 4, 4, 4, 4]
 
 additional_feature_mul = [100, 50, 65, 65]
 
+feature_actual_sizes = [
+    3 ** 8, 3 ** 8, 3 ** 8, 3 ** 5, 3 ** 6, 3 ** 7, 3 ** 8, 3 ** 10, 3 ** 10, 3 ** 10, 3 ** 10, 3 ** 9, 3 ** 10, 3 ** 10, 3 ** 10, 3 ** 10, 
+    100 * 100, 50 * 50, 65 * 65, 65 * 65,
+    2 ** 16, 2 ** 16, 2 ** 16, 2 ** 16
+]
+
+feature_idxes = [
+    0, 4, 8, 12, 16, 20, 24, 26, 30, 34, 38, 42, 46, 50, 54, 58, 
+    62, 63, 64, 65, 
+    66, 70, 74, 78, 
+    82
+]
+
+input_sizes = [16, 16, 16, 10, 12, 14, 16, 20, 20, 20, 20, 18, 20, 20, 20, 20, 2, 2, 2, 2, 16, 16, 16, 16]
+
+input_sizes_raw = []
+for i in range(len(input_sizes)):
+    for _ in range(n_pattern_varieties[i]):
+        input_sizes_raw.append(input_sizes[i])
 
 
+def idx2pattern(pattern_idx, idx):
+    pattern_size = feature_sizes[pattern_nums[pattern_idx]]
+    pattern_unzipped = [0.0 for _ in range(pattern_size * 2)]
+    for cell in range(pattern_size):
+        elem = idx % 3
+        if elem == 0:
+            pattern_unzipped[cell] = 1.0
+        elif elem == 1:
+            pattern_unzipped[pattern_size + cell] = 1.0
+        idx //= 3
+    return pattern_unzipped
 
+def idx2pattern2(pattern_idx, idx):
+    pattern_size = feature_sizes[pattern_idx]
+    pattern_unzipped = [0.0 for _ in range(pattern_size * 2)]
+    for cell in range(pattern_size):
+        elem = idx % 3
+        if elem == 0:
+            pattern_unzipped[cell] = 1.0
+        elif elem == 1:
+            pattern_unzipped[pattern_size + cell] = 1.0
+        idx //= 3
+    return pattern_unzipped
 
+def idx2mobility(idx):
+    pattern_unzipped = [0.0 for _ in range(8 * 2)]
+    for cell in range(8 * 2):
+        elem = idx % 2
+        if elem == 1:
+            pattern_unzipped[cell] = 1.0
+        idx //= 2
+    return pattern_unzipped
 
 # create model
 x = [None for _ in range(n_raw_data)]
@@ -105,16 +155,16 @@ for i in range(n_additional_features):
     y_add = Dense(n_dense_additional, name=add_names[i] + '_dense1')(y_add)
     y_add = LeakyReLU(alpha=0.01)(y_add)
     y_add = Dense(n_dense_additional, name=add_names[i] + '_out')(y_add)
-    y_before_add.add(y_add)
+    y_before_add.append(y_add)
     idx += 1
 mobility_names = ['mobility1', 'mobility2', 'mobility3', 'mobility4']
 for i in range(n_mobility):
     layers = []
-    layers.append(Dense(n_dense_pattern, name=names[i] + '_dense0'))
+    layers.append(Dense(n_dense_pattern, name=mobility_names[i] + '_dense0'))
     layers.append(LeakyReLU(alpha=0.01))
-    layers.append(Dense(n_dense_pattern, name=names[i] + '_dense1'))
+    layers.append(Dense(n_dense_pattern, name=mobility_names[i] + '_dense1'))
     layers.append(LeakyReLU(alpha=0.01))
-    layers.append(Dense(1, name=names[i] + '_out'))
+    layers.append(Dense(1, name=mobility_names[i] + '_out'))
     add_elems = []
     for j in range(4):
         x[idx] = Input(shape=8 * 2, name=mobility_names[i] + '_in_' + str(j))
@@ -124,13 +174,13 @@ for i in range(n_mobility):
         add_elems.append(tmp)
         idx += 1
     y_before_add.extend(add_elems)
-y = Add()(y_before_add)
+y = Add(name='last_layer')(y_before_add)
 model = Model(inputs=x, outputs=y)
 
-#model = load_model('learned_data/bef_' + str(stone_strt) + '_' + str(stone_end) + '.h5')
+#model = load_model('learned_data/' + str(use_phase) + '_' + str(n_dense_pattern) + '.h5')
 
-#model.summary()
-#plot_model(model, to_file='model.png', show_shapes=True)
+model.summary()
+plot_model(model, to_file='model.png', show_shapes=True)
 
 model.compile(loss='mse', metrics='mae', optimizer='adam')
 
@@ -138,30 +188,33 @@ model.compile(loss='mse', metrics='mae', optimizer='adam')
 
 
 
-
 # input index data
-all_data_idx = [[] for _ in range(n_raw_data)]
+all_data_idx = [[] for _ in range(n_raw_data_input)]
 all_labels_raw = []
 for file in input_files:
     with open(file, 'rb') as f:
         while True:
             if len(all_labels_raw) % 10000 == 0:
-                print(len(all_labels_raw))
-            try:
-                b = f.read(4)
-                n_stones = int.from_bytes(b, byteorder='big', signed=True)
-                phase = (n_stones - 4) // ply_d
-                if phase == use_phase:
-                    for i in range(n_raw_data):
-                        b = f.read(4)
-                        all_data_idx[i].append(int.from_bytes(b, byteorder='big', signed=True))
-                    b = f.read(4)
-                    all_labels_raw.append(int.from_bytes(b, byteorder='big', signed=True))
-                else:
-                    f.read(4 * (n_raw_data + 1))
-            except:
+                print('\r', len(all_labels_raw), end='')
+            if len(all_labels_raw) >= 1000000:
                 break
-
+            b = f.read(4)
+            if not b:
+                break
+            n_stones = int.from_bytes(b, byteorder='little', signed=False)
+            #print(n_stones)
+            phase = (n_stones - 4) // ply_d
+            b = f.read(4)
+            if phase == use_phase:
+                for i in range(n_raw_data_input):
+                    b = f.read(4)
+                    all_data_idx[i].append(int.from_bytes(b, byteorder='little', signed=False))
+                b = f.read(4)
+                score = int.from_bytes(b, byteorder='little', signed=True)
+                all_labels_raw.append(score)
+            else:
+                f.read(4 * (n_raw_data_input + 1))
+            
 
 
 
@@ -173,53 +226,30 @@ for file in input_files:
 
 
 # translate to input array
-def idx2pattern(pattern_idx, idx):
-    pattern_size = feature_sizes[pattern_nums[pattern_idx]]
-    pattern_unzipped = [0.0 for _ in range(pattern_size * 2)]
-    for cell in range(feature_sizes[pattern_idx]):
-        elem = idx % 3
-        if elem == 0:
-            pattern_unzipped[cell] = 1.0
-        elif elem == 1:
-            pattern_unzipped[pattern_size + cell] = 1.0
-        elem //= 3
-
-def idx2mobility(idx):
-    pattern_unzipped = [0.0 for _ in range(8 * 2)]
-    for cell in range(8 * 2):
-        elem = idx % 2
-        if elem == 1:
-            pattern_unzipped[cell] = 1.0
-        elem //= 2
-
-all_data = [[] for _ in range(n_raw_data)]
-all_labels = []
-for data_idx in range(len(all_labels_raw)):
+all_data = [np.zeros((len(all_labels_raw), input_sizes_raw[i])) for i in range(n_raw_data)]
+all_labels = np.zeros(len(all_labels_raw))
+for data_idx in trange(len(all_labels_raw)):
     for pattern_idx in range(n_raw_patterns):
         idx = all_data_idx[pattern_idx][data_idx]
         pattern_unzipped = idx2pattern(pattern_idx, idx)
-        all_data[pattern_idx].append(pattern_unzipped)
+        all_data[pattern_idx][data_idx] = np.array(pattern_unzipped)
     for additional_feature_idx in range(n_additional_features):
-        additional_feature = [all_data_idx[n_raw_patterns + additional_feature_idx * 2], all_data_idx[n_raw_patterns + additional_feature_idx * 2 + 1]]
-        all_data[n_raw_patterns + additional_feature_idx].append(additional_feature)
+        additional_feature = [all_data_idx[n_raw_patterns + additional_feature_idx * 2][data_idx], all_data_idx[n_raw_patterns + additional_feature_idx * 2 + 1][data_idx]]
+        all_data[n_raw_patterns + additional_feature_idx][data_idx] = np.array(additional_feature)
     for mobility_idx in range(n_raw_mobility):
-        idx = all_data_idx[n_raw_patterns + n_additional_features * 2 + mobility_idx]
+        idx = all_data_idx[n_raw_patterns + n_additional_features * 2 + mobility_idx][data_idx]
         pattern_unzipped = idx2mobility(idx)
-        all_data[n_raw_patterns + n_additional_features + mobility_idx].append(idx)
+        all_data[n_raw_patterns + n_additional_features + mobility_idx][data_idx] = np.array(pattern_unzipped)
     score = all_labels_raw[data_idx] / 64
-    all_labels.append(score)
+    all_labels[data_idx] = score
 
-
+print([all_data[i].shape for i in range(n_raw_data)], all_labels.shape)
 
 
 
 # learn
 len_data = len(all_labels)
 print('len_data', len_data)
-
-all_data = [np.array(arr) for arr in all_data]
-all_labels = np.array(all_labels)
-print('converted to np arr')
 
 n_train_data = int(len_data * (1.0 - test_ratio))
 n_test_data = int(len_data * test_ratio)
@@ -239,20 +269,46 @@ test_labels = all_labels[test_idxes]
 early_stop = EarlyStopping(monitor='val_loss', patience=5)
 #model_checkpoint = ModelCheckpoint(filepath=os.path.join('learned_data/' + str(phase), 'model_{epoch:02d}_{val_loss:.5f}_{val_mae:.5f}.h5'), monitor='val_loss', verbose=1)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=4, min_lr=0.0001)
-history = model.fit(train_data, train_labels, epochs=n_epochs, validation_data=(test_data, test_labels), callbacks=[early_stop, reduce_lr])
+history = model.fit(train_data, train_labels, epochs=n_epochs, batch_size = 2048, validation_data=(test_data, test_labels), callbacks=[early_stop, reduce_lr])
 
 #now = datetime.datetime.today()
 #print(str(now.year) + digit(now.month, 2) + digit(now.day, 2) + '_' + digit(now.hour, 2) + digit(now.minute, 2))
-model.save('learned_data/' + str(phase) + '.h5')
+model.save('learned_data/' + str(use_phase) + '_' + str(n_dense_pattern) + '.h5')
 
 for key in ['loss', 'val_loss']:
     plt.plot(history.history[key], label=key)
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.legend(loc='best')
-    plt.savefig('graph/loss_' + str(phase) + '.png')
+    plt.savefig('graph/loss_' + str(use_phase) + '_' + str(n_dense_pattern) + '.png')
     plt.clf()
 
-
-
 # pre calculation
+pre_calc_data = [np.zeros((65536, input_sizes_raw[i])) for i in range(n_raw_data)]
+for idx in trange(65536):
+    for pattern_idx in range(n_patterns):
+        if idx < feature_actual_sizes[pattern_idx]:
+            #print(idx2pattern2(pattern_idx, idx))
+            pre_calc_data[feature_idxes[pattern_idx]][idx] = np.array(idx2pattern2(pattern_idx, idx))
+    for additional_feature in range(n_additional_features):
+        if idx < feature_actual_sizes[n_patterns + additional_feature]:
+            pre_calc_data[feature_idxes[n_patterns + additional_feature]][idx] = np.array([idx // additional_feature_mul[additional_feature], idx % additional_feature_mul[additional_feature]])
+    for mobility_idx in range(n_mobility):
+        pre_calc_data[feature_idxes[n_patterns + n_additional_features + mobility_idx]][idx] = np.array(idx2mobility(idx))
+
+last_layer_model = Model(inputs=model.input, outputs=model.get_layer('last_layer').input)
+
+predictions = last_layer_model.predict(pre_calc_data, batch_size = 2048)
+print(len(predictions), len(predictions[0]))
+step = 256
+n_lines = 0
+plus_elem = 0
+with open('learned_data/' + str(use_phase) + '_' + str(n_dense_pattern) + '.txt', 'w') as f:
+    for feature in range(24):
+        for i in range(feature_actual_sizes[pattern_idx]):
+            val = round(predictions[feature_idxes[pattern_idx]][i][0] * 64 * step)
+            if predictions[feature_idxes[pattern_idx]][i][0] > 0.0:
+                plus_elem += 1
+            f.write(str(val) + '\n')
+            n_lines += 1
+print('done', n_lines, plus_elem)
