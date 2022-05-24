29968eebf3289b7a11ae01d2aabef881fefb80a5
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-05-24 12:24:01 +0900

    update tt

--- src/transpose_table.hpp
+++ src/transpose_table.hpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-#define TRANSPOSE_TABLE_SIZE 4194304
-#define TRANSPOSE_TABLE_MASK 4194303
+#define TRANSPOSE_TABLE_SIZE 2097152
+#define TRANSPOSE_TABLE_MASK 2097151
 
 #define CACHE_SAVE_EMPTY 15
 
@@ -214,7 +214,6 @@ class Child_transpose_table{
 
 class Node_parent_transpose_table{
     private:
-        mutex mtx;
         uint64_t player;
         uint64_t opponent;
         int lower;
@@ -228,7 +227,6 @@ class Node_parent_transpose_table{
         }
 
         inline void register_value_with_board(const Board *board, const int l, const int u){
-            lock_guard<mutex> lock(mtx);
             player = board->player;
             opponent = board->opponent;
             lower = l;
@@ -236,7 +234,6 @@ class Node_parent_transpose_table{
         }
 
         inline bool register_value(const Board *board, const int l, const int u){
-            lock_guard<mutex> lock(mtx);
             if (board->player == player && board->opponent == opponent){
                 lower = l;
                 upper = u;
@@ -246,7 +243,6 @@ class Node_parent_transpose_table{
         }
 
         inline void register_value_with_board(Node_parent_transpose_table *from){
-            lock_guard<mutex> lock(mtx);
             player = from->player;
             opponent = from->opponent;
             lower = from->lower;
@@ -254,42 +250,85 @@ class Node_parent_transpose_table{
         }
 
         inline void get(const Board *board, int *l, int *u){
-            lock_guard<mutex> lock(mtx);
             if (board->player == player && board->opponent == opponent){
                 *l = lower;
                 *u = upper;
             }
         }
 
-        inline int n_stones(){
+        inline int n_stones() const{
             return pop_count_ull(player | opponent);
         }
 };
 
+class Node_mutex_parent_transpose_table{
+    private:
+        mutex mtx;
+    public:
+        Node_parent_transpose_table *node;
+    
+    public:
+        inline void register_value_with_board(const Board *board, const int l, const int u){
+            lock_guard<mutex> lock(mtx);
+            node->register_value_with_board(board, l, u);
+        }
+
+        inline bool register_value(const Board *board, const int l, const int u){
+            lock_guard<mutex> lock(mtx);
+            return node->register_value(board, l, u);
+        }
+
+        inline void register_value_with_board(Node_parent_transpose_table *from){
+            lock_guard<mutex> lock(mtx);
+            node->register_value_with_board(from);
+        }
+
+        inline void get(const Board *board, int *l, int *u){
+            lock_guard<mutex> lock(mtx);
+            return node->get(board, l, u);
+        }
+
+        inline int n_stones() const{
+            return node->n_stones();
+        }
+
+        inline bool is_null() const{
+            return node == NULL;
+        }
+
+        inline void set(){
+            node = (Node_parent_transpose_table*)malloc(sizeof(Node_parent_transpose_table));
+        }
+
+        inline void set_null(){
+            node = NULL;
+        }
+
+        inline void init(){
+            node->init();
+            node = NULL;
+        }
+};
+
 #if USE_MULTI_THREAD
-    void init_parent_transpose_table(Node_parent_transpose_table *table[], int s, int e){
+    void init_parent_transpose_table(Node_mutex_parent_transpose_table table[], int s, int e){
         for(int i = s; i < e; ++i){
-            if (table[i] != NULL){
-                table[i]->init();
-                table[i] = NULL;
-            }
+            if (!table[i].is_null())
+                table[i].init();
         }
     }
 
-    void copy_parent_transpose_table(Node_parent_transpose_table *from[], Node_parent_transpose_table *to[], int s, int e){
+    void copy_parent_transpose_table(Node_mutex_parent_transpose_table from[], Node_mutex_parent_transpose_table to[], int s, int e){
         for(int i = s; i < e; ++i){
-            if (from[i] != NULL){
-                if (from[i]->n_stones() < HW2 - CACHE_SAVE_EMPTY){
-                    if (to[i] == NULL){
-                        Node_parent_transpose_table *p = (Node_parent_transpose_table*)malloc(sizeof(Node_parent_transpose_table));
-                        to[i] = new(p) Node_parent_transpose_table;
-                    }
-                    to[i]->register_value_with_board(from[i]);
+            if (!from[i].is_null()){
+                if (from[i].n_stones() < HW2 - CACHE_SAVE_EMPTY){
+                    if (to[i].is_null())
+                        to[i].set();
+                    to[i].register_value_with_board(from[i].node);
                 }
             } else{
-                if (to[i] != NULL)
-                    to[i]->init();
-                to[i] = NULL;
+                if (!to[i].is_null())
+                    to[i].init();
             }
         }
     }
@@ -297,12 +336,12 @@ class Node_parent_transpose_table{
 
 class Parent_transpose_table{
     private:
-        Node_parent_transpose_table *table[TRANSPOSE_TABLE_SIZE];
+        Node_mutex_parent_transpose_table table[TRANSPOSE_TABLE_SIZE];
 
     public:
         inline void first_init(){
             for(int i = 0; i < TRANSPOSE_TABLE_SIZE; ++i)
-                table[i] = NULL;
+                table[i].set_null();
         }
 
         #if USE_MULTI_THREAD
@@ -322,27 +361,23 @@ class Parent_transpose_table{
         #else
             inline void init(){
                 for(int i = 0; i < TRANSPOSE_TABLE_SIZE; ++i){
-                    if (table[i] != NULL){
-                        table[i]->init();
-                        table[i] = NULL;
-                    }
+                    if (!table[i].is_null())
+                        table[i].init();
                 }
             }
         #endif
 
         inline void reg(const Board *board, const uint32_t hash, const int l, const int u){
-            if (table[hash] == NULL){
-                Node_parent_transpose_table *p = (Node_parent_transpose_table*)malloc(sizeof(Node_parent_transpose_table));
-                table[hash] = new(p) Node_parent_transpose_table;
-            }
-            table[hash]->register_value_with_board(board, l, u);
+            if (table[hash].is_null())
+                table[hash].set();
+            table[hash].register_value_with_board(board, l, u);
         }
 
-        inline void get(const Board *board, const uint32_t hash, int *l, int *u) const{
+        inline void get(const Board *board, const uint32_t hash, int *l, int *u){
             *l = -INF;
             *u = INF;
-            if (table[hash] != NULL)
-                table[hash]->get(board, l, u);
+            if (!table[hash].is_null())
+                table[hash].get(board, l, u);
         }
 
         #if USE_MULTI_THREAD
@@ -362,14 +397,13 @@ class Parent_transpose_table{
         #else
             inline void copy(Parent_transpose_table *to){
                 for(int i = 0; i < TRANSPOSE_TABLE_SIZE; ++i){
-                    if (table[i] != NULL){
-                        if (to->table[i] == NULL)
-                            to->table[i] = (Node_parent_transpose_table*)malloc(sizeof(Node_parent_transpose_table));
-                        to->table[i]->register_value_with_board(table[i]);
+                    if (!table[i].is_null()){
+                        if (to->table[i].is_null())
+                            to->table[i].set();
+                        to->table[i].register_value_with_board(table[i]);
                     } else{
-                        if (to->table[i] != NULL)
-                            to->table[i]->init();
-                        to->table[i] = NULL;
+                        if (!to->table[i].is_null())
+                            to->table[i].init();
                     }
                 }
             }
