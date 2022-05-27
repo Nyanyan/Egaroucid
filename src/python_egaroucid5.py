75cfd6c15ff5ff15c75079addf777057d801b8bc
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-05-27 12:52:39 +0900

    egaroucid5 on python written

---
+++ src/python_egaroucid5.py
@@ -0,0 +1,6 @@
+import egaroucid5
+
+print(egaroucid5.init('test/resources/eval.egev', 'test/resources/book.egbk'))
+
+print(egaroucid5.ai('1..11110.1111110110011101101110011111100...11110....1..0........0', 21))
+
---
+++ src/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup, Extension
+
+setup(name = 'egaroucid5', version = '1.0.0', ext_modules = [Extension('egaroucid5', ['egaroucid5module.cpp'])])
