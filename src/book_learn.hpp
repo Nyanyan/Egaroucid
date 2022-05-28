84b5c742f92b3e425122a1f478aa0965c311f7e5
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-05-28 17:10:32 +0900

    update book learn

--- src/book_learn.hpp
+++ src/book_learn.hpp
@@ -38,8 +38,10 @@ inline int book_learn_calc_value(Board board, int level, int bound){
     return g;
 }
 
-int book_learn_search(Board board, int level, const int book_depth, const int best_value, const int expected_error, Board *board_copy, uint64_t *strt_tim, string book_file, string book_bak){
+int book_learn_search(Board board, int level, const int book_depth, const int best_value, int error_sum, const int expected_error, const int adopt_error_sum, Board *board_copy, uint64_t *strt_tim, string book_file, string book_bak){
     if (!global_searching)
+        return SCORE_UNDEFINED;
+    if (error_sum > adopt_error_sum)
         return SCORE_UNDEFINED;
     if (tim() - *strt_tim > AUTO_BOOK_SAVE_TIME){
         book.save_bin(book_file, book_bak);
@@ -50,7 +52,7 @@ int book_learn_search(Board board, int level, const int book_depth, const int be
         g = -INF;
         if (-SCORE_MAX <= best_value && best_value <= SCORE_MAX)
             g = book_learn_calc_value(board, level, best_value);
-        if (g <= best_value || best_value < -SCORE_MAX || SCORE_MAX < best_value){
+        if ((g != -INF && g <= best_value) || best_value < -SCORE_MAX || SCORE_MAX < best_value){
             g = ai(board, level, true, 0).value;
             book.reg(board, -g);
             return g;
@@ -60,7 +62,7 @@ int book_learn_search(Board board, int level, const int book_depth, const int be
     uint64_t legal = board.get_legal();
     if (legal == 0ULL){
         board.pass();
-            g = -book_learn_search(board, level, book_depth, -best_value, expected_error, board_copy, strt_tim, book_file, book_bak);
+            g = -book_learn_search(board, level, book_depth, -best_value, error_sum, expected_error, adopt_error_sum, board_copy, strt_tim, book_file, book_bak);
         board.pass();
         return g;
     }
@@ -73,7 +75,7 @@ int book_learn_search(Board board, int level, const int book_depth, const int be
     calc_flip(&flip, &board, (uint8_t)best_move.policy);
     board.move(&flip);
         board.copy(board_copy);
-        g = -book_learn_search(board, level, book_depth, SCORE_UNDEFINED, expected_error, board_copy, strt_tim, book_file, book_bak);
+        g = -book_learn_search(board, level, book_depth, SCORE_UNDEFINED, error_sum, expected_error, adopt_error_sum, board_copy, strt_tim, book_file, book_bak);
         if (-SCORE_MAX <= g && g <= SCORE_MAX){
             best_move.value = g;
             cerr << "PV value " << best_move.value << endl;
@@ -95,7 +97,7 @@ int book_learn_search(Board board, int level, const int book_depth, const int be
                     flag = true;
             }
             if (flag || (-SCORE_MAX <= g && g <= SCORE_MAX && g >= best_move.value - expected_error)){
-                g = -book_learn_search(board, level, book_depth, -best_move.value, expected_error, board_copy, strt_tim, book_file, book_bak);
+                g = -book_learn_search(board, level, book_depth, -best_move.value, error_sum + max(0, best_move.value - g), expected_error, adopt_error_sum, board_copy, strt_tim, book_file, book_bak);
                 if (-SCORE_MAX <= g && g <= SCORE_MAX){
                     if (g > best_move.value){
                         best_move.value = g;
@@ -118,7 +120,8 @@ int book_learn_search(Board board, int level, const int book_depth, const int be
 inline void learn_book(Board root_board, int level, const int book_depth, const int expected_error, Board *board_copy, string book_file, string book_bak, bool *book_learning){
     uint64_t strt_tim = tim();
     cerr << "book learn started" << endl;
-    int g = book_learn_search(root_board, level, book_depth, SCORE_UNDEFINED, expected_error, board_copy, &strt_tim, book_file, book_bak);
+    const int adopt_error_sum = max(expected_error, (book_depth + 4 - root_board.n) * expected_error / 5);
+    int g = book_learn_search(root_board, level, book_depth, SCORE_UNDEFINED, 0, expected_error, adopt_error_sum, board_copy, &strt_tim, book_file, book_bak);
     root_board.copy(board_copy);
     cerr << "book learn finished " << g << endl;
     *book_learning = false;
