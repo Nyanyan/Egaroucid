268b031ed4c4c50d229aa55842b8ca87fd6aca60
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-10-10 19:53:31 +0900

    fixed auto-lang-select

---
+++ docs/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    //Language
+    var language = (window.navigator.languages && window.navigator.languages[0]) ||
+    window.navigator.language ||
+    window.navigator.userLanguage ||
+    window.navigator.browserLanguage;
+
+    console.log(language);
+
+    if (language == 'ja')
+        window.location.href = "https://www.egaroucid-app.nyanyan.dev/ja";
+    else
+        window.location.href = "https://www.egaroucid-app.nyanyan.dev/en";
+}
