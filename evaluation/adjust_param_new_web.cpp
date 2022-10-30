a0bb11a498cd26ec6dc48d2f15578faecdcb5397
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-10-30 19:42:36 +0900

    delete useless files

--- evaluation/adjust_param_new_web.cpp
+++ evaluation/adjust_param_new_web.cpp
@@ -10,10 +10,10 @@
 
 using namespace std;
 
-#define n_phases 30
-#define phase_n_stones 2
+#define n_phases 10
+#define phase_n_stones 6
 #define n_patterns 16
-#define n_eval (n_patterns + 4 + 4)
+#define n_eval 23
 #define max_surround 100
 #define max_canput 50
 #define max_stability 65
@@ -45,7 +45,7 @@ int sa_phase, sa_player;
 #define step 256
 #define sc_w (step * HW2)
 
-#define n_data 50000000
+#define n_data 65000000 / 3
 
 #define n_raw_params 86
 
@@ -56,9 +56,9 @@ unsigned long long second = 0;
 
 double alpha[n_eval][max_evaluate_idx];
 
-const int pattern_sizes[n_eval] = {8, 8, 8, 5, 6, 7, 8, 10, 10, 10, 10, 9, 10, 10, 10, 10, 0, 0, 0, 0, 8, 8, 8, 8};
-const int eval_sizes[n_eval] = {p38, p38, p38, p35, p36, p37, p38, p310, p310, p310, p310, p39, p310, p310, p310, p310, max_surround * max_surround, max_canput * max_canput, max_stability * max_stability, max_stone_num * max_stone_num, p48, p48, p48, p48};
-short eval_arr[n_phases][n_eval][max_evaluate_idx];
+const int pattern_sizes[n_eval] = {8, 8, 8, 5, 6, 7, 8, 10, 10, 10, 10, 9, 10, 10, 10, 10, 0, 0, 0, 8, 8, 8, 8};
+const int eval_sizes[n_eval] = {p38, p38, p38, p35, p36, p37, p38, p310, p310, p310, p310, p39, p310, p310, p310, p310, max_surround * max_surround, max_canput * max_canput, max_stone_num * max_stone_num, p48, p48, p48, p48};
+double eval_arr[n_phases][n_eval][max_evaluate_idx];
 int test_data[n_data / n_phases][n_raw_params];
 double test_labels[n_data / n_phases];
 int nums;
@@ -86,32 +86,46 @@ void initialize_param(){
     cerr << endl;
 }
 
-inline bool input_param(){
-    FILE* fp;
-    #ifdef _WIN64
-        if (fopen_s(&fp, "resources/eval.egev", "rb") != 0){
-            cerr << "can't open eval.egev" << endl;
-            return false;
-        }
-    #else
-        fp = fopen("resources/eval.egev", "rb");
-        if (fp == NULL){
-            cerr << "can't open eval.egev" << endl;
-            return false;
-        }
-    #endif
-    int phase_idx, pattern_idx;
-    for (phase_idx = 0; phase_idx < N_PHASES; ++phase_idx){
+void input_param(){
+    ifstream ifs("f_param.txt");
+    if (ifs.fail()){
+        cerr << "evaluation file not exist" << endl;
+        exit(1);
+    }
+    string line;
+    int t =0;
+    int phase_idx, pattern_idx, pattern_elem, dense_idx, canput, sur0, sur1, i, j, k;
+    for (phase_idx = 0; phase_idx < n_phases; ++phase_idx){
+        cerr << "=";
         for (pattern_idx = 0; pattern_idx < n_eval; ++pattern_idx){
-            if (fread(eval_arr[phase_idx][pattern_idx], 2, eval_sizes[pattern_idx], fp) < eval_sizes[pattern_idx]){
-                cerr << "eval.egev broken" << endl;
-                fclose(fp);
-                return false;
+            for (pattern_elem = 0; pattern_elem < eval_sizes[pattern_idx]; ++pattern_elem){
+                ++t;
+                getline(ifs, line);
+                eval_arr[phase_idx][pattern_idx][pattern_elem] = stoi(line);
             }
         }
     }
-    cerr << "evaluation function initialized" << endl;
-    return true;
+    cerr << t << endl;
+}
+
+void input_param_onephase(string file){
+    ifstream ifs(file);
+    if (ifs.fail()){
+        cerr << "evaluation file not exist" << endl;
+        exit(1);
+    }
+    string line;
+    int t =0;
+    int pattern_idx, pattern_elem, dense_idx, canput, sur0, sur1, i, j, k;
+    for (pattern_idx = 0; pattern_idx < n_eval; ++pattern_idx){
+        cerr << "=";
+        for (pattern_elem = 0; pattern_elem < eval_sizes[pattern_idx]; ++pattern_elem){
+            ++t;
+            getline(ifs, line);
+            eval_arr[sa_phase][pattern_idx][pattern_elem] = stoi(line);
+        }
+    }
+    cerr << t << endl;
 }
 
 inline int calc_sur0_sur1(int arr[]){
@@ -134,6 +148,14 @@ inline double calc_score(int phase, int i);
 
 void input_test_data(int argc, char *argv[]){
     int i, j, k;
+    /*
+    ifstream ifs("big_data.txt");
+    if (ifs.fail()){
+        cerr << "evaluation file not exist" << endl;
+        exit(1);
+    }
+    string line;
+    */
     int phase, player, score;
     int t = 0, u = 0;
     nums = 0;
@@ -171,16 +193,16 @@ void input_test_data(int argc, char *argv[]){
         n_data_score[i] = 0;
     int sur, canput, stab, num;
     FILE* fp;
-    for (int file_idx = 1; file_idx < argc; ++file_idx){
+    for (int file_idx = 7; file_idx < argc; ++file_idx){
         cerr << argv[file_idx] << endl;
         if (fopen_s(&fp, argv[file_idx], "rb") != 0) {
             cerr << "can't open " << argv[file_idx] << endl;
             continue;
         }
-        while (t < n_data){
+        while (u < n_data / n_phases - 10){
             ++t;
             if ((t & 0b1111111111111111) == 0b1111111111111111)
-                cerr << '\r' << t;
+                cerr << '\r' << t << " " << u;
             if (fread(&phase, 4, 1, fp) < 1)
                 break;
             phase = (phase - 4) / phase_n_stones;
@@ -193,23 +215,20 @@ void input_test_data(int argc, char *argv[]){
                     used_idxes[pattern_nums[i]].emplace(test_data[nums][i]);
                 sur = calc_sur0_sur1(test_data[nums]);
                 canput = calc_canput0_canput1(test_data[nums]);
-                stab = calc_stab0_stab1(test_data[nums]);
                 num = calc_num0_num1(test_data[nums]);
                 used_idxes[16].emplace(sur);
                 used_idxes[17].emplace(canput);
-                used_idxes[18].emplace(stab);
-                used_idxes[19].emplace(num);
+                used_idxes[18].emplace(num);
                 for (i = 0; i < 16; ++i)
-                    used_idxes[20 + i / 4].emplace(test_data[nums][70 + i]);
+                    used_idxes[19 + i / 4].emplace(test_data[nums][70 + i]);
                 test_labels[nums] = score * step;
                 for (i = 0; i < 62; ++i)
                     test_memo[pattern_nums[i]][test_data[nums][i]].push_back(nums);
                 test_memo[16][sur].push_back(nums);
                 test_memo[17][canput].push_back(nums);
-                test_memo[18][stab].push_back(nums);
-                test_memo[19][num].push_back(nums);
+                test_memo[18][num].push_back(nums);
                 for (i = 0; i < 16; ++i)
-                    test_memo[20 + i / 4][test_data[nums][70 + i]].push_back(nums);
+                    test_memo[19 + i / 4][test_data[nums][70 + i]].push_back(nums);
                 test_scores.push_back(0);
                 pre_calc_scores.push_back(0);
                 ++n_data_score[score + 64];
@@ -217,10 +236,9 @@ void input_test_data(int argc, char *argv[]){
                     ++n_data_idx[pattern_nums[i]][test_data[nums][i]][score + 64];
                 ++n_data_idx[16][sur][score + 64];
                 ++n_data_idx[17][canput][score + 64];
-                ++n_data_idx[18][stab][score + 64];
-                ++n_data_idx[19][num][score + 64];
+                ++n_data_idx[18][num][score + 64];
                 for (i = 0; i < 16; ++i)
-                    ++n_data_idx[20 + i / 4][test_data[nums][70 + i]][score + 64];
+                    ++n_data_idx[19 + i / 4][test_data[nums][70 + i]][score + 64];
                 /*
                 if (nums == 0){
                     for (i = 0; i < n_raw_params; ++i)
@@ -294,6 +312,8 @@ void output_param_onephase(){
     int pattern_idx, pattern_elem, dense_idx, canput, sur0, sur1, i, j;
     cerr << "=";
     for (pattern_idx = 0; pattern_idx < n_eval; ++pattern_idx){
+        if (12 <= pattern_idx && pattern_idx <= 15)
+            continue;
         for (pattern_elem = 0; pattern_elem < eval_sizes[pattern_idx]; ++pattern_elem){
             cout << round(eval_arr[sa_phase][pattern_idx][pattern_elem]) << endl;
         }
@@ -355,42 +375,41 @@ inline double calc_score(int phase, int i){
         eval_arr[phase][11][test_data[i][43]] + 
         eval_arr[phase][11][test_data[i][44]] + 
         eval_arr[phase][11][test_data[i][45]] + 
-        eval_arr[phase][12][test_data[i][46]] + 
-        eval_arr[phase][12][test_data[i][47]] + 
-        eval_arr[phase][12][test_data[i][48]] + 
-        eval_arr[phase][12][test_data[i][49]] + 
-        eval_arr[phase][13][test_data[i][50]] + 
-        eval_arr[phase][13][test_data[i][51]] + 
-        eval_arr[phase][13][test_data[i][52]] + 
-        eval_arr[phase][13][test_data[i][53]] + 
-        eval_arr[phase][14][test_data[i][54]] + 
-        eval_arr[phase][14][test_data[i][55]] + 
-        eval_arr[phase][14][test_data[i][56]] + 
-        eval_arr[phase][14][test_data[i][57]] + 
-        eval_arr[phase][15][test_data[i][58]] + 
-        eval_arr[phase][15][test_data[i][59]] + 
-        eval_arr[phase][15][test_data[i][60]] + 
-        eval_arr[phase][15][test_data[i][61]] + 
+        //eval_arr[phase][12][test_data[i][46]] + 
+        //eval_arr[phase][12][test_data[i][47]] + 
+        //eval_arr[phase][12][test_data[i][48]] + 
+        //eval_arr[phase][12][test_data[i][49]] + 
+        //eval_arr[phase][13][test_data[i][50]] + 
+        //eval_arr[phase][13][test_data[i][51]] + 
+        //eval_arr[phase][13][test_data[i][52]] + 
+        //eval_arr[phase][13][test_data[i][53]] + 
+        //eval_arr[phase][14][test_data[i][54]] + 
+        //eval_arr[phase][14][test_data[i][55]] + 
+        //eval_arr[phase][14][test_data[i][56]] + 
+        //eval_arr[phase][14][test_data[i][57]] + 
+        //eval_arr[phase][15][test_data[i][58]] + 
+        //eval_arr[phase][15][test_data[i][59]] + 
+        //eval_arr[phase][15][test_data[i][60]] + 
+        //eval_arr[phase][15][test_data[i][61]] + 
         eval_arr[phase][16][calc_sur0_sur1(test_data[i])] + 
         eval_arr[phase][17][calc_canput0_canput1(test_data[i])] + 
-        eval_arr[phase][18][calc_stab0_stab1(test_data[i])] + 
-        eval_arr[phase][19][calc_num0_num1(test_data[i])] + 
-        eval_arr[phase][20][test_data[i][70]] + 
-        eval_arr[phase][20][test_data[i][71]] + 
-        eval_arr[phase][20][test_data[i][72]] + 
-        eval_arr[phase][20][test_data[i][73]] + 
-        eval_arr[phase][21][test_data[i][74]] + 
-        eval_arr[phase][21][test_data[i][75]] + 
-        eval_arr[phase][21][test_data[i][76]] + 
-        eval_arr[phase][21][test_data[i][77]] + 
-        eval_arr[phase][22][test_data[i][78]] + 
-        eval_arr[phase][22][test_data[i][79]] + 
-        eval_arr[phase][22][test_data[i][80]] + 
-        eval_arr[phase][22][test_data[i][81]] + 
-        eval_arr[phase][23][test_data[i][82]] + 
-        eval_arr[phase][23][test_data[i][83]] + 
-        eval_arr[phase][23][test_data[i][84]] + 
-        eval_arr[phase][23][test_data[i][85]];
+        eval_arr[phase][18][calc_num0_num1(test_data[i])] + 
+        eval_arr[phase][19][test_data[i][70]] + 
+        eval_arr[phase][19][test_data[i][71]] + 
+        eval_arr[phase][19][test_data[i][72]] + 
+        eval_arr[phase][19][test_data[i][73]] + 
+        eval_arr[phase][20][test_data[i][74]] + 
+        eval_arr[phase][20][test_data[i][75]] + 
+        eval_arr[phase][20][test_data[i][76]] + 
+        eval_arr[phase][20][test_data[i][77]] + 
+        eval_arr[phase][21][test_data[i][78]] + 
+        eval_arr[phase][21][test_data[i][79]] + 
+        eval_arr[phase][21][test_data[i][80]] + 
+        eval_arr[phase][21][test_data[i][81]] + 
+        eval_arr[phase][22][test_data[i][82]] + 
+        eval_arr[phase][22][test_data[i][83]] + 
+        eval_arr[phase][22][test_data[i][84]] + 
+        eval_arr[phase][22][test_data[i][85]];
         /*
         + 
         eval_arr[phase][24][test_data[i][86]] + 
@@ -435,7 +454,7 @@ inline int calc_rev_idx(int pattern_idx, int pattern_size, int idx){
     if (pattern_idx <= 7 || pattern_idx == 12){
         for (int i = 0; i < pattern_size; ++i)
             res += pow3[i] * calc_pop(idx, i, pattern_size);
-    } else if (pattern_idx == 8){
+    } else if (pattern_idx == 8){ // triangle
         res += p39 * calc_pop(idx, 0, pattern_size);
         res += p38 * calc_pop(idx, 4, pattern_size);
         res += p37 * calc_pop(idx, 7, pattern_size);
@@ -446,7 +465,7 @@ inline int calc_rev_idx(int pattern_idx, int pattern_size, int idx){
         res += p32 * calc_pop(idx, 2, pattern_size);
         res += p31 * calc_pop(idx, 6, pattern_size);
         res += calc_pop(idx, 3, pattern_size);
-    } else if (pattern_idx == 9){
+    } else if (pattern_idx == 9){ // edge block
         res += p39 * calc_pop(idx, 5, pattern_size);
         res += p38 * calc_pop(idx, 4, pattern_size);
         res += p37 * calc_pop(idx, 3, pattern_size);
@@ -457,7 +476,7 @@ inline int calc_rev_idx(int pattern_idx, int pattern_size, int idx){
         res += p32 * calc_pop(idx, 8, pattern_size);
         res += p31 * calc_pop(idx, 7, pattern_size);
         res += calc_pop(idx, 6, pattern_size);
-    } else if (pattern_idx == 10){
+    } else if (pattern_idx == 10){ // cross
         res += p39 * calc_pop(idx, 0, pattern_size);
         res += p38 * calc_pop(idx, 1, pattern_size);
         res += p37 * calc_pop(idx, 2, pattern_size);
@@ -468,7 +487,7 @@ inline int calc_rev_idx(int pattern_idx, int pattern_size, int idx){
         res += p32 * calc_pop(idx, 4, pattern_size);
         res += p31 * calc_pop(idx, 5, pattern_size);
         res += calc_pop(idx, 6, pattern_size);
-    } else if (pattern_idx == 11){
+    } else if (pattern_idx == 11){ // corner9
         res += p38 * calc_pop(idx, 0, pattern_size);
         res += p37 * calc_pop(idx, 3, pattern_size);
         res += p36 * calc_pop(idx, 6, pattern_size);
@@ -478,7 +497,7 @@ inline int calc_rev_idx(int pattern_idx, int pattern_size, int idx){
         res += p32 * calc_pop(idx, 2, pattern_size);
         res += p31 * calc_pop(idx, 5, pattern_size);
         res += calc_pop(idx, 8, pattern_size);
-    } else if (pattern_idx == 13){
+    } else if (pattern_idx == 13){ // narrow triangle
         res += p39 * calc_pop(idx, 0, pattern_size);
         res += p38 * calc_pop(idx, 5, pattern_size);
         res += p37 * calc_pop(idx, 7, pattern_size);
@@ -489,7 +508,7 @@ inline int calc_rev_idx(int pattern_idx, int pattern_size, int idx){
         res += p32 * calc_pop(idx, 2, pattern_size);
         res += p31 * calc_pop(idx, 3, pattern_size);
         res += calc_pop(idx, 4, pattern_size);
-    } else if (pattern_idx == 14){
+    } else if (pattern_idx == 14){ // fish
         res += p39 * calc_pop(idx, 0, pattern_size);
         res += p38 * calc_pop(idx, 2, pattern_size);
         res += p37 * calc_pop(idx, 1, pattern_size);
@@ -500,7 +519,7 @@ inline int calc_rev_idx(int pattern_idx, int pattern_size, int idx){
         res += p32 * calc_pop(idx, 7, pattern_size);
         res += p31 * calc_pop(idx, 5, pattern_size);
         res += calc_pop(idx, 9, pattern_size);
-    } else if (pattern_idx == 15){
+    } else if (pattern_idx == 15){ // kite
         res += p39 * calc_pop(idx, 0, pattern_size);
         res += p38 * calc_pop(idx, 2, pattern_size);
         res += p37 * calc_pop(idx, 1, pattern_size);
@@ -511,7 +530,7 @@ inline int calc_rev_idx(int pattern_idx, int pattern_size, int idx){
         res += p32 * calc_pop(idx, 4, pattern_size);
         res += p31 * calc_pop(idx, 5, pattern_size);
         res += calc_pop(idx, 6, pattern_size);
-    } else if (pattern_idx >= n_patterns + 4){
+    } else if (pattern_idx >= n_patterns + 3){
         for (int i = 0; i < 8; ++i){
             res |= (1 & (idx >> i)) << (HW_M1 - i);
             res |= (1 & (idx >> (HW + i))) << (HW + HW_M1 - i);
@@ -520,24 +539,7 @@ inline int calc_rev_idx(int pattern_idx, int pattern_size, int idx){
     return res;
 }
 
-inline void calc_score(){
-    for (int i = 0; i < nums; ++i)
-        pre_calc_scores[i] = calc_score(sa_phase, i);
-}
-
-inline double mse(){
-    int i, j, score;
-    double avg_score, res = 0.0, err;
-    avg_score = 0;
-    for (i = 0; i < nums; ++i){
-        score = pre_calc_scores[i];
-        err = (test_labels[i] - (double)score) / step;
-        avg_score += err / nums * err;
-    }
-    return avg_score;
-}
-
-inline double mae(){
+inline void scoring_mae(){
     int i, j, score;
     double avg_score, res = 0.0;
     avg_score = 0;
@@ -545,7 +547,77 @@ inline double mae(){
         score = pre_calc_scores[i];
         avg_score += fabs(test_labels[i] - (double)score) / nums;
     }
-    return avg_score / step;
+    cerr << " " << avg_score << " " << avg_score / step << "                   ";
+}
+
+inline void scoring_mae_stdout(){
+    int i, j, score;
+    double avg_score, res = 0.0;
+    avg_score = 0;
+    for (i = 0; i < nums; ++i){
+        score = pre_calc_scores[i];
+        avg_score += fabs(test_labels[i] - (double)score) / nums;
+    }
+    cout << " " << avg_score << " " << avg_score / step;
+}
+
+inline double scoring_next_step(int pattern, int idx){
+    double score, res = 0.0, err;
+    int data_size = nums;
+    for (const int &i: test_memo[pattern][idx]){
+        score = pre_calc_scores[i];
+        err = (test_labels[i] - score) * bias[(int)test_labels[i] / step + 64];
+        res += err;
+    }
+    return res;
+}
+
+inline void next_step(){
+    int pattern, rev_idx;
+    double err;
+    for (int i = 0; i < nums; ++i)
+        pre_calc_scores[i] = calc_score(sa_phase, i);
+    for (pattern = 0; pattern < n_eval; ++pattern){
+        for (const int &idx: used_idxes_vector[pattern]){
+            if (pattern < n_patterns || n_patterns + 3 <= pattern){
+                rev_idx = rev_idxes[pattern][idx];
+                if (idx < rev_idx){
+                    err = scoring_next_step(pattern, idx) + scoring_next_step(pattern, rev_idx);
+                    eval_arr[sa_phase][pattern][idx] += 2.0 * alpha[pattern][idx] * err;
+                    eval_arr[sa_phase][pattern][rev_idx] += 2.0 * alpha[pattern][idx] * err;
+                } else if (idx == rev_idx){
+                    err = scoring_next_step(pattern, idx);
+                    eval_arr[sa_phase][pattern][idx] += 2.0 * alpha[pattern][idx] * err;
+                }
+            } else{
+                err = scoring_next_step(pattern, idx);
+                eval_arr[sa_phase][pattern][idx] += 2.0 * alpha[pattern][idx] * err;
+            }
+        }
+    }
+}
+
+void sd(unsigned long long tl){
+    cerr << "alpha " << alpha << endl;
+    unsigned long long strt = tim(), now = tim();
+    int t = 0;
+    for (;;){
+        ++t;
+        next_step();
+        if ((t & 0b0) == 0){
+            now = tim();
+            if (now - strt > tl)
+                break;
+            cerr << "\r " << t << " " << (int)((double)(now - strt) / tl * 1000);
+            scoring_mae();
+        }
+    }
+    cerr << t;
+    scoring_mae();
+    cerr << endl;
+    cout << t << " " << (now - strt) / 1000;
+    scoring_mae_stdout();
+    cout << endl;
 }
 
 void init(){
@@ -563,6 +635,11 @@ void init(){
 }
 
 int main(int argc, char *argv[]){
+    sa_phase = atoi(argv[1]);
+    hour = atoi(argv[2]);
+    minute = atoi(argv[3]);
+    second = atoi(argv[4]);
+    beta = atof(argv[5]);
     int i, j;
 
     minute += hour * 60;
@@ -573,14 +650,13 @@ int main(int argc, char *argv[]){
     board_init();
     init();
     initialize_param();
-    input_param();
+    //output_param_onephase();
+    //input_param_onephase((string)(argv[6]));
+    input_test_data(argc, argv);
 
-    for (sa_phase = 0; sa_phase < N_PHASES; ++sa_phase){
-        input_test_data(argc, argv);
-        calc_score();
-        cerr << "phase " << sa_phase << " mse " << mse() << " mae " << mae() << endl;
-        cout << "phase " << sa_phase << " mse " << mse() << " mae " << mae() << endl;
-    }
+    sd(second * 1000);
+
+    output_param_onephase();
 
     return 0;
 }
--- evaluation/learn_all.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-for use_phase in reversed(range(30)):
-    if use_phase < 10:
-        subprocess.run('python learn.py ' + str(use_phase) + ' data_0000001.dat data_0000002.dat data_0000003.dat')
-    else:
-        subprocess.run('python learn.py ' + str(use_phase) + ' data_0000004.dat data_0000006.dat data_0000007.dat data_0000008.dat data_0000009.dat data_0000010.dat')
-    exit()
