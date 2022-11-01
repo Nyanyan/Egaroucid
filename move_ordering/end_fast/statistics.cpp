cc7ab2e9bab91adc8ac93768201bfb37a04cbaab
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-11-02 01:54:58 +0900

    add move ordering statistics

---
+++ move_ordering/end_fast/statistics.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <ios>
+#include "util/board.hpp"
+
+using namespace std;
+
+#define N_DIRS 1
+
+int N_EMPTIES_MIN = 5;
+int N_EMPTIES_MAX = 7;
+
+#define N_DIGIT 7
+
+#define INDENT "  "
+
+void print_arr_64(uint64_t board_arr[], string head){
+    for (int i = 0; i < HW; ++i){
+        cout << head;
+        for (int j = 0; j < HW; ++j)
+            cout << right << setw(N_DIGIT) << board_arr[i * HW + j] << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void print_arr_10(uint64_t board_arr[], string head){
+    uint64_t arr10[10];
+    constexpr int cells[10][8] = {
+        {0, 7, 56, 63, -1},
+        {1, 6, 8, 15, 48, 55, 57, 62},
+        {2, 5, 23, 16, 40, 47, 58, 61},
+        {3, 4, 24, 31, 32, 39, 59, 60},
+        {9, 14, 49, 54, -1},
+        {10, 13, 17, 22, 41, 46, 50, 53},
+        {11, 12, 25, 30, 33, 38, 51, 52},
+        {18, 21, 42, 45, -1},
+        {19, 20, 26, 29, 34, 37, 43, 44},
+        {27, 28, 35, 36, -1}
+    };
+    for (int i = 0; i < 10; ++i){
+        arr10[i] = 0ULL;
+        for (int j = 0; j < 8; ++j){
+            if (cells[i][j] == -1)
+                break;
+            arr10[i] += board_arr[cells[i][j]];
+        }
+    }
+    int t = 0;
+    for (int i = 0; i < 4; ++i){
+        cout << head;
+        for (int j = 0; j < i; ++j){
+            for (int k = 0; k < N_DIGIT + 1; ++k)
+                cout << " ";
+        }
+        for (int j = 4 - i; j > 0; --j)
+            cout << right << setw(N_DIGIT) << arr10[t++] << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void print_arr(uint64_t board_arr[], string head){
+    cout << head << "64 cells: " << endl;
+    print_arr_64(board_arr, head + INDENT);
+    cout << head << "10 cells: " << endl;
+    print_arr_10(board_arr, head + INDENT);
+    cout << endl;
+}
+
+struct Data{
+    uint64_t n_data;
+    uint64_t places[HW2];
+    uint64_t parities[4];
+
+
+    Data(){
+        n_data = 0;
+        for (int i = 0; i < HW2; ++i)
+            places[i] = 0;
+        for (int i = 0; i < 4; ++i)
+            parities[i] = 0;
+    }
+
+    void print(){
+        cout << "n_data: " << n_data << endl;
+
+        cout << "places: " << endl;
+        print_arr(places, INDENT);
+
+        cout << "parities: " << endl;
+        cout << INDENT << "even:       " << parities[0] << endl;
+        cout << INDENT << "odd:        " << parities[1] << endl;
+        cout << INDENT << "even only:  " << parities[2] << endl;
+        cout << INDENT << "odd only:   " << parities[3] << endl;
+
+        cout << endl << "done" << endl;
+    }
+};
+
+void add_data(Data *data, string line){
+    Board board;
+    board.player = 0ULL;
+    board.opponent = 0ULL;
+    uint64_t p = 0ULL, o = 0ULL;
+    for (int i = 0; i < HW2; ++i){
+        if (line[i] == 'p')
+            board.player |= 1ULL << i;
+        else if (line[i] == 'o')
+            board.opponent |= 1ULL << i;
+    }
+    int n_discs = pop_count_ull(board.player | board.opponent);
+    if (HW2 - N_EMPTIES_MAX <= n_discs && n_discs <= HW2 - N_EMPTIES_MIN){
+        uint64_t empty = ~(board.player | board.opponent);
+        int parity = 1 & pop_count_ull(empty & 0x000000000F0F0F0FULL);
+        parity |= (1 & pop_count_ull(empty & 0x00000000F0F0F0F0ULL)) << 1;
+        parity |= (1 & pop_count_ull(empty & 0x0F0F0F0F00000000ULL)) << 2;
+        parity |= (1 & pop_count_ull(empty & 0xF0F0F0F000000000ULL)) << 3;
+        
+        int policy = line[HW2] - '!';
+        int score = line[HW2 + 1] - '!';
+        score *= 2;
+        score -= HW2;
+
+        ++data->n_data;
+        ++data->places[policy];
+        if (parity == 0)
+            ++data->parities[2]; // even only
+        if (parity == 0b1111)
+            ++data->parities[3]; // odd only
+        else if (parity & cell_div4[policy])
+            ++data->parities[1]; // odd
+        else
+            ++data->parities[0]; // even
+    }
+}
+
+int main(int argc, char *argv[]){
+    bit_init();
+    board_init();
+    flip_init();
+
+    if (argc >= 3){
+        N_EMPTIES_MIN = atoi(argv[1]);
+        N_EMPTIES_MAX = atoi(argv[1]);
+    }
+    cerr << N_EMPTIES_MIN << " empties to " << N_EMPTIES_MAX << " empties" << endl;
+    string data_dir = "data/";
+    string sub_dirs[N_DIRS] = {
+        "records15/"
+    };
+    int n_data[N_DIRS] = {
+        1
+    };
+    Data data;
+    for (int dir_idx = 0; dir_idx < N_DIRS; ++dir_idx){
+        for (int file_idx = 0; file_idx < n_data[dir_idx]; ++file_idx){
+            ostringstream sout;
+            sout << setfill('0') << setw(7) << file_idx;
+            string file_name = sout.str();
+            ifstream ifs(data_dir + sub_dirs[dir_idx] + file_name + ".txt");
+            string line;
+            while (getline(ifs, line)){
+                add_data(&data, line);
+            }
+        }
+    }
+    data.print();
+    return 0;
+}
---
+++ move_ordering/end_fast/util/flip.hpp
@@ -0,0 +1,415 @@
+#pragma once
+#include <iostream>
+#include "setting.hpp"
+#include "common.hpp"
+#include "bit.hpp"
+
+using namespace std;
+
+uint8_t flip_pre_calc[N_8BIT][N_8BIT][HW];
+uint8_t n_flip_pre_calc[N_8BIT][N_8BIT][HW];
+
+class Flip{
+    public:
+        uint_fast8_t pos;
+        uint64_t flip;
+        int32_t value;
+    
+    public:
+        inline void copy(Flip *mob) const{
+            mob->pos = pos;
+            mob->flip = flip;
+            mob->value = value;
+        }
+
+        #if FLIP_CALC_MODE == 0
+            inline void calc_flip(const uint64_t player, const uint64_t opponent, const uint_fast8_t place){
+                uint64_t wh, put, m1, m2, m3, m4, m5, m6, rev;
+                put = 1ULL << place;
+                rev = 0;
+
+                wh = opponent & 0b0111111001111110011111100111111001111110011111100111111001111110ULL;
+                m1 = put >> 1;
+                if( (m1 & wh) != 0 ) {
+                    if( ((m2 = m1 >> 1) & wh) == 0  ) {
+                        if( (m2 & player) != 0 )
+                            rev |= m1;
+                    } else if( ((m3 = m2 >> 1) & wh) == 0 ) {
+                        if( (m3 & player) != 0 )
+                            rev |= m1 | m2;
+                    } else if( ((m4 = m3 >> 1) & wh) == 0 ) {
+                        if( (m4 & player) != 0 )
+                            rev |= m1 | m2 | m3;
+                    } else if( ((m5 = m4 >> 1) & wh) == 0 ) {
+                        if( (m5 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4;
+                    } else if( ((m6 = m5 >> 1) & wh) == 0 ) {
+                        if( (m6 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5;
+                    } else {
+                        if( ((m6 >> 1) & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5 | m6;
+                    }
+                }
+                m1 = put << 1;
+                if( (m1 & wh) != 0 ) {
+                    if( ((m2 = m1 << 1) & wh) == 0  ) {
+                        if( (m2 & player) != 0 )
+                            rev |= m1;
+                    } else if( ((m3 = m2 << 1) & wh) == 0 ) {
+                        if( (m3 & player) != 0 )
+                            rev |= m1 | m2;
+                    } else if( ((m4 = m3 << 1) & wh) == 0 ) {
+                        if( (m4 & player) != 0 )
+                            rev |= m1 | m2 | m3;
+                    } else if( ((m5 = m4 << 1) & wh) == 0 ) {
+                        if( (m5 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4;
+                    } else if( ((m6 = m5 << 1) & wh) == 0 ) {
+                        if( (m6 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5;
+                    } else {
+                        if( ((m6 << 1) & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5 | m6;
+                    }
+                }
+
+                wh = opponent & 0b0000000011111111111111111111111111111111111111111111111100000000ULL;
+                m1 = put >> HW;
+                if( (m1 & wh) != 0 ) {
+                    if( ((m2 = m1 >> HW) & wh) == 0  ) {
+                        if( (m2 & player) != 0 )
+                            rev |= m1;
+                    } else if( ((m3 = m2 >> HW) & wh) == 0 ) {
+                        if( (m3 & player) != 0 )
+                            rev |= m1 | m2;
+                    } else if( ((m4 = m3 >> HW) & wh) == 0 ) {
+                        if( (m4 & player) != 0 )
+                            rev |= m1 | m2 | m3;
+                    } else if( ((m5 = m4 >> HW) & wh) == 0 ) {
+                        if( (m5 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4;
+                    } else if( ((m6 = m5 >> HW) & wh) == 0 ) {
+                        if( (m6 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5;
+                    } else {
+                        if( ((m6 >> HW) & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5 | m6;
+                    }
+                }
+                m1 = put << HW;
+                if( (m1 & wh) != 0 ) {
+                    if( ((m2 = m1 << HW) & wh) == 0  ) {
+                        if( (m2 & player) != 0 )
+                            rev |= m1;
+                    } else if( ((m3 = m2 << HW) & wh) == 0 ) {
+                        if( (m3 & player) != 0 )
+                            rev |= m1 | m2;
+                    } else if( ((m4 = m3 << HW) & wh) == 0 ) {
+                        if( (m4 & player) != 0 )
+                            rev |= m1 | m2 | m3;
+                    } else if( ((m5 = m4 << HW) & wh) == 0 ) {
+                        if( (m5 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4;
+                    } else if( ((m6 = m5 << HW) & wh) == 0 ) {
+                        if( (m6 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5;
+                    } else {
+                        if( ((m6 << HW) & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5 | m6;
+                    }
+                }
+
+                wh = opponent & 0b0000000001111110011111100111111001111110011111100111111000000000ULL;
+                m1 = put >> (HW - 1);
+                if( (m1 & wh) != 0 ) {
+                    if( ((m2 = m1 >> (HW - 1)) & wh) == 0  ) {
+                        if( (m2 & player) != 0 )
+                            rev |= m1;
+                    } else if( ((m3 = m2 >> (HW - 1)) & wh) == 0 ) {
+                        if( (m3 & player) != 0 )
+                            rev |= m1 | m2;
+                    } else if( ((m4 = m3 >> (HW - 1)) & wh) == 0 ) {
+                        if( (m4 & player) != 0 )
+                            rev |= m1 | m2 | m3;
+                    } else if( ((m5 = m4 >> (HW - 1)) & wh) == 0 ) {
+                        if( (m5 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4;
+                    } else if( ((m6 = m5 >> (HW - 1)) & wh) == 0 ) {
+                        if( (m6 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5;
+                    } else {
+                        if( ((m6 >> (HW - 1)) & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5 | m6;
+                    }
+                }
+                m1 = put << (HW - 1);
+                if( (m1 & wh) != 0 ) {
+                    if( ((m2 = m1 << (HW - 1)) & wh) == 0  ) {
+                        if( (m2 & player) != 0 )
+                            rev |= m1;
+                    } else if( ((m3 = m2 << (HW - 1)) & wh) == 0 ) {
+                        if( (m3 & player) != 0 )
+                            rev |= m1 | m2;
+                    } else if( ((m4 = m3 << (HW - 1)) & wh) == 0 ) {
+                        if( (m4 & player) != 0 )
+                            rev |= m1 | m2 | m3;
+                    } else if( ((m5 = m4 << (HW - 1)) & wh) == 0 ) {
+                        if( (m5 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4;
+                    } else if( ((m6 = m5 << (HW - 1)) & wh) == 0 ) {
+                        if( (m6 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5;
+                    } else {
+                        if( ((m6 << (HW - 1)) & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5 | m6;
+                    }
+                }
+
+                m1 = put >> (HW + 1);
+                if( (m1 & wh) != 0 ) {
+                    if( ((m2 = m1 >> (HW + 1)) & wh) == 0  ) {
+                        if( (m2 & player) != 0 )
+                            rev |= m1;
+                    } else if( ((m3 = m2 >> (HW + 1)) & wh) == 0 ) {
+                        if( (m3 & player) != 0 )
+                            rev |= m1 | m2;
+                    } else if( ((m4 = m3 >> (HW + 1)) & wh) == 0 ) {
+                        if( (m4 & player) != 0 )
+                            rev |= m1 | m2 | m3;
+                    } else if( ((m5 = m4 >> (HW + 1)) & wh) == 0 ) {
+                        if( (m5 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4;
+                    } else if( ((m6 = m5 >> (HW + 1)) & wh) == 0 ) {
+                        if( (m6 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5;
+                    } else {
+                        if( ((m6 >> (HW + 1)) & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5 | m6;
+                    }
+                }
+                m1 = put << (HW + 1);
+                if( (m1 & wh) != 0 ) {
+                    if( ((m2 = m1 << (HW + 1)) & wh) == 0  ) {
+                        if( (m2 & player) != 0 )
+                            rev |= m1;
+                    } else if( ((m3 = m2 << (HW + 1)) & wh) == 0 ) {
+                        if( (m3 & player) != 0 )
+                            rev |= m1 | m2;
+                    } else if( ((m4 = m3 << (HW + 1)) & wh) == 0 ) {
+                        if( (m4 & player) != 0 )
+                            rev |= m1 | m2 | m3;
+                    } else if( ((m5 = m4 << (HW + 1)) & wh) == 0 ) {
+                        if( (m5 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4;
+                    } else if( ((m6 = m5 << (HW + 1)) & wh) == 0 ) {
+                        if( (m6 & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5;
+                    } else {
+                        if( ((m6 << (HW + 1)) & player) != 0 )
+                            rev |= m1 | m2 | m3 | m4 | m5 | m6;
+                    }
+                }
+
+                flip = rev;
+                pos = place;
+            }
+            
+        #elif FLIP_CALC_MODE == 1
+            inline void calc_flip(const uint64_t player, const uint64_t opponent, const uint_fast8_t place){
+                int_fast8_t t, u;
+                uint8_t p, o;
+                flip = 0;
+                pos = place;
+
+                t = place / HW;
+                u = place % HW;
+                p = (player >> (HW * t)) & 0b11111111;
+                o = (opponent >> (HW * t)) & 0b11111111;
+                flip |= (uint64_t)flip_pre_calc[p][o][u] << (HW * t);
+
+                p = join_v_line(player, u);
+                o = join_v_line(opponent, u);
+                flip |= split_v_lines[flip_pre_calc[p][o][t]] << u;
+
+                u += t;
+                if (u >= 2 && u <= 12){
+                    p = join_d7_line(player, u) & d7_mask[place];
+                    o = join_d7_line(opponent, u) & d7_mask[place];
+                    flip |= split_d7_lines[flip_pre_calc[p][o][HW_M1 - t]][u - 2];
+                }
+
+                u -= t * 2;
+                if (u >= -5 && u <= 5){
+                    p = join_d9_line(player, u) & d9_mask[place];
+                    o = join_d9_line(opponent, u) & d9_mask[place];
+                    flip |= split_d9_lines[flip_pre_calc[p][o][t]][u + 5];
+                }
+            }
+        #elif FLIP_CALC_MODE == 2
+            inline void calc_flip(const uint64_t player, const uint64_t opponent, const uint_fast8_t place){
+                int_fast8_t t, u;
+                uint8_t p, o;
+                flip = 0;
+                pos = place;
+
+                t = place / HW;
+                u = place % HW;
+                p = (player >> (HW * t)) & 0b11111111;
+                o = (opponent >> (HW * t)) & 0b11111111;
+                flip |= (uint64_t)flip_pre_calc[p][o][u] << (HW * t);
+
+                p = join_v_line(player, u);
+                o = join_v_line(opponent, u);
+                flip |= split_v_lines[flip_pre_calc[p][o][t]] << u;
+
+                u += t;
+                if (u >= 2 && u <= 12){
+                    p = join_d7_lines[u - 2](player);
+                    o = join_d7_lines[u - 2](opponent);
+                    flip |= split_d7_lines[flip_pre_calc[p][o][HW_M1 - t]][u - 2];
+                }
+
+                u -= t * 2;
+                if (u >= -5 && u <= 5){
+                    p = join_d9_lines[u + 5](player);
+                    o = join_d9_lines[u + 5](opponent);
+                    flip |= split_d9_lines[flip_pre_calc[p][o][t]][u + 5];
+                }
+            }
+        #endif
+};
+
+#if LAST_FLIP_CALC_MODE == 0
+
+    inline int_fast8_t count_last_flip(uint64_t player, uint64_t opponent, const uint_fast8_t place){
+        int_fast8_t t, u;
+        uint8_t p, o;
+        int_fast8_t res = 0;
+
+        t = place / HW;
+        u = place % HW;
+        p = (player >> (HW * t)) & 0b11111111;
+        o = (opponent >> (HW * t)) & 0b11111111;
+        res += n_flip_pre_calc[p][o][u];
+
+        p = join_v_line(player, u);
+        o = join_v_line(opponent, u);
+        res += n_flip_pre_calc[p][o][t];
+
+        t = place / HW;
+        u = place % HW + t;
+        if (u >= 2 && u <= 12){
+            p = join_d7_line(player, u) & d7_mask[place];
+            o = join_d7_line(opponent, u) & d7_mask[place];
+            res += pop_count_uchar(flip_pre_calc[p][o][HW_M1 - t] & d7_mask[place]);
+        }
+
+        u -= t * 2;
+        if (u >= -5 && u <= 5){
+            p = join_d9_line(player, u) & d9_mask[place];
+            o = join_d9_line(opponent, u) & d9_mask[place];
+            res += pop_count_uchar(flip_pre_calc[p][o][t] & d9_mask[place]);
+        }
+        return res;
+    }
+
+#elif LAST_FLIP_CALC_MODE == 1
+
+    inline int_fast8_t count_last_flip(uint64_t player, uint64_t opponent, const uint_fast8_t place){
+        int_fast8_t t, u;
+        uint8_t p, o;
+        int_fast8_t res = 0;
+
+        t = place / HW;
+        u = place % HW;
+        p = (player >> (HW * t)) & 0b11111111;
+        o = (opponent >> (HW * t)) & 0b11111111;
+        res += n_flip_pre_calc[p][o][u];
+
+        p = join_v_line(player, u);
+        o = join_v_line(opponent, u);
+        res += n_flip_pre_calc[p][o][t];
+
+        t = place / HW;
+        u = place % HW + t;
+        if (u >= 2 && u <= 12){
+            p = join_d7_lines[u - 2](player);
+            o = join_d7_lines[u - 2](opponent);
+            res += pop_count(flip_pre_calc[p][o][HW_M1 - t] & d7_mask[place]);
+        }
+
+        u -= t * 2;
+        if (u >= -5 && u <= 5){
+            p = join_d9_lines[u + 5](player);
+            o = join_d9_lines[u + 5](opponent);
+            res += pop_count(flip_pre_calc[p][o][t] & d9_mask[place]);
+        }
+        return res;
+    }
+
+#endif
+
+void flip_init(){
+    int player, opponent, place;
+    int wh, put, m1, m2, m3, m4, m5, m6, i;
+    for (player = 0; player < N_8BIT; ++player){
+        for (opponent = 0; opponent < N_8BIT; ++opponent){
+            for (place = 0; place < HW; ++place){
+                flip_pre_calc[player][opponent][place] = 0;
+                n_flip_pre_calc[player][opponent][place] = 0;
+                if ((1 & (player >> place)) == 0 && (1 & (opponent >> place)) == 0 && (player & opponent) == 0){
+                    put = 1 << place;
+                    wh = opponent & 0b01111110;
+                    m1 = put >> 1;
+                    if( (m1 & wh) != 0 ) {
+                        if( ((m2 = m1 >> 1) & wh) == 0  ) {
+                            if( (m2 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1;
+                        } else if( ((m3 = m2 >> 1) & wh) == 0 ) {
+                            if( (m3 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2;
+                        } else if( ((m4 = m3 >> 1) & wh) == 0 ) {
+                            if( (m4 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2 | m3;
+                        } else if( ((m5 = m4 >> 1) & wh) == 0 ) {
+                            if( (m5 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2 | m3 | m4;
+                        } else if( ((m6 = m5 >> 1) & wh) == 0 ) {
+                            if( (m6 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2 | m3 | m4 | m5;
+                        } else {
+                            if( ((m6 >> 1) & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2 | m3 | m4 | m5 | m6;
+                        }
+                    }
+                    m1 = put << 1;
+                    if( (m1 & wh) != 0 ) {
+                        if( ((m2 = m1 << 1) & wh) == 0  ) {
+                            if( (m2 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1;
+                        } else if( ((m3 = m2 << 1) & wh) == 0 ) {
+                            if( (m3 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2;
+                        } else if( ((m4 = m3 << 1) & wh) == 0 ) {
+                            if( (m4 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2 | m3;
+                        } else if( ((m5 = m4 << 1) & wh) == 0 ) {
+                            if( (m5 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2 | m3 | m4;
+                        } else if( ((m6 = m5 << 1) & wh) == 0 ) {
+                            if( (m6 & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2 | m3 | m4 | m5;
+                        } else {
+                            if( ((m6 << 1) & player) != 0 )
+                                flip_pre_calc[player][opponent][place] |= m1 | m2 | m3 | m4 | m5 | m6;
+                        }
+                    }
+                    n_flip_pre_calc[player][opponent][place] = 0;
+                    for (i = 1; i < HW_M1; ++i)
+                        n_flip_pre_calc[player][opponent][place] += 1 & (flip_pre_calc[player][opponent][place] >> i);
+                }
+            }
+        }
+    }
+}
