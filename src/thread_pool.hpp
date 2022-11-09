439af191d3e156f814109d0b658355707f153bbb
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-11-09 21:06:54 +0900

    thread_pool.hpp updated

--- src/thread_pool.hpp
+++ src/thread_pool.hpp
@@ -1,6 +1,8 @@
 /*
     Egaroucid Project
 
+    @file thread_pool.hpp
+        Thread pool for Egaroucid
     @date 2021-2022
     @author Takuto Yamana (a.k.a. Nyanyan)
     @license GPL-3.0 license
@@ -11,7 +13,5 @@
 #include "setting.hpp"
 #include "board.hpp"
 #include "thread_pool/thread_pool.hpp"
-
-using namespace std;
 
 ctpl::thread_pool thread_pool(1);
