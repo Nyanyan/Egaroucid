2f3f0291c51626ff4db02d08c0104b929639ea0e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-11-09 14:17:56 +0900

    add all.hpp

---
+++ src/all.hpp
@@ -0,0 +1,14 @@
+/*
+    Egaroucid Project
+
+    @file all.hpp
+        Include all things about Egaroucid's AI
+    @date 2021-2022
+    @author Takuto Yamana (a.k.a Nyanyan)
+    @license GPL-3.0 license
+*/
+
+#pragma once
+#include "ai.hpp"
+#include "book_enlarge.hpp"
+#include "umigame.hpp"
