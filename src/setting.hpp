d5be9f7eb75c028e7c93fc9022192e16ff15d47b
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-11-09 23:11:14 +0900

    update setting.hpp

--- src/setting.hpp
+++ src/setting.hpp
@@ -8,20 +8,14 @@
     @license GPL-3.0 license
 */
 
+/*
+    @brief performance settings
+*/
 // use SIMD
 #define USE_SIMD true
 
 // vertical mirror
 #define USE_FAST_VERTICAL_MIRROR true
-
-// parity ordering
-#define USE_END_PO true
-
-// stability cut
-#define USE_END_SC true
-
-// prob cut
-#define USE_MID_MPC true
 
 // pop_count
 #define USE_BUILTIN_POPCOUNT true
@@ -33,6 +27,28 @@
 // last parity ordering optimization
 #define LAST_PO_OPTIMIZE true
 
+// use parallel clog search
+#define USE_PARALLEL_CLOG_SEARCH true
+
+// use SIMD in evaluation (pattern) function
+#define USE_SIMD_EVALUATION true
+
+// use bit gather optimization
+#define USE_BIT_GATHER_OPTIMIZE true
+
+
+/*
+    @brief search settings
+*/
+// parity ordering
+#define USE_END_PO true
+
+// stability cut
+#define USE_END_SC true
+
+// Multi-ProbCut
+#define USE_MID_MPC true
+
 // use probcut to predict it seems to be an all node
 #define USE_ALL_NODE_PREDICTION false
 
@@ -41,19 +57,16 @@
 #define USE_NEGA_ALPHA_END false
 #define USE_NEGA_ALPHA_END_FAST false
 
-// use parallel clog search
-#define USE_PARALLEL_CLOG_SEARCH true
-
-// use SIMD in evaluation (pattern) function
-#define USE_SIMD_EVALUATION true
-#define USE_SIMD_DEBUG false
-
 // move ordering
 #define USE_AUTO_OPTIMIZED_MOVE_ORDERING_MID true
 #define USE_AUTO_OPTIMIZED_MOVE_ORDERING_NWS true
 
-// use bit gather optimization
-#define USE_BIT_GATHER_OPTIMIZE true
+
+/*
+    @brief debug settings
+*/
+// use SIMD in evaluation (pattern) function
+#define USE_SIMD_DEBUG false
 
 // evaluation harness
 #define USE_EVALUATION_HARNESS false
