dcd7b8fcd85624ff1d9a593a070a9a1aaf120091
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-11-09 17:16:32 +0900

    mobility.hpp updated

--- src/mobility.hpp
+++ src/mobility.hpp
@@ -1,6 +1,8 @@
 /*
     Egaroucid Project
 
+    @file mobility.hpp
+        Calculate legal moves
     @date 2021-2022
     @author Takuto Yamana (a.k.a. Nyanyan)
     @license GPL-3.0 license
@@ -12,13 +14,28 @@
 #include "bit.hpp"
 #include "setting.hpp"
 
+
+/*
+    @brief mobility initialize
+*/
+__m256i shift1897, mflipH;
+void mobility_init(){
+    shift1897 = _mm256_set_epi64x(7, 9, 8, 1);
+    mflipH = _mm256_set_epi64x(0x7E7E7E7E7E7E7E7E, 0x7E7E7E7E7E7E7E7E, -1, 0x7E7E7E7E7E7E7E7E);
+}
+
+/*
+    @brief Get a bitboard representing all legal moves
+
+    @param P                    a bitboard representing player
+    @param O                    a bitboard representing opponent
+    @return all legal moves as a bitboard
+*/
 // original code from http://www.amy.hi-ho.ne.jp/okuhara/bitboard.htm
 // modified by Nyanyan
 inline uint64_t calc_legal(const uint64_t P, const uint64_t O){
     __m256i	PP, mOO, MM, flip_l, flip_r, pre_l, pre_r, shift2;
     __m128i	M;
-    const __m256i shift1897 = _mm256_set_epi64x(7, 9, 8, 1);
-    const __m256i mflipH = _mm256_set_epi64x(0x7E7E7E7E7E7E7E7E, 0x7E7E7E7E7E7E7E7E, -1, 0x7E7E7E7E7E7E7E7E);
     PP = _mm256_broadcastq_epi64(_mm_cvtsi64_si128(P));
     mOO = _mm256_and_si256(_mm256_broadcastq_epi64(_mm_cvtsi64_si128(O)), mflipH);
     flip_l = _mm256_and_si256(mOO, _mm256_sllv_epi64(PP, shift1897));
