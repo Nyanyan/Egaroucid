7bd9be7dd86773784e257d9d590492b648b99758
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-11-09 22:40:10 +0900

    util.hpp updated

--- src/util.hpp
+++ src/util.hpp
@@ -1,6 +1,8 @@
 /*
     Egaroucid Project
 
+    @file util.hpp
+        Utility
     @date 2021-2022
     @author Takuto Yamana (a.k.a. Nyanyan)
     @license GPL-3.0 license
@@ -10,15 +12,22 @@
 #include <iostream>
 #include "board.hpp"
 
+/*
+    @brief Input board from console
+
+    '0' as black, '1' as white, '.' as empty
+
+    @return board structure
+*/
 Board input_board(){
     Board res;
     char elem;
     int player;
-    cin >> player;
+    std::cin >> player;
     res.player = 0;
     res.opponent = 0;
     for (int i = 0; i < HW2; ++i){
-        cin >> elem;
+        std::cin >> elem;
         if (elem == '0'){
             if (player == BLACK)
                 res.player |= 1ULL << (HW2_M1 - i);
@@ -31,10 +40,15 @@ Board input_board(){
                 res.opponent |= 1ULL << (HW2_M1 - i);
         }
     }
-    //res.print();
     return res;
 }
 
+/*
+    @brief Generate coordinate in string
+
+    @param idx                  index of the coordinate
+    @return coordinate as string
+*/
 string idx_to_coord(int idx){
     int y = HW_M1 - idx / HW;
     int x = HW_M1 - idx % HW;
