669a65df084570e4794f17a076c0ce0c2bb6c984
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-11-10 02:02:55 +0900

    add console version file

---
+++ src/console/command.hpp
@@ -0,0 +1,9 @@
+/*
+    Egaroucid Project
+
+    @file command.hpp
+        Commands for console version
+    @date 2021-2022
+    @author Takuto Yamana (a.k.a. Nyanyan)
+    @license GPL-3.0 license
+*/
