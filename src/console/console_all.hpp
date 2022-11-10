dfc9cfcfa46d891b5aa08b4480f1961e40fc8d77
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-11-10 11:27:04 +0900

    files for console

---
+++ src/console/console_all.hpp
@@ -0,0 +1,12 @@
+/*
+    Egaroucid Project
+
+    @file console_all.hpp
+        Include all things about Egaroucid's console system
+    @date 2021-2022
+    @author Takuto Yamana (a.k.a. Nyanyan)
+    @license GPL-3.0 license
+*/
+
+#include "command.hpp"
+#include "option.hpp"
---
+++ src/console/option.hpp
@@ -0,0 +1,10 @@
+/*
+    Egaroucid Project
+
+    @file option.hpp
+        Commandline options
+    @date 2021-2022
+    @author Takuto Yamana (a.k.a. Nyanyan)
+    @license GPL-3.0 license
+*/
+
