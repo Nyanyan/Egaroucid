4aeaf9a9dd10e0bb25b18035868f740a710752c2
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-11-19 11:59:06 +0900

    comment

--- statistics/node_statistics.py
+++ statistics/node_statistics.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-s = '1519 15963 779063 5112562 33783053'
+s = '1519 15963 779063 5112562 33783053' # ffo42@single thread
 
 data = [int(elem) for elem in s.split()]
 data.append(sum(data))
