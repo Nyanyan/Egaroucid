3651e9271ad07ba40be042c3c5353efd2d640b64
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-12-18 13:06:32 +0900

    update get_binary_path for windows

--- src/console/console_common.hpp
+++ src/console/console_common.hpp
@@ -11,6 +11,7 @@
 #pragma once
 #include <iostream>
 #include <stdlib.h>
+#include <string>
 #ifdef _WIN64
     #include <windows.h>
 #endif
@@ -19,6 +20,17 @@
 #define MODE_AI_HUMAN 1
 #define MODE_AI_AI 2
 #define MODE_HUMAN_HUMAN 3
+
+std::string replace_backslash(std::string str){
+    std::string res;
+    for (int i = 0; i < (int)str.length(); ++i){
+        if (str[i] == '\\')
+            res += '/';
+        else
+            res += str[i];
+    }
+    return res;
+}
 
 #ifdef _WIN64
     std::string get_binary_path(){
@@ -33,6 +45,7 @@
             _splitpath_s(raw_path,drive,dir,fname,ext);
             res = drive;
             res += dir;
+            res = replace_backslash(res);
         }
         return res;
     }
