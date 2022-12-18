cfdddf97dce850284b6d0a236ce841f62b7f3d14
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-12-18 12:53:58 +0900

    updating get_binary_path

--- src/console/console_common.hpp
+++ src/console/console_common.hpp
@@ -8,8 +8,33 @@
     @license GPL-3.0 license
 */
 
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#ifdef _WIN64
+    #include <windows.h>
+#endif
 
 #define MODE_HUMAN_AI 0
 #define MODE_AI_HUMAN 1
 #define MODE_AI_AI 2
 #define MODE_HUMAN_HUMAN 3
+
+#ifdef _WIN64
+    std::string get_binary_path(){
+        std::string res;
+        char raw_path[MAX_PATH + 1];
+        if (GetModuleFileName(NULL, raw_path, MAX_PATH)){
+            char drive[MAX_PATH + 1],
+                dir[MAX_PATH + 1],
+                fname[MAX_PATH + 1],
+                ext[MAX_PATH + 1];
+
+            _splitpath_s(raw_path,drive,dir,fname,ext);
+            res = drive;
+            res += dir;
+        }
+        return res;
+    }
+#else
+#endif
