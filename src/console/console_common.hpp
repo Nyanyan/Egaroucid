74b44623187c49b54fd1683b3be503b829ea88f1
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-12-18 15:23:32 +0900

    update for linux tested on Ubuntu and worked :)

--- src/console/console_common.hpp
+++ src/console/console_common.hpp
@@ -10,11 +10,16 @@
 
 #pragma once
 #include <iostream>
-//#include <filesystem>
 #include <stdlib.h>
 #include <string>
 #ifdef _WIN64
     #include <windows.h>
+#elif __APPLE__ // TBD
+#else
+    #include <filesystem>
+    #include <linux/limits.h>
+    #include <sys/types.h>
+    #include <unistd.h>
 #endif
 
 #define MODE_HUMAN_AI 0
@@ -22,39 +27,29 @@
 #define MODE_AI_AI 2
 #define MODE_HUMAN_HUMAN 3
 
-
-std::string replace_backslash(std::string str){
-    std::string res;
-    for (int i = 0; i < (int)str.length(); ++i){
-        if (str[i] == '\\')
-            res += '/';
-        else
-            res += str[i];
-    }
-    return res;
-}
-
-std::string get_parent_path(char raw_path[]){
-    char drive[MAX_PATH + 1],
-        dir[MAX_PATH + 1],
-        fname[MAX_PATH + 1],
-        ext[MAX_PATH + 1];
-    _splitpath_s(raw_path,drive,dir,fname,ext);
-    std::string res = drive;
-    res += dir;
-    return res;
-}
-
-/*
-std::string get_parent_path(char raw_path[]){
-    std::filesystem::path p = raw_path;
-    std::string res = p.parent_path().string();
-    std::cerr << res << std::endl;
-    return res;
-}
-*/
-
 #ifdef _WIN64
+    std::string replace_backslash(std::string str){
+        std::string res;
+        for (int i = 0; i < (int)str.length(); ++i){
+            if (str[i] == '\\')
+                res += '/';
+            else
+                res += str[i];
+        }
+        return res;
+    }
+
+    std::string get_parent_path(char raw_path[]){
+        char drive[MAX_PATH + 1],
+            dir[MAX_PATH + 1],
+            fname[MAX_PATH + 1],
+            ext[MAX_PATH + 1];
+        _splitpath_s(raw_path,drive,dir,fname,ext);
+        std::string res = drive;
+        res += dir;
+        return res;
+    }
+
     std::string get_binary_path(){
         std::string res;
         char raw_path[MAX_PATH + 1];
@@ -64,6 +59,12 @@ std::string get_parent_path(char raw_path[]){
     }
 #elif __APPLE__ // TBD
 #else
+    std::string get_parent_path(char raw_path[]){
+        std::filesystem::path p = raw_path;
+        std::string res = p.parent_path().string() + "/";
+        return res;
+    }
+
     std::string get_binary_path(){
         char raw_path[PATH_MAX + 1];
         const size_t LINKSIZE = 100;
