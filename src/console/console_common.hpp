95140fe6bbfb9bf64cbf6d7f040c4bb3da04f1f5
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-12-18 13:14:36 +0900

    get_binary_path for linux written NEED CHECK

--- src/console/console_common.hpp
+++ src/console/console_common.hpp
@@ -32,22 +32,35 @@ std::string replace_backslash(std::string str){
     return res;
 }
 
+std::string get_parent_path(char raw_path[]){
+    char drive[MAX_PATH + 1],
+        dir[MAX_PATH + 1],
+        fname[MAX_PATH + 1],
+        ext[MAX_PATH + 1];
+    _splitpath_s(raw_path,drive,dir,fname,ext);
+    std::string res = drive;
+    res += dir;
+    return res;
+}
+
 #ifdef _WIN64
     std::string get_binary_path(){
         std::string res;
         char raw_path[MAX_PATH + 1];
-        if (GetModuleFileName(NULL, raw_path, MAX_PATH)){
-            char drive[MAX_PATH + 1],
-                dir[MAX_PATH + 1],
-                fname[MAX_PATH + 1],
-                ext[MAX_PATH + 1];
-
-            _splitpath_s(raw_path,drive,dir,fname,ext);
-            res = drive;
-            res += dir;
-            res = replace_backslash(res);
-        }
+        if (GetModuleFileName(NULL, raw_path, MAX_PATH))
+            res = replace_backslash(get_parent_path(raw_path));
         return res;
     }
+#elif __APPLE__ // TBD
 #else
+    std::string get_binary_path(){
+        char raw_path[PATH_MAX + 1];
+        const size_t LINKSIZE = 100;
+        char link[LINKSIZE];
+        snprintf(link, LINKSIZE, "/proc/%d/exe", getpid());
+        readlink(link, raw_path, PATH_MAX);
+        std::cerr << raw_path << std::endl;
+        std::string res = get_parent_path(raw_path);
+        return res;
+    }
 #endif
