48c0ddb100de49295d14ed31a397edd6b84b5f01
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-02-01 17:22:02 +0900

    rm

--- move_ordering/end_last7/new_util/setting.hpp
+++
@@ -1,8 +0,0 @@
-
-
-// vertical mirror
-#define USE_FAST_VERTICAL_MIRROR true
-
-// flip calculating
-#define FLIP_CALC_MODE 1
-#define LAST_FLIP_CALC_MODE 0
--- src/Egaroucid_web_compile.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-with open('Egaroucid_web_compile_cmd.txt', 'r') as f:
-    cmd = f.read()
-o = subprocess.run(cmd, shell=True, encoding='utf-8', stderr=subprocess.STDOUT, timeout=None)
-print('------------------compiled------------------')
