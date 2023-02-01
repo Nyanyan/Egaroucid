507c163625a6870f692dac3a0389fd922eee68c5
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-02-01 17:20:33 +0900

    separate build from src

---
+++ build/egaroucid_vs_edax.py
@@ -0,0 +1,123 @@
+import subprocess
+from othello_py import *
+import sys
+
+with open('openingssmall.txt', 'r') as f:
+    tactic = [elem for elem in f.read().splitlines()]
+
+#shuffle(tactic)
+
+level = int(sys.argv[1])
+n_games = int(sys.argv[2])
+
+egaroucid = subprocess.Popen(('Egaroucid_for_console.exe -quiet -nobook -level ' + str(level)).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+edax = subprocess.Popen(('edax-4.4 -q -level ' + str(level)).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+egaroucid_win = [0, 0]
+edax_win = [0, 0]
+draw = [0, 0]
+
+print('level', level)
+
+max_num = min(len(tactic), n_games)
+smpl = range(len(tactic))
+print('play', max_num, 'games')
+
+for num in range(max_num):
+    tactic_idx = smpl[num % len(tactic)]
+    for player in range(2):
+        record = ''
+        boards = []
+        o = othello()
+        for i in range(0, len(tactic[tactic_idx]), 2):
+            if not o.check_legal():
+                o.player = 1 - o.player
+                o.check_legal()
+            x = ord(tactic[tactic_idx][i].lower()) - ord('a')
+            y = int(tactic[tactic_idx][i + 1]) - 1
+            record += tactic[tactic_idx][i] + tactic[tactic_idx][i + 1]
+            o.move(y, x)
+        while True:
+            if not o.check_legal():
+                o.player = 1 - o.player
+                if not o.check_legal():
+                    break
+            grid_str = 'setboard '
+            for yy in range(hw):
+                for xx in range(hw):
+                    if o.grid[yy][xx] == black:
+                        grid_str += 'b'
+                    elif o.grid[yy][xx] == white:
+                        grid_str += 'w'
+                    else:
+                        grid_str += '.'
+            if o.player == black:
+                grid_str += ' b\n'
+            else:
+                grid_str += ' w\n'
+            if o.player == player:
+                egaroucid.stdin.write(grid_str.encode('utf-8'))
+                egaroucid.stdin.flush()
+                egaroucid.stdin.write('go\n'.encode('utf-8'))
+                egaroucid.stdin.flush()
+                line = ''
+                while line == '':
+                    line = egaroucid.stdout.readline().decode().replace('\r', '').replace('\n', '')
+                coord = line
+                try:
+                    y = int(coord[1]) - 1
+                    x = ord(coord[0]) - ord('a')
+                except:
+                    print('error')
+                    print(grid_str[:-1])
+                    print(o.player, player)
+                    print(coord)
+                    egaroucid.stdin.write('quit\n'.encode('utf-8'))
+                    egaroucid.stdin.flush()
+                    edax.stdin.write('quit\n'.encode('utf-8'))
+                    edax.stdin.flush()
+                    exit()
+            else:
+                edax.stdin.write(grid_str.encode('utf-8'))
+                edax.stdin.flush()
+                edax.stdin.write('go\n'.encode('utf-8'))
+                edax.stdin.flush()
+                line = ''
+                while len(line) < 3:
+                    line = edax.stdout.readline().decode().replace('\r', '').replace('\n', '')
+                try:
+                    coord = line.split()[2]
+                    y = int(coord[1]) - 1
+                    x = ord(coord[0]) - ord('A')
+                except:
+                    print('error')
+                    print(grid_str[:-1])
+                    print(o.player, player)
+                    print(coord)
+                    egaroucid.stdin.write('quit\n'.encode('utf-8'))
+                    egaroucid.stdin.flush()
+                    edax.stdin.write('quit\n'.encode('utf-8'))
+                    edax.stdin.flush()
+                    exit()
+            record += chr(ord('a') + x) + str(y + 1)
+            if not o.move(y, x):
+                o.print_info()
+                print(grid_str[:-1])
+                print(o.player, player)
+                print(coord)
+                print(y, x)
+        if o.n_stones[player] > o.n_stones[1 - player]:
+            egaroucid_win[player] += 1
+        elif o.n_stones[player] == o.n_stones[1 - player]:
+            draw[player] += 1
+        else:
+            edax_win[player] += 1
+            #print(record)
+        print('\r', num, max_num, ' ', egaroucid_win, draw, edax_win, sum(egaroucid_win), sum(edax_win), sum(egaroucid_win) / max(1, sum(egaroucid_win) + sum(edax_win)), end='')
+
+egaroucid.stdin.write('quit\n'.encode('utf-8'))
+egaroucid.stdin.flush()
+edax.stdin.write('quit\n'.encode('utf-8'))
+edax.stdin.flush()
+print('')
+
+print('level: ', level, ' Egaroucid plays black WDL: ', egaroucid_win[0], '-', draw[0], '-', edax_win[0], ' ', egaroucid_win[0] / (egaroucid_win[0] + edax_win[0]), ' Egaroucid plays white WDL: ', egaroucid_win[1], '-', draw[1], '-', edax_win[1], ' ', egaroucid_win[1] / (egaroucid_win[1] + edax_win[1]), ' Egaroucid win rate: ', sum(egaroucid_win) / max(1, sum(egaroucid_win) + sum(edax_win)), sep='')
---
+++ build/ffotest_40_49.py
@@ -0,0 +1,72 @@
+import subprocess
+import sys
+
+answer = '''#40 38  a2
+#41 0   h4
+#42 6   g2
+#43 -12 c7  g3
+#44 -14 d2  b8
+#45 6   b2
+#46 -8  b3
+#47 4   g2
+#48 28  f6
+#49 16  e1
+#50 10  d8
+#51 6   e2  a3
+#52 0   a3
+#53 -2  d8
+#54 -2  c7
+#55 0   g6  g4  b7
+#56 2   h5
+#57 -10 a6
+#58 4   g1
+#59 64  g8  h4  e8
+#60 20  c2 
+#61 -14 h3 g1
+#62 28  e8
+#63 -2  f2
+#64 20  b4
+#65 10  g1
+#66 30  h3
+#67 22  h3
+#68 28  e8 
+#69 0   h3
+#70 -24 e3
+#71 20  d2
+#72 24  e1
+#73 -4  g4
+#74 -30 f1
+#75 14  d2
+#76 32  a3
+#77 34  b7
+#78 8  f1
+#79 64 d7'''
+
+answer = answer.splitlines()
+
+#if sys.argv[1] == 'g':
+cmd = 'Egaroucid_for_Console.exe -l 60 -nobook -thread 23 -hash 25 -solve problem/ffo40-49.txt'
+#else:
+#    cmd = 'Egaroucid_console.exe -l 60 -nobook -thread 23 -hash 25 -solve problem/ffo40-49.txt'
+print(cmd)
+egaroucid = subprocess.Popen((cmd).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+res = ''
+line = egaroucid.stdout.readline().decode().replace('\n', '').replace('\r', '')
+print('#   ' + line)
+for i in range(10):
+    line = egaroucid.stdout.readline().decode().replace('\n', '').replace('\r', '')
+    policy = line.split()[3][:-1]
+    policies = answer[i].split()[2:]
+    if not (policy in policies):
+        line += ' WRONG POLICY'
+    score = line.split()[4][:-1]
+    correct_score = answer[i].split()[1]
+    if int(score) != int(correct_score):
+        line += ' WRONG SCORE'
+    line = '#' + str(40 + i) + ' ' + line
+    print(line)
+    res += line + '\n'
+line = egaroucid.stdout.readline().decode().replace('\n', '').replace('\r', '')
+print(line)
+egaroucid.kill()
