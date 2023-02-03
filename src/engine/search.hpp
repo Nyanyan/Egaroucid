8a24dbe1f2ceaa5d11f77a3e8064cc1a6fc937f4
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-02-03 17:52:50 +0900

    optimize code

--- src/engine/search.hpp
+++ src/engine/search.hpp
@@ -276,7 +276,8 @@ class Search{
             @brief Get evaluation phase
         */
         inline int phase(){
-            return std::min(N_PHASES - 1, (n_discs - 4) / PHASE_N_STONES);
+            return n_discs - 4;
+            //return std::min(N_PHASES - 1, (n_discs - 4) / PHASE_N_STONES);
         }
 };
 
