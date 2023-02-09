51a8df1ea1d41b154bc2896d0a3192ff188d9172
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-02-09 21:16:40 +0900

    add problem maker

---
+++ src/tools/pickup_problem/pickup_problem.cpp
@@ -0,0 +1,67 @@
+#include "./../../engine/board.hpp"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+void pck_init(){
+    bit_init();
+    mobility_init();
+    flip_init();
+}
+
+std::string board_to_str(Board board, int8_t p){
+    uint_fast8_t arr[HW2];
+    board.translate_to_arr(arr, p);
+    std::string res;
+    for (int i = 0; i < HW2; ++i){
+        if (arr[i] == BLACK)
+            res += "X";
+        else if (arr[i] == WHITE)
+            res += "O";
+        else
+            res += "-";
+    }
+    res += " ";
+    if (p == BLACK)
+        res += "X";
+    else
+        res += "O";
+    return res;
+}
+
+int main(int argc, char* argv[]){
+    if (argc < 3){
+        std::cerr << "input [in_file] [n_discs] [n_problems]" << std::endl;
+        return 1;
+    }
+    pck_init();
+    std::string in_file = std::string(argv[1]);
+    int n_discs = atoi(argv[2]);
+    int n_problems = atoi(argv[3]);
+    std::cerr << in_file << " " << n_discs << " " << n_problems << std::endl;
+    FILE* fp;
+    if (fopen_s(&fp, in_file.c_str(), "rb") != 0) {
+        std::cerr << "can't open " << in_file << std::endl;
+        return 1;
+    }
+    int n = 0;
+    Board board;
+    int8_t player, score, policy;
+    while (n < n_problems){
+        if (fread(&(board.player), 8, 1, fp) < 1)
+            break;
+        fread(&(board.opponent), 8, 1, fp);
+        fread(&player, 1, 1, fp);
+        fread(&policy, 1, 1, fp);
+        fread(&score, 1, 1, fp);
+        if (board.n_discs() == n_discs){
+            std::cout << board_to_str(board, player) << std::endl;
+            ++n;
+        }
+    }
+    std::cerr << n << " results printed" << std::endl;
+    return 0;
+}
