542c10c03f0775ca65b35e49eb5b4c013a610624
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-02-11 01:48:22 +0900

    try to speed up evaluation

--- src/tools/evaluation/create_pattern_starts.py
+++ src/tools/evaluation/create_pattern_starts.py
@@ -1,5 +1,15 @@
-arr = [10, 10, 10, 9, 8, 8, 8, 5, 6, 7, 8, 10, 10, 10, 10, 10]
-res = [1]
+arr = [
+    8, 8, 8, 5, 
+    6, 7, 8, 9, 
+    10, 10, 10, 10, 
+    10, 10, 10, 10
+]
+res = [2]
 for elem in arr:
     res.append(res[-1] + 3 ** elem)
 print(res)
+
+res2 = []
+for elem in res:
+    res2.append(elem - res[4])
+print(res2[4:8])
