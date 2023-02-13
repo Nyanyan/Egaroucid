70b48f2243408334a5b8a67a54a60e7887bae33f
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-02-13 13:46:39 +0900

    full stability with SIMD

--- src/engine/stability.hpp
+++ src/engine/stability.hpp
@@ -6,6 +6,7 @@
     @date 2021-2023
     @author Takuto Yamana (a.k.a. Nyanyan)
     @license GPL-3.0 license
+    @notice I referred to codes written by others
 */
 
 #pragma once
@@ -97,66 +98,104 @@ inline void stability_init() {
     }
 }
 
-/*
-    @brief Calculate full stability in horizontal direction
+// @notice from http://www.amy.hi-ho.ne.jp/okuhara/bitboard.htm
+// modified by Nyanyan
+#if USE_SIMD
+    inline void full_stability(uint64_t discs, uint64_t *h, uint64_t *v, uint64_t *d7, uint64_t *d9){
+        // horizontal & vertical
+        const __m128i shift0 = _mm_set_epi64x(1, 8);
+        const __m128i shift1 = _mm_set_epi64x(2, 16);
+        const __m128i shift2 = _mm_set_epi64x(4, 32);
+        const __m128i hvmask = _mm_set_epi64x(0x0101010101010101ULL, 0x00000000000000FFULL);
+        const __m128i hvmul = _mm_set_epi64x(0x00000000000000FFULL, 0x0101010101010101ULL);
+        __m128i hv = _mm_set1_epi64x(discs);
+        hv = _mm_and_si128(hv, _mm_srlv_epi64(hv, shift0));
+        hv = _mm_and_si128(hv, _mm_srlv_epi64(hv, shift1));
+        hv = _mm_and_si128(hv, _mm_srlv_epi64(hv, shift2));
+        hv = _mm_and_si128(hv, hvmask);
+        hv = _mm_mullo_epi64(hv, hvmul);
+        *v = _mm_cvtsi128_si64(hv);
+        *h = _mm_cvtsi128_si64(_mm_unpackhi_epi64(hv, hv));
 
-    @param full                 a bitboard representing discs
-*/
-inline uint64_t full_stability_h(uint64_t full){
-    full &= full >> 1;
-    full &= full >> 2;
-    full &= full >> 4;
-    return (full & 0x0101010101010101ULL) * 0xFF;
-}
+        // diagonal
+        const __m128i e790 = _mm_set1_epi64x(0xff80808080808080);
+        const __m128i e791 = _mm_set1_epi64x(0x01010101010101ff);
+        const __m128i e792 = _mm_set1_epi64x(0x00003f3f3f3f3f3f);
+        const __m128i e793 = _mm_set1_epi64x(0x0f0f0f0ff0f0f0f0);
+        __m128i l79, r79;
+        l79 = r79 = _mm_unpacklo_epi64(_mm_cvtsi64_si128(discs), _mm_cvtsi64_si128(vertical_mirror(discs)));
+        l79 = _mm_and_si128(l79, _mm_or_si128(e790, _mm_srli_epi64(l79, 9)));
+        r79 = _mm_and_si128(r79, _mm_or_si128(e791, _mm_slli_epi64(r79, 9)));
+        l79 = _mm_andnot_si128(_mm_andnot_si128(_mm_srli_epi64(l79, 18), e792), l79);
+        r79 = _mm_andnot_si128(_mm_slli_epi64(_mm_andnot_si128(r79, e792), 18), r79);
+        l79 = _mm_and_si128(_mm_and_si128(l79, r79), _mm_or_si128(e793,
+            _mm_or_si128(_mm_srli_epi64(l79, 36), _mm_slli_epi64(r79, 36))));
+        *d9 = _mm_cvtsi128_si64(l79);
+        *d7 = vertical_mirror(_mm_cvtsi128_si64(_mm_unpackhi_epi64(l79, l79)));
+    }
+#else
+    /*
+        @brief Calculate full stability in horizontal direction
 
-/*
-    @brief Calculate full stability in vertical direction
+        @param full                 a bitboard representing discs
+    */
+    inline uint64_t full_stability_h(uint64_t full){
+        full &= full >> 1;
+        full &= full >> 2;
+        full &= full >> 4;
+        return (full & 0x0101010101010101ULL) * 0xFF;
+    }
 
-    @param full                 a bitboard representing discs
-*/
-inline uint64_t full_stability_v(uint64_t full){
-    full &= (full >> 8) | (full << 56);
-    full &= (full >> 16) | (full << 48);
-    full &= (full >> 32) | (full << 32);
-    return full;
-}
+    /*
+        @brief Calculate full stability in vertical direction
 
-/*
-    @brief Calculate full stability in diagonal direction
+        @param full                 a bitboard representing discs
+    */
+    inline uint64_t full_stability_v(uint64_t full){
+        full &= (full >> 8) | (full << 56);
+        full &= (full >> 16) | (full << 48);
+        full &= (full >> 32) | (full << 32);
+        return full;
+    }
 
-    @param full                 a bitboard representing discs
-    @param full_d7              an integer to store result of d7 line
-    @param full_d9              an integer to store result of d9 line
-*/
-inline void full_stability_d(uint64_t full, uint64_t *full_d7, uint64_t *full_d9){
-    constexpr uint64_t edge = 0xFF818181818181FFULL;
-    uint64_t l7, r7, l9, r9;
-    l7 = r7 = full;
-    l7 &= edge | (l7 >> 7);		r7 &= edge | (r7 << 7);
-    l7 &= 0xFFFF030303030303ULL | (l7 >> 14);	r7 &= 0xC0C0C0C0C0C0FFFFULL | (r7 << 14);
-    l7 &= 0xFFFFFFFF0F0F0F0FULL | (l7 >> 28);	r7 &= 0xF0F0F0F0FFFFFFFFULL | (r7 << 28);
-    *full_d7 = l7 & r7;
+    /*
+        @brief Calculate full stability in diagonal direction
 
-    l9 = r9 = full;
-    l9 &= edge | (l9 >> 9);		r9 &= edge | (r9 << 9);
-    l9 &= 0xFFFFC0C0C0C0C0C0ULL | (l9 >> 18);	r9 &= 0x030303030303FFFFULL | (r9 << 18);
-    *full_d9 = l9 & r9 & (0x0F0F0F0FF0F0F0F0ULL | (l9 >> 36) | (r9 << 36));
-}
+        @param full                 a bitboard representing discs
+        @param full_d7              an integer to store result of d7 line
+        @param full_d9              an integer to store result of d9 line
+    */
+    inline void full_stability_d(uint64_t full, uint64_t *full_d7, uint64_t *full_d9){
+        constexpr uint64_t edge = 0xFF818181818181FFULL;
+        uint64_t l7, r7, l9, r9;
+        l7 = r7 = full;
+        l7 &= edge | (l7 >> 7);        r7 &= edge | (r7 << 7);
+        l7 &= 0xFFFF030303030303ULL | (l7 >> 14);    r7 &= 0xC0C0C0C0C0C0FFFFULL | (r7 << 14);
+        l7 &= 0xFFFFFFFF0F0F0F0FULL | (l7 >> 28);    r7 &= 0xF0F0F0F0FFFFFFFFULL | (r7 << 28);
+        *full_d7 = l7 & r7;
 
-/*
-    @brief Calculate full stability in all direction
+        l9 = r9 = full;
+        l9 &= edge | (l9 >> 9);        r9 &= edge | (r9 << 9);
+        l9 &= 0xFFFFC0C0C0C0C0C0ULL | (l9 >> 18);    r9 &= 0x030303030303FFFFULL | (r9 << 18);
+        *full_d9 = l9 & r9 & (0x0F0F0F0FF0F0F0F0ULL | (l9 >> 36) | (r9 << 36));
+    }
 
-    @param discs                a bitboard representing discs
-    @param h                    an integer to store result of h line
-    @param v                    an integer to store result of h line
-    @param d7                   an integer to store result of d7 line
-    @param d9                   an integer to store result of d9 line
-*/
-inline void full_stability(uint64_t discs, uint64_t *h, uint64_t *v, uint64_t *d7, uint64_t *d9){
-    *h = full_stability_h(discs);
-    *v = full_stability_v(discs);
-    full_stability_d(discs, d7, d9);
-}
+    /*
+        @brief Calculate full stability in all direction
+
+        @param discs                a bitboard representing discs
+        @param h                    an integer to store result of h line
+        @param v                    an integer to store result of h line
+        @param d7                   an integer to store result of d7 line
+        @param d9                   an integer to store result of d9 line
+    */
+    inline void full_stability(uint64_t discs, uint64_t *h, uint64_t *v, uint64_t *d7, uint64_t *d9){
+        *h = full_stability_h(discs);
+        *v = full_stability_v(discs);
+        full_stability_d(discs, d7, d9);
+    }
+#endif
+// end of modification
 
 /*
     @brief Calculate stable discs as a bitboard
@@ -171,11 +210,11 @@ inline uint64_t calc_stability_bits(Board *board){
     uint64_t full_h, full_v, full_d7, full_d9;
     uint64_t edge_stability = 0, player_stability = 0, opponent_stability = 0, n_stability;
     uint64_t h, v, d7, d9;
-    const uint64_t player_mask = board->player & 0b0000000001111110011111100111111001111110011111100111111000000000ULL;
-    const uint64_t opponent_mask = board->opponent & 0b0000000001111110011111100111111001111110011111100111111000000000ULL;
+    const uint64_t player_mask = board->player & 0x007E7E7E7E7E7E00ULL;
+    const uint64_t opponent_mask = board->opponent & 0x007E7E7E7E7E7E00ULL;
     uint8_t pl, op;
-    pl = board->player & 0b11111111U;
-    op = board->opponent & 0b11111111U;
+    pl = 0b11111111U & board->player;
+    op = 0b11111111U & board->opponent;
     edge_stability |= stability_edge_arr[pl][op][0];
     pl = join_h_line(board->player, 7);
     op = join_h_line(board->opponent, 7);
@@ -225,11 +264,11 @@ inline void calc_stability(Board *board, int *stab0, int *stab1){
     uint64_t full_h, full_v, full_d7, full_d9;
     uint64_t edge_stability = 0, player_stability = 0, opponent_stability = 0, n_stability;
     uint64_t h, v, d7, d9;
-    const uint64_t player_mask = board->player & 0b0000000001111110011111100111111001111110011111100111111000000000ULL;
-    const uint64_t opponent_mask = board->opponent & 0b0000000001111110011111100111111001111110011111100111111000000000ULL;
+    const uint64_t player_mask = board->player & 0x007E7E7E7E7E7E00ULL;
+    const uint64_t opponent_mask = board->opponent & 0x007E7E7E7E7E7E00ULL;
     uint8_t pl, op;
-    pl = board->player & 0b11111111U;
-    op = board->opponent & 0b11111111U;
+    pl = 0b11111111U & board->player;
+    op = 0b11111111U & board->opponent;
     edge_stability |= stability_edge_arr[pl][op][0];
     pl = join_h_line(board->player, 7);
     op = join_h_line(board->opponent, 7);
