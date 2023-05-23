ce90fd3497cff2ac49fbf3946b8c634c6bd209a5
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-05-23 13:37:55 +0900

    optimizing parity ordering

--- src/tools/util/parity_ordering_simd_opt.py
+++ src/tools/util/parity_ordering_simd_opt.py
@@ -15,5 +15,31 @@ for places in places_lst:
         place_count.append(places.count(elem))
     sorted_place_count = sorted(place_count)
     need_to_sort = (sorted_place_count == [0, 1, 1, 2]) and (place_count != sorted_place_count)
-    if need_to_sort:
-        print(parities, places)
+    sort_lst = [0, 1, 2, 3]
+    if need_to_sort: # 2-1-1-0
+        sort_lst = []
+        no_priority = []
+        for i in range(4):
+            for j in range(4):
+                if i == j:
+                    continue
+                if places[i] == places[j]:
+                    no_priority.append(i)
+                    break
+            else:
+                sort_lst.append(i)
+        sort_lst.extend(no_priority)
+    #for i in range(4):
+    #    sort_lst[i] += 12
+    #    sort_lst[i] ^= 0x80
+    sort_32bit = 0
+    for i in range(4):
+        sort_32bit <<= 8
+        sort_32bit |= sort_lst[i]
+    parity_ordering_shuffle_mask[parities] = sort_32bit
+
+for i in range(64):
+    print(parity_ordering_shuffle_mask[i], end='')
+    print('U, ', end='')
+    if i % 8 == 7:
+        print('')
