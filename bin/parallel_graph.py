53ebb9c7689ddf8de757c8ad6e8fec14481854ee
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-06-02 20:16:55 +0900

    edit some benchmark program

--- bin/parallel_graph.py
+++ bin/parallel_graph.py
@@ -1,14 +1,21 @@
 import matplotlib.pyplot as plt
+import sys
 
-with open('par_20230602_4.txt', 'r') as f:
+file = sys.argv[1]
+
+with open(file, 'r') as f:
     raw_data = f.read().splitlines()
 
 x = []
 y = []
 for line in raw_data:
-    x.append(float(line.split()[0]))
-    y.append(float(line.split()[1]))
-    if x[-1] >= 1150:
+    x_elem = float(line.split()[0])
+    if len(x) == 0 or x[-1] != x_elem:
+        x.append(x_elem)
+        y.append(float(line.split()[1]))
+    if len(y) >= 50 and sum(y[-50:]) == max(y) * 50:
+        x = x[:-50]
+        y = y[:-50]
         break
 
 print('avg', sum(y) / len(y))
