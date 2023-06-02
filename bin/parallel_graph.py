fb0caae4a67cdd482e8a4ae2e4cf37eb8610b724
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-06-02 19:30:19 +0900

    YBWC_WINDOW_SPLIT_BROTHER_THRESHOLD = 2

--- bin/parallel_graph.py
+++ bin/parallel_graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-with open('par_20230602_2.txt', 'r') as f:
+with open('par_20230602_4.txt', 'r') as f:
     raw_data = f.read().splitlines()
 
 x = []
@@ -8,8 +8,10 @@ y = []
 for line in raw_data:
     x.append(float(line.split()[0]))
     y.append(float(line.split()[1]))
-    if x[-1] >= 900:
+    if x[-1] >= 1150:
         break
+
+print('avg', sum(y) / len(y))
 
 plt.plot(x, y)
 plt.minorticks_on()
