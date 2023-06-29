d7a1bbea8a849427e04f887fbf8739121b0cb568
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-06-30 02:51:29 +0900

    update display which player to move

--- src/gui/draw.hpp
+++ src/gui/draw.hpp
@@ -58,10 +58,10 @@ void draw_info(Colors colors, History_elem history_elem, Fonts fonts, Menu_eleme
         else
             ai_human_str = language.get("info", "human");
         if (black_to_move ^ pausing_in_pass) {
-            fonts.font(language.get("info", "black") + U" " + ai_human_str).draw(18, Arg::topCenter(INFO_SX + INFO_WIDTH / 2, INFO_SY + 22));
+            fonts.font(language.get("info", "black") + U" (" + ai_human_str + U")").draw(17, Arg::topCenter(INFO_SX + INFO_WIDTH / 2, INFO_SY + 22));
         }
         else {
-            fonts.font(language.get("info", "white") + U" " + ai_human_str).draw(18, Arg::topCenter(INFO_SX + INFO_WIDTH / 2, INFO_SY + 22));
+            fonts.font(language.get("info", "white") + U" (" + ai_human_str + U")").draw(17, Arg::topCenter(INFO_SX + INFO_WIDTH / 2, INFO_SY + 22));
         }
     }
     else {
