874a67cc5c3b63f64367030fa331eba8275980e2
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-07-14 10:14:05 +0900

    update eval file cmd

--- bin/egaroucid_vs_edax.py
+++ bin/egaroucid_vs_edax.py
@@ -11,7 +11,14 @@ shuffle(tactic)
 level = int(sys.argv[1])
 n_games = int(sys.argv[2])
 
-egaroucid = subprocess.Popen(('Egaroucid_for_console.exe -quiet -nobook -thread 19 -level ' + str(level)).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+
+file = None
+if len(sys.argv) == 4:
+    file = sys.argv[3]
+    print('egaroucid eval ', file)
+    egaroucid = subprocess.Popen(('Egaroucid_for_console.exe -eval ' + file + ' -quiet -nobook -thread 19 -level ' + str(level)).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+else:
+    egaroucid = subprocess.Popen(('Egaroucid_for_console.exe -quiet -nobook -thread 19 -level ' + str(level)).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 edax = subprocess.Popen(('wEdax-x64-modern.exe -q -level ' + str(level)).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 egaroucid_win = [0, 0]
 edax_win = [0, 0]
