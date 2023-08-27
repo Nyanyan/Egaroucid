6fe5d5b4dcc29e812ff76f742d70f2b5da5c3360
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-08-27 16:49:59 +0900

    update eval and mpc

--- src/tools/probcut/model_sigma_mid.py
+++ src/tools/probcut/model_sigma_mid.py
@@ -44,20 +44,29 @@ for n_discs in range(61):
         w_n_discs.append(n_discs)
         x_depth1.append(depth1)
         y_depth2.append(depth2)
-        s = max(12.0, min(22.0, 0.0 + (22.0 - 0.0) * n_discs / 50)) # d2 = 60
-        e = max(10.0, min(20.0, 0.0 + (20.0 - 0.0) * n_discs / 50)) # d2 = 0
+        s = 16.0 + 3.0 * n_discs / 60 #max(10.0, min(21.0, 18.0 + (21.0 - 18.0) * n_discs / 30)) # d2 = 60
+        e = 3.0 + 2.0 * n_discs / 60 #max(5.0, min(13.0, 8.0 + (13.0 - 8.0) * n_discs / 30)) # d2 = 0
+        z_error.append(e + (s - e) * depth2 / 60)
+        weight.append(0.001)
+
+for n_discs in range(61):
+    for depth2 in range(10, 60):
+        depth1 = depth2 / 2
+        w_n_discs.append(n_discs)
+        x_depth1.append(depth1)
+        y_depth2.append(depth2)
+        s = 11.0 + 2.0 * n_discs / 60 #max(10.0, min(18.0, 18.0 + (10.0 - 18.0) * n_discs / 30)) # d2 = 60
+        e = 4.0 + 2.0 * n_discs / 60 #max(5.0, min(10.0, 10.0 + (6.0 - 10.0) * n_discs / 30)) # d2 = 0
         z_error.append(e + (s - e) * depth2 / 60)
         weight.append(0.001)
 
 for n_discs in range(61):
     for depth2 in range(15, 60):
-        depth1 = 30
+        depth1 = depth2
         w_n_discs.append(n_discs)
         x_depth1.append(depth1)
         y_depth2.append(depth2)
-        s = max(8.0, min(15.0, 1.0 + (15.0 - 1.0) * n_discs / 50)) # d2 = 60
-        e = max(7.0, min(14.0, 1.0 + (14.0 - 1.0) * n_discs / 50)) # d2 = 0
-        z_error.append(e + (s - e) * depth2 / 60)
+        z_error.append(3.5)
         weight.append(0.002)
 
 def f(wxy, probcut_a, probcut_b, probcut_c, probcut_d, probcut_e, probcut_f, probcut_g, probcut_h, probcut_i, probcut_j):
@@ -97,11 +106,14 @@ def plot_fit_result_onephase(n_discs, params):
 
 probcut_params_before = [1.0 for _ in range(10)]
 
+probcut_params_old = [-0.001265116404528472, -1.758143972292579, 1.7566279520842052, -1.0938733019995888, 1.1290649413162603, 10.55327508419982, 3.106465908394277, 1, 1, 1]
+
 popt, pcov = curve_fit(f, (w_n_discs, x_depth1, y_depth2), z_error, np.array(probcut_params_before), sigma=weight, absolute_sigma=True)
 #popt = probcut_params_before
 print([float(elem) for elem in popt])
 for i in range(len(popt)):
     print('#define probcut_' + chr(ord('a') + i), popt[i])
-plot_fit_result_onephase(20, popt)
-plot_fit_result_onephase(30, popt)
-plot_fit_result_onephase(40, popt)
+
+for i in [10, 30, 50]:
+    plot_fit_result_onephase(i, popt)
+    #plot_fit_result_onephase(i, probcut_params_old)
