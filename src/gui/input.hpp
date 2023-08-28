78819ff112f4390383915aa0987bf37c1898047e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-08-29 08:21:39 +0900

    delete space and indention in input mode

--- src/gui/input.hpp
+++ src/gui/input.hpp
@@ -14,66 +14,6 @@
 #include "./../engine/engine_all.hpp"
 #include "function/function_all.hpp"
 
-std::vector<History_elem> import_transcript_processing(std::vector<History_elem> n_history, History_elem strt_elem, std::string transcript, bool* failed) {
-    Board h_bd = strt_elem.board;
-    String transcript_str = Unicode::Widen(transcript).replace(U"\r", U"").replace(U"\n", U"").replace(U" ", U"");
-    if (transcript_str.size() % 2 != 0 && transcript_str.size() >= 120) {
-        *failed = true;
-    }
-    else {
-        int y, x;
-        uint64_t legal;
-        Flip flip;
-        History_elem history_elem;
-        int player = strt_elem.player;
-        //history_elem.set(h_bd, player, GRAPH_IGNORE_VALUE, -1, -1, -1, "");
-        //n_history.emplace_back(history_elem);
-        for (int i = 0; i < (int)transcript_str.size(); i += 2) {
-            x = (int)transcript_str[i] - (int)'a';
-            if (x < 0 || HW <= x) {
-                x = (int)transcript_str[i] - (int)'A';
-                if (x < 0 || HW <= x) {
-                    *failed = true;
-                    break;
-                }
-            }
-            y = (int)transcript_str[i + 1] - (int)'1';
-            if (y < 0 || HW <= y) {
-                *failed = true;
-                break;
-            }
-            y = HW_M1 - y;
-            x = HW_M1 - x;
-            legal = h_bd.get_legal();
-            if (1 & (legal >> (y * HW + x))) {
-                calc_flip(&flip, &h_bd, y * HW + x);
-                h_bd.move_board(&flip);
-                player ^= 1;
-                if (h_bd.get_legal() == 0ULL) {
-                    h_bd.pass();
-                    player ^= 1;
-                    if (h_bd.get_legal() == 0ULL) {
-                        h_bd.pass();
-                        player ^= 1;
-                        if (i != transcript_str.size() - 2) {
-                            *failed = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            else {
-                *failed = true;
-                break;
-            }
-            n_history.back().next_policy = y * HW + x;
-            history_elem.set(h_bd, player, GRAPH_IGNORE_VALUE, -1, y * HW + x, -1, "");
-            n_history.emplace_back(history_elem);
-        }
-    }
-    return n_history;
-}
-
 class Import_transcript : public App::Scene {
 private:
     Button single_back_button;
@@ -126,6 +66,7 @@ public:
                     return_pressed = true;
                 }
             }
+            str = str.replace(U"\r\n", U"").replace(U"\n", U"").replace(U" ", U"");
             transcript = str.narrow();
             getData().fonts.font(str + U'|' + editingText).draw(15, text_area.stretched(-4), getData().colors.black);
             back_button.draw();
@@ -193,6 +134,67 @@ public:
     void draw() const override {
 
     }
+
+private:
+    std::vector<History_elem> import_transcript_processing(std::vector<History_elem> n_history, History_elem strt_elem, std::string transcript, bool* failed) {
+        Board h_bd = strt_elem.board;
+        String transcript_str = Unicode::Widen(transcript).replace(U"\r", U"").replace(U"\n", U"").replace(U" ", U"");
+        if (transcript_str.size() % 2 != 0 && transcript_str.size() >= 120) {
+            *failed = true;
+        }
+        else {
+            int y, x;
+            uint64_t legal;
+            Flip flip;
+            History_elem history_elem;
+            int player = strt_elem.player;
+            //history_elem.set(h_bd, player, GRAPH_IGNORE_VALUE, -1, -1, -1, "");
+            //n_history.emplace_back(history_elem);
+            for (int i = 0; i < (int)transcript_str.size(); i += 2) {
+                x = (int)transcript_str[i] - (int)'a';
+                if (x < 0 || HW <= x) {
+                    x = (int)transcript_str[i] - (int)'A';
+                    if (x < 0 || HW <= x) {
+                        *failed = true;
+                        break;
+                    }
+                }
+                y = (int)transcript_str[i + 1] - (int)'1';
+                if (y < 0 || HW <= y) {
+                    *failed = true;
+                    break;
+                }
+                y = HW_M1 - y;
+                x = HW_M1 - x;
+                legal = h_bd.get_legal();
+                if (1 & (legal >> (y * HW + x))) {
+                    calc_flip(&flip, &h_bd, y * HW + x);
+                    h_bd.move_board(&flip);
+                    player ^= 1;
+                    if (h_bd.get_legal() == 0ULL) {
+                        h_bd.pass();
+                        player ^= 1;
+                        if (h_bd.get_legal() == 0ULL) {
+                            h_bd.pass();
+                            player ^= 1;
+                            if (i != transcript_str.size() - 2) {
+                                *failed = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                else {
+                    *failed = true;
+                    break;
+                }
+                n_history.back().next_policy = y * HW + x;
+                history_elem.set(h_bd, player, GRAPH_IGNORE_VALUE, -1, y * HW + x, -1, "");
+                n_history.emplace_back(history_elem);
+            }
+        }
+        return n_history;
+    }
 };
 
 class Import_board : public App::Scene {
@@ -244,6 +246,7 @@ public:
                     return_pressed = true;
                 }
             }
+            str = str.replace(U"\r\n", U"").replace(U"\n", U"").replace(U" ", U"");
             board_str = str.narrow();
             getData().fonts.font(str + U'|' + editingText).draw(15, text_area.stretched(-4), getData().colors.black);
             back_button.draw();
