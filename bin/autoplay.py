06ce3ded73d5a16b839938d42159c5c00abb964a
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-09-03 07:08:13 +0900

    update autoplay script

--- bin/autoplay.py
+++ bin/autoplay.py
@@ -1,13 +1,15 @@
 import subprocess
 from tqdm import trange
 
-IDX_START = 1
+IDX_START = 25
 
 LEVEL = 11
 N_GAMES_PER_FILE = 10000
-N_PARALLEL = 8
-N_THREAD = 32 // N_PARALLEL
-N_ADDITIONAL_DIVISION = 10
+N_THREAD = 32
+
+N_PARALLEL = 4
+N_ADDITIONAL_DIVISION = 20
+N_THREAD_PER_EXE = N_THREAD // N_PARALLEL
 
 N_PLAY_PER_AI = N_GAMES_PER_FILE / N_ADDITIONAL_DIVISION / N_PARALLEL
 if N_PLAY_PER_AI != int(N_PLAY_PER_AI):
@@ -19,7 +21,7 @@ def fill0(n, r):
     res = str(n)
     return '0' * (r - len(res)) + res
 
-cmd = 'Egaroucid_for_Console_6_4_0_x64_SIMD.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD) + ' -selfplay ' + str(N_PLAY_PER_AI)
+cmd = 'Egaroucid_for_Console_6_4_0_x64_SIMD.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD_PER_EXE) + ' -selfplay ' + str(N_PLAY_PER_AI)
 print(cmd)
 
 for idx in range(IDX_START, IDX_START + 1000):
