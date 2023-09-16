979c55c19b6a16ec5ca338aea6e31b12156fbe89
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-09-17 01:47:46 +0900

    update battle

---
+++ web_resources/win_rate_translator.py
@@ -0,0 +1,32 @@
+import pyperclip
+import sys
+
+if sys.argv[1] == 'ja':
+    head = '''<table>
+<tr>
+<th>名称</th>
+<th>勝率</th>
+'''
+else:
+    head = '''<table>
+<tr>
+<th>Name</th>
+<th>Winning Rate</th>
+'''
+
+res = head
+while True:
+    try:
+        data = input().split()
+        name = data[0]
+        winning_rate = data[-1]
+        res += '<tr>\n'
+        res += '<td>' + name + '</td>\n'
+        res += '<td>' + winning_rate + '</td>\n'
+        res += '</tr>\n'
+    except:
+        break
+
+res += '</table>'
+
+pyperclip.copy(res)
