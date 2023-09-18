d99256aa07b0bd751c031ccc86de05b7ebcc6978
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-09-18 23:56:35 +0900

    updating evaluation function

--- src/tools/evaluation/optimizer_phase.py
+++ src/tools/evaluation/optimizer_phase.py
@@ -20,14 +20,14 @@ else:
 
 train_data = [str(elem) + '.dat' for elem in train_data_nums]
 
-train_root_dir = './../../../train_data/bin_data/20230918_2/'
+train_root_dir = './../../../train_data/bin_data/20230918_3/'
 
 #train_dirs = [train_root_dir + str(4 + elem) + '/' for elem in range(int(phase) * 2, int(phase) * 2 + 2)]
 train_dirs = [train_root_dir + str(int(phase)) + '/']
 #train_dirs = [train_root_dir + str(int(phase) * 2) + '/', train_root_dir + str(int(phase) * 2 + 1) + '/']
 
-model_dir = './../../../model/20230918_6/'
-#model_dir = './../../../model/nomodel/'
+#model_dir = './../../../model/20230918_6/'
+model_dir = './../../../model/nomodel/'
 
 additional_params = ''
 for tfile in train_data:
