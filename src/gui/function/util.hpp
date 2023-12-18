688e1daae273051f8142fd55b6a6d6bd3ede59cb
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-12-18 14:22:43 +0900

    update util extension determination

--- src/gui/function/util.hpp
+++ src/gui/function/util.hpp
@@ -14,10 +14,15 @@
 
 std::string get_extension(std::string file){
     std::string res;
+    bool dot_found = false;
     for (int i = (int)file.size() - 1; i >= 0; --i){
-        if (file[i] == '.')
+        if (file[i] == '.'){
+            dot_found = true;
             break;
+        }
         res.insert(0, {file[i]});
     }
-    return res;
+    if (dot_found)
+        return res;
+    return "";
 }
