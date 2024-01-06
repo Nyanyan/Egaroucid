dc514e1c0e576b84b33d8c2ac6a9f561e03835f1
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-01-06 16:52:39 +0900

    update happy_new_year.py

--- src/happy_new_year.py
+++ src/happy_new_year.py
@@ -14,7 +14,17 @@ for file in files:
     with open(file, 'r', encoding='utf-8') as f:
         lines = f.read().splitlines()
     for i, line in enumerate(lines):
-        if '@date' in line:
+        if '#define EGAROUCID_DATE ' in line:
+            spl = line.split()
+            n_line = ''
+            for j in range(len(spl)):
+                if '-' in spl[j] :
+                    sspl = spl[j].split('-')
+                    n_line += sspl[0] + '-' + new_year
+                else:
+                    n_line += spl[j]
+            line = n_line
+        if '@date' in line and '-' in line:
             lines[i] = lines[i][:lines[i].find('-') + 1] + new_year
     new_code = '\n'.join(lines)
     with open(file, 'w', encoding='utf-8') as f:
