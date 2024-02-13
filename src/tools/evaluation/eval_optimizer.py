542d1b6dfce8982c71226b8bf10d58421d503519
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-02-13 22:57:21 +0900

    updating evaluation function, #151

--- src/tools/evaluation/eval_optimizer.py
+++ src/tools/evaluation/eval_optimizer.py
@@ -4,7 +4,7 @@ import sys
 strt_phase = int(sys.argv[1])
 end_phase = int(sys.argv[2])
 
-for phase in range(strt_phase, end_phase):
+for phase in range(strt_phase, end_phase + 1):
     print('optimizing phase', phase)
     cmd = 'python optimizer_phase.py ' + str(phase)
     p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
