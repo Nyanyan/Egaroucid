db865d618557126d2c19fbd3dc531ef6a65c5dc5
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-02-14 10:51:54 +0900

    updating evaluation function, #151

--- src/tools/evaluation/optimizer_phase.py
+++ src/tools/evaluation/optimizer_phase.py
@@ -9,9 +9,9 @@ if len(sys.argv) > 3:
     beta = str(sys.argv[5])
 else:
     hour = '0'
-    minute = '10'
+    minute = '3'
     second = '0'
-    beta = '0.2'
+    beta = '0.3'
 
 if int(phase) < 10:
     train_data_nums = [1, 2, 3, 4, 6, 7, 8, 9, 11, 15, 16, 17, 18, 19, 99]
@@ -25,8 +25,8 @@ train_root_dir = './../../../train_data/bin_data/20240212_1/'
 #train_dirs = [train_root_dir + str(elem) + '/' for elem in range(int(phase) * 2, int(phase) * 2 + 2)]
 train_dirs = [train_root_dir + str(int(phase)) + '/']
 
-model_dir = './../../../model/20240213_1/'
-#model_dir = './../../../model/nomodel/'
+#model_dir = './../../../model/20240213_1/'
+model_dir = './../../../model/nomodel/'
 
 additional_params = ''
 for tfile in train_data:
