041777a790ad11ad4752872a3fc4f5644ffc74f8
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-02-21 23:55:19 +0900

    update Egaroucid for Console

--- src/Egaroucid_console.cpp
+++ src/Egaroucid_console.cpp
@@ -50,6 +50,7 @@ int main(int argc, char* argv[]){
             if (!options.quiet){
                 print_board_info(&board);
                 std::cout << std::endl;
+                //std::cerr << "val " << mid_evaluate(&board.board) << std::endl;
             }
             if (!execute_special_tasks_loop(&board, &state, &options))
                 check_command(&board, &state, &options);
