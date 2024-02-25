946c501506fb29a6ee63a42ac40df8c9c91ae83f
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-02-25 13:10:03 +0900

    add probcut data generator

--- src/console/function.hpp
+++ src/console/function.hpp
@@ -61,11 +61,55 @@ void solve_problems(std::vector<std::string> arg, Options *options, State *state
     std::cout << "total " << total.nodes << " nodes in " << ((double)total.time / 1000) << "s NPS " << (total.nodes * 1000 / total.time) << std::endl;
 }
 
+void tune_probcut_end(){
+    std::ofstream ofs("probcut_end.txt");
+    Board board;
+    Flip flip;
+    Search_result short_ans, long_ans;
+    for (int i = 0; i < 100; ++i){
+        for (int depth = 1; depth < 26; ++depth){
+            board.reset();
+            for (int j = 0; j < HW2 - 4 - depth && board.check_pass(); ++j){ // random move
+                uint64_t legal = board.get_legal();
+                int random_idx = myrandrange(0, pop_count_ull(legal));
+                int t = 0;
+                for (uint_fast8_t cell = first_bit(&legal); legal; cell = next_bit(&legal)){
+                    if (t == random_idx){
+                        calc_flip(&flip, &board, cell);
+                        break;
+                    }
+                    ++t;
+                }
+                board.move_board(&flip);
+            }
+            if (board.check_pass()){
+                int short_depth = mpc_search_depth_arr[1][depth];
+                if (short_depth == 0){
+                    short_ans.value = mid_evaluate(&board);
+                } else{
+                    short_ans = tree_search(board, short_depth, MPC_100_LEVEL, false, true);
+                }
+                long_ans = tree_search(board, depth, MPC_100_LEVEL, false, true);
+                // n_discs short_depth error
+                std::cerr << HW2 - depth << " " << short_depth << " " << abs(short_ans.value - long_ans.value) << std::endl;
+                ofs << HW2 - depth << " " << short_depth << " " << abs(short_ans.value - long_ans.value) << std::endl;
+            }
+        }
+    }
+}
+
 void execute_special_tasks(){
     // move ordering tuning (endsearch)
     #if TUNE_MOVE_ORDERING_END
         std::cout << "tune move ordering (endsearch)" << std::endl;
         tune_move_ordering_end("problem/13_13.txt");
+        std::exit(0);
+    #endif
+
+    // probcut (endsearch)
+    #if TUNE_PROBCUT_END
+        std::cout << "tune probcut (endsearch)" << std::endl;
+        tune_probcut_end();
         std::exit(0);
     #endif
 }
