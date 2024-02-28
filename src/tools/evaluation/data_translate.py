efd403175f26d93a519c7db52846cd44ff272075
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-02-28 09:09:40 +0900

    updating evaluation function, #151

--- src/tools/evaluation/data_translate.py
+++ src/tools/evaluation/data_translate.py
@@ -4,14 +4,14 @@ import glob
 
 bin_root_dir = './../../../train_data/bin_data/20240223_1/'
 input_root_dir = './../../../train_data/board_data/'
-board_sub_dir_nums = [6, 7, 8, 9, 11, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+board_sub_dir_nums = [6, 7, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
 board_n_moves = {}
 board_n_moves['6'] = [20, 60]
 board_n_moves['7'] = [20, 60]
 board_n_moves['8'] = [20, 60]
 board_n_moves['9'] = [20, 60]
-#board_n_moves['10'] = [40, 60]
+board_n_moves['10'] = [40, 60]
 board_n_moves['11'] = [20, 60]
 board_n_moves['15'] = [20, 60]
 board_n_moves['16'] = [19, 60]
@@ -28,7 +28,7 @@ board_n_moves['25'] = [30, 60]
 
 N_PHASES = 60
 
-for phase in range(16, N_PHASES):
+for phase in range(40, N_PHASES):
     bin_dir = bin_root_dir + str(phase)
     try:
         os.mkdir(bin_dir)
