ba8236aa28b4d0b508e24349fc91703255174d2f
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-04-09 13:47:00 +0900

    delete hint lazy smp

--- src/engine/lazy_smp.hpp
+++ src/engine/lazy_smp.hpp
@@ -77,7 +77,7 @@ Search_result lazy_smp(Board board, int depth, uint_fast8_t mpc_level, bool show
                     //std::cerr << sub_thread_idx << " " << sub_depth << " " << SELECTIVITY_PERCENTAGE[sub_mpc_level] << std::endl;
                     searches[sub_thread_idx].init(&board, sub_mpc_level, false, true);
                     bool pushed = false;
-                    parallel_tasks.emplace_back(thread_pool.push(&pushed, std::bind(&nega_scout, &searches[sub_thread_idx], -SCORE_MAX, SCORE_MAX, sub_depth, false, use_legal, sub_is_end_search, &sub_searching)));
+                    parallel_tasks.emplace_back(thread_pool.push(&pushed, std::bind(&nega_scout, &searches[sub_thread_idx], -SCORE_MAX, SCORE_MAX, sub_depth, false, LEGAL_UNDEFINED, sub_is_end_search, &sub_searching)));
                     sub_depth_arr.emplace_back(sub_depth);
                     if (!pushed){
                         parallel_tasks.pop_back();
@@ -161,119 +161,4 @@ Search_result lazy_smp(Board board, int depth, uint_fast8_t mpc_level, bool show
     result.is_end_search = false;
     result.probability = SELECTIVITY_PERCENTAGE[mpc_level];
     return result;
-}
-
-
-void lazy_smp_hint(Board board, int depth, uint_fast8_t mpc_level, bool show_log, uint64_t use_legal, bool use_multi_thread, int n_display, double values[], int hint_types[]){
-    uint64_t strt = tim();
-    int main_depth = 1;
-    int main_mpc_level = mpc_level;
-    const int max_depth = HW2 - board.n_discs();
-    depth = std::min(depth, max_depth);
-    bool is_end_search = (depth == max_depth);
-    if (is_end_search){
-        main_mpc_level = MPC_74_LEVEL;
-    }
-    while (main_depth <= depth && main_mpc_level <= mpc_level && global_searching){
-        bool is_last_search_show_log = (main_depth == depth) && (main_mpc_level == mpc_level) && show_log;
-        bool main_is_end_search = false;
-        if (main_depth >= max_depth){
-            main_is_end_search = true;
-            main_depth = max_depth;
-        }
-        /*
-        std::vector<Lazy_SMP_task> sub_tasks;
-        int sub_depth = main_depth;
-        if (use_multi_thread){
-            for (int i = 1; i < thread_pool.get_n_idle() / 2; ++i){
-                int sub_depth = main_depth + i;
-                int sub_mpc_level = main_mpc_level;
-                bool sub_is_end_search = false;
-                if (sub_depth >= max_depth){
-                    if (main_is_end_search){
-                        sub_mpc_level = main_mpc_level + sub_depth - max_depth;
-                    } else{
-                        sub_mpc_level = sub_depth - max_depth;
-                    }
-                    sub_depth = max_depth;
-                    sub_is_end_search = true;
-                }
-                if (sub_mpc_level <= MPC_100_LEVEL){
-                    Lazy_SMP_task sub_task;
-                    sub_task.mpc_level = sub_mpc_level;
-                    sub_task.depth = sub_depth;
-                    sub_task.is_end_search = sub_is_end_search;
-                    sub_tasks.emplace_back(sub_task);
-                }
-            }
-        }
-        std::vector<std::future<void>> parallel_tasks;
-        bool sub_searching = true;
-        std::vector<Search> searches(sub_tasks.size());
-        for (int i = 0; i < (int)sub_tasks.size(); ++i){
-            bool pushed = false;
-            searches[i].init(&board, sub_tasks[i].mpc_level, false, true);
-            while (!pushed){
-                parallel_tasks.emplace_back(thread_pool.push(&pushed, std::bind(&first_nega_scout_hint_sub_thread, &searches[i], sub_tasks[i].depth, sub_tasks[i].is_end_search, use_legal, &sub_searching)));
-                if (!pushed){
-                    parallel_tasks.pop_back();
-                }
-            }
-        }
-        */
-        Search main_search;
-        //main_search.init(&board, main_mpc_level, use_multi_thread, parallel_tasks.size() != 0);
-        main_search.init(&board, main_mpc_level, use_multi_thread, false);
-        bool searching = true;
-        int hint_type = main_depth;
-        if (main_is_end_search){ // endgame & this is last search
-            hint_type = SELECTIVITY_PERCENTAGE[main_mpc_level];
-        }
-        uint64_t use_legal_copy = use_legal;
-        first_nega_scout_hint(&main_search, main_depth, depth, main_is_end_search, use_legal, &searching, values, hint_types, hint_type, n_display);
-        /*
-        sub_searching = false;
-        for (std::future<void> &task: parallel_tasks){
-            task.get();
-        }
-        */
-        if (is_last_search_show_log){
-            std::cerr << "main ";
-        } else{
-            std::cerr << "pre ";
-        }
-        if (main_is_end_search){
-            std::cerr << "end ";
-        } else{
-            std::cerr << "mid ";
-        }
-        //std::cerr << "depth " << main_depth << "@" << SELECTIVITY_PERCENTAGE[main_mpc_level] << "%" << " n_worker " << sub_tasks.size() << std::endl;
-        std::cerr << "depth " << main_depth << "@" << SELECTIVITY_PERCENTAGE[main_mpc_level] << "%" << std::endl;
-        if (show_log){
-            for (int y = 0; y < HW; ++y){
-                for (int x = 0; x < HW; ++x){
-                    int cell = HW2_M1 - y * 8 - x;
-                    if (1 & (board.get_legal() >> cell))
-                        std::cerr << round(values[cell]) << " ";
-                    else
-                        std::cerr << "  ";
-                }
-                std::cerr << std::endl;
-            }
-        }
-        if (!is_end_search || main_depth < depth - LAZYSMP_ENDSEARCH_PRESEARCH_OFFSET){
-            ++main_depth;
-        } else{
-            if (main_depth < depth){
-                main_depth = depth;
-                main_mpc_level = MPC_74_LEVEL;
-            } else{
-                if (main_mpc_level == MPC_88_LEVEL && mpc_level > MPC_88_LEVEL){
-                    main_mpc_level = mpc_level;
-                } else{
-                    ++main_mpc_level;
-                }
-            }
-        }
-    }
 }
