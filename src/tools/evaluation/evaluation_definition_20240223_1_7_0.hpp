0cfaeb4131d82ca5dd381de2632e7d92dc54867b
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-04-09 23:40:19 +0900

    refactoring evaluation definition

--- src/tools/evaluation/evaluation_definition_20240223_1_7_0.hpp
+++ src/tools/evaluation/evaluation_definition_20240223_1_7_0.hpp
@@ -12,21 +12,10 @@
 #ifndef OPTIMIZER_INCLUDE
     #include "./../../engine/board.hpp"
 #endif
+#include "evaluation_definition_common.hpp"
 
 #define EVAL_DEFINITION_NAME "20240223_1"
 #define EVAL_DEFINITION_DESCRIPTION "6.5's pattern + n_discs_of_player + n_surround_of_both"
-
-#ifndef HW
-    #define HW 8
-#endif
-
-#ifndef HW2
-    #define HW2 64
-#endif
-
-#ifndef SCORE_MAX
-    #define SCORE_MAX 64
-#endif
 
 /*
     @brief evaluation pattern definition
@@ -45,7 +34,7 @@
 #define ADJ_MAX_EVALUATE_IDX 59049
 #define ADJ_N_EVAL (16 + 2)
 #define ADJ_N_FEATURES (62 + 2)
-#define N_FLOOR_UNIQUE_FEATURES 16 // floorpow2(ADJ_N_EVAL): 16-31->16 32-63->32
+//#define N_FLOOR_UNIQUE_FEATURES 16 // floorpow2(ADJ_N_EVAL): 16-31->16 32-63->32
 
 // phase
 #define ADJ_N_PHASES 60
@@ -60,105 +49,6 @@
 */
 #define ADJ_STEP 32
 #define ADJ_STEP_2 16
-
-#ifndef PNO
-    /*
-        @brief 3 ^ N definition
-    */
-    #define PNO 0
-    #define P30 1
-    #define P31 3
-    #define P32 9
-    #define P33 27
-    #define P34 81
-    #define P35 243
-    #define P36 729
-    #define P37 2187
-    #define P38 6561
-    #define P39 19683
-    #define P310 59049
-#endif
-
-#ifndef COORD_NO
-    /*
-        @brief coordinate definition
-    */
-    #define COORD_A1 63
-    #define COORD_B1 62
-    #define COORD_C1 61
-    #define COORD_D1 60
-    #define COORD_E1 59
-    #define COORD_F1 58
-    #define COORD_G1 57
-    #define COORD_H1 56
-
-    #define COORD_A2 55
-    #define COORD_B2 54
-    #define COORD_C2 53
-    #define COORD_D2 52
-    #define COORD_E2 51
-    #define COORD_F2 50
-    #define COORD_G2 49
-    #define COORD_H2 48
-
-    #define COORD_A3 47
-    #define COORD_B3 46
-    #define COORD_C3 45
-    #define COORD_D3 44
-    #define COORD_E3 43
-    #define COORD_F3 42
-    #define COORD_G3 41
-    #define COORD_H3 40
-
-    #define COORD_A4 39
-    #define COORD_B4 38
-    #define COORD_C4 37
-    #define COORD_D4 36
-    #define COORD_E4 35
-    #define COORD_F4 34
-    #define COORD_G4 33
-    #define COORD_H4 32
-
-    #define COORD_A5 31
-    #define COORD_B5 30
-    #define COORD_C5 29
-    #define COORD_D5 28
-    #define COORD_E5 27
-    #define COORD_F5 26
-    #define COORD_G5 25
-    #define COORD_H5 24
-
-    #define COORD_A6 23
-    #define COORD_B6 22
-    #define COORD_C6 21
-    #define COORD_D6 20
-    #define COORD_E6 19
-    #define COORD_F6 18
-    #define COORD_G6 17
-    #define COORD_H6 16
-
-    #define COORD_A7 15
-    #define COORD_B7 14
-    #define COORD_C7 13
-    #define COORD_D7 12
-    #define COORD_E7 11
-    #define COORD_F7 10
-    #define COORD_G7 9
-    #define COORD_H7 8
-
-    #define COORD_A8 7
-    #define COORD_B8 6
-    #define COORD_C8 5
-    #define COORD_D8 4
-    #define COORD_E8 3
-    #define COORD_F8 2
-    #define COORD_G8 1
-    #define COORD_H8 0
-
-    #define COORD_NO 64
-#endif
-
-constexpr int adj_pow3[11] = {P30, P31, P32, P33, P34, P35, P36, P37, P38, P39, P310};
 
 /*
     @brief definition of patterns in evaluation function
@@ -291,8 +181,10 @@ constexpr int adj_rev_patterns[ADJ_N_PATTERNS][ADJ_MAX_PATTERN_CELLS] = {
 };
 
 constexpr int adj_eval_sizes[ADJ_N_EVAL] = {
-    P38, P38, P38, P35, P36, P37, P38, P39, 
-    P310, P310, P310, P310, P310, P310, P310, P310, 
+    P38, P38, P38, P35, 
+    P36, P37, P38, P39, 
+    P310, P310, P310, P310, 
+    P310, P310, P310, P310, 
     ADJ_MAX_STONE_NUM, 
     ADJ_MAX_SURROUND * ADJ_MAX_SURROUND
 };
--- src/tools/evaluation/evaluation_definition_20240301_4_move_ordering_end_nws.hpp
+++ src/tools/evaluation/evaluation_definition_20240301_4_move_ordering_end_nws.hpp
@@ -12,21 +12,10 @@
 #ifndef OPTIMIZER_INCLUDE
     #include "./../../engine/board.hpp"
 #endif
+#include "evaluation_definition_common.hpp"
 
 #define EVAL_DEFINITION_NAME "20240301_4_move_ordering_end_nws"
 #define EVAL_DEFINITION_DESCRIPTION "evaluation function for move ordering"
-
-#ifndef HW
-    #define HW 8
-#endif
-
-#ifndef HW2
-    #define HW2 64
-#endif
-
-#ifndef SCORE_MAX
-    #define SCORE_MAX 64
-#endif
 
 /*
     @brief evaluation pattern definition
@@ -57,105 +46,6 @@
 */
 #define ADJ_STEP 32
 #define ADJ_STEP_2 16
-
-#ifndef PNO
-    /*
-        @brief 3 ^ N definition
-    */
-    #define PNO 0
-    #define P30 1
-    #define P31 3
-    #define P32 9
-    #define P33 27
-    #define P34 81
-    #define P35 243
-    #define P36 729
-    #define P37 2187
-    #define P38 6561
-    #define P39 19683
-    #define P310 59049
-#endif
-
-#ifndef COORD_NO
-    /*
-        @brief coordinate definition
-    */
-    #define COORD_A1 63
-    #define COORD_B1 62
-    #define COORD_C1 61
-    #define COORD_D1 60
-    #define COORD_E1 59
-    #define COORD_F1 58
-    #define COORD_G1 57
-    #define COORD_H1 56
-
-    #define COORD_A2 55
-    #define COORD_B2 54
-    #define COORD_C2 53
-    #define COORD_D2 52
-    #define COORD_E2 51
-    #define COORD_F2 50
-    #define COORD_G2 49
-    #define COORD_H2 48
-
-    #define COORD_A3 47
-    #define COORD_B3 46
-    #define COORD_C3 45
-    #define COORD_D3 44
-    #define COORD_E3 43
-    #define COORD_F3 42
-    #define COORD_G3 41
-    #define COORD_H3 40
-
-    #define COORD_A4 39
-    #define COORD_B4 38
-    #define COORD_C4 37
-    #define COORD_D4 36
-    #define COORD_E4 35
-    #define COORD_F4 34
-    #define COORD_G4 33
-    #define COORD_H4 32
-
-    #define COORD_A5 31
-    #define COORD_B5 30
-    #define COORD_C5 29
-    #define COORD_D5 28
-    #define COORD_E5 27
-    #define COORD_F5 26
-    #define COORD_G5 25
-    #define COORD_H5 24
-
-    #define COORD_A6 23
-    #define COORD_B6 22
-    #define COORD_C6 21
-    #define COORD_D6 20
-    #define COORD_E6 19
-    #define COORD_F6 18
-    #define COORD_G6 17
-    #define COORD_H6 16
-
-    #define COORD_A7 15
-    #define COORD_B7 14
-    #define COORD_C7 13
-    #define COORD_D7 12
-    #define COORD_E7 11
-    #define COORD_F7 10
-    #define COORD_G7 9
-    #define COORD_H7 8
-
-    #define COORD_A8 7
-    #define COORD_B8 6
-    #define COORD_C8 5
-    #define COORD_D8 4
-    #define COORD_E8 3
-    #define COORD_F8 2
-    #define COORD_G8 1
-    #define COORD_H8 0
-
-    #define COORD_NO 64
-#endif
-
-constexpr int adj_pow3[11] = {P30, P31, P32, P33, P34, P35, P36, P37, P38, P39, P310};
 
 /*
     @brief definition of patterns in evaluation function
@@ -196,7 +86,9 @@ constexpr Adj_Feature_to_coord adj_feature_to_coord[ADJ_N_SYMMETRY_PATTERNS] = {
     {10, {COORD_H8, COORD_G7, COORD_F6, COORD_E5, COORD_G8, COORD_F7, COORD_E6, COORD_H7, COORD_G6, COORD_F5}}  // 45
 };
 
-constexpr int adj_pattern_n_cells[ADJ_N_PATTERNS] = {10, 10, 10, 10};
+constexpr int adj_pattern_n_cells[ADJ_N_PATTERNS] = {
+    10, 10, 10, 10
+};
 
 constexpr int adj_rev_patterns[ADJ_N_PATTERNS][ADJ_MAX_PATTERN_CELLS] = {
     {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, // 8 edge + 2x
