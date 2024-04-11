8a14a9835db94564b16321065bdc3f497cad3d80
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-04-12 00:27:59 +0900

    update script

--- bin/tools/count_transcript.py
+++ bin/tools/count_transcript.py
@@ -1,12 +1,12 @@
 import glob
 from tqdm import trange
 
-dr = './../../train_data/transcript/records2/*.txt'
-
+dr = './../../train_data/transcript/records22/*.txt'
+print(dr)
 files = glob.glob(dr)
 
 n_games = 0
 for file in files:
     with open(file, 'r') as f:
         n_games += len(f.read().splitlines())
-print(n_games)
+print(n_games, 'games')
