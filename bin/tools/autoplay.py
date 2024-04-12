375478c539944621354efee96a0e03358d159cc7
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-04-12 14:20:39 +0900

    update autoplay script

--- bin/tools/autoplay.py
+++ bin/tools/autoplay.py
@@ -5,26 +5,24 @@ import os
 
 n_random_moves = int(sys.argv[1])
 
-IDX_START = 7
-IDX_END = 100
+IDX_START = 0
+IDX_END = 0
 
-LEVEL = 11
+LEVEL = 15
 N_GAMES_PER_FILE = 10000
-N_THREAD = 31
+N_THREAD = 32
 
 def fill0(n, r):
     res = str(n)
     return '0' * (r - len(res)) + res
 
-#cmd = 'Egaroucid_for_Console_6_4_0_x64_SIMD.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD_PER_EXE) + ' -selfplay ' + str(N_PLAY_PER_AI)
-#cmd = 'Egaroucid_for_Console.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD + 1) + ' -selfplay ' + str(N_GAMES_PER_FILE)
-cmd = 'versions/Egaroucid_for_Console_6_5_X/Egaroucid_for_Console.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD) + ' -selfplay ' + str(N_GAMES_PER_FILE) + ' ' + str(n_random_moves)
+cmd = './../versions/Egaroucid_for_Console_beta/Egaroucid_for_Console.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD) + ' -selfplay ' + str(N_GAMES_PER_FILE) + ' ' + str(n_random_moves)
 print(cmd)
 
 for idx in range(IDX_START, IDX_END + 1):
     print(fill0(idx, 7))
     #with open('transcript/' + fill0(idx, 7) + '.txt', 'w') as f:
-    dr = 'transcript/' + str(n_random_moves)
+    dr = './../transcript/' + str(n_random_moves)
     try:
         os.mkdir(dr)
     except:
--- bin/tools/autoplay_line.py
+++ bin/tools/autoplay_line.py
@@ -19,14 +19,11 @@ def fill0(n, r):
     res = str(n)
     return '0' * (r - len(res)) + res
 
-#cmd = 'Egaroucid_for_Console_6_4_0_x64_SIMD.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD_PER_EXE) + ' -selfplay ' + str(N_PLAY_PER_AI)
-#cmd = 'Egaroucid_for_Console.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD + 1) + ' -selfplay ' + str(N_GAMES_PER_FILE)
-
 for idx in range(IDX_START, IDX_END + 1):
     print(fill0(idx, 7))
-    file = 'problem/first11_all/' + fill0(idx, 7) + '.txt'
-    cmd = 'versions/Egaroucid_for_Console_6_5_X/Egaroucid_for_Console.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD) + ' -selfplayline ' + file
-    with open('transcript/first11/' + fill0(idx, 7) + '.txt', 'w') as f:
+    file = './../problem/first11_all/' + fill0(idx, 7) + '.txt'
+    cmd = './../versions/Egaroucid_for_Console_6_5_X/Egaroucid_for_Console.exe -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD) + ' -selfplayline ' + file
+    with open('./../transcript/first11/' + fill0(idx, 7) + '.txt', 'w') as f:
         egaroucid = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
         for i in trange(N_GAMES_PER_FILE):
             line = egaroucid.stdout.readline().decode().replace('\r', '').replace('\n', '') + '\n'
