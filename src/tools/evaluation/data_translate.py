b5559119f447e7e59548d7a3a24d00fcbf67a615
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-05-25 13:22:31 +0900

    add testdata

--- src/tools/evaluation/data_translate.py
+++ src/tools/evaluation/data_translate.py
@@ -8,7 +8,7 @@ bin_root_dir = './../../../train_data/bin_data/20240223_1/'
 exe = 'data_board_to_idx.out'
 N_PHASES = 60
 #board_sub_dir_nums = [6, 7, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27]
-board_sub_dir_nums = [36, 37, 38]
+board_sub_dir_nums = [35]
 #'''
 '''
 # 7.0 move ordering_end_nws
