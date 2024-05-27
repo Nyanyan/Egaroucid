938307923848175e778603e2dbc7d5e6a3cd9a65
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-05-28 02:01:49 +0900

    ssc prediction not very good

--- src/tools/state_space_complexity/README.md
+++ src/tools/state_space_complexity/README.md
@@ -1,3 +1,27 @@
 # State Space Complexity
 
 状態空間複雑性の推定
+
+```
+$ calc_max_n_legal.out
+silhouette done in 51675 ms
+n_discs 4 max_n_legal 4 board 810000000 1008000000
+n_discs 5 max_n_legal 3 board 1000000000 818080000
+n_discs 6 max_n_legal 5 board 808080000 1010100000
+n_discs 7 max_n_legal 6 board 1008000000 8100e0000
+n_discs 8 max_n_legal 9 board 80c000000 10101c0000
+n_discs 9 max_n_legal 11 board 418100000 81820080000
+n_discs 10 max_n_legal 12 board 838200000 1004181000
+n_discs 11 max_n_legal 14 board 418101000 81820280000
+n_discs 12 max_n_legal 15 board 8382000 1c14041000
+n_discs 13 max_n_legal 16 board 1204080800 838161000
+n_discs 14 max_n_legal 18 board 18182000 1c24641000
+n_discs 15 max_n_legal 20 board c0818100000 1e001620080000
+n_discs 16 max_n_legal 21 board 201818000000 101864241c0000
+n_discs 17 max_n_connected 29 bits 163e5a303800 time 3977 ms
+n_discs 18 max_n_connected 30 bits 163e5a307800 time 43071 ms
+n_discs 19 max_n_connected 31 bits 2163e5a307800 time 200433 ms
+n_discs 20 max_n_connected 32 bits a163e5a307800 time 668632 ms
+
+```
+
--- src/tools/state_space_complexity/state_space_complexity.py
+++ src/tools/state_space_complexity/state_space_complexity.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from scipy.special import comb
 import math
 
+n_state_arr_exact = [1, 1, 3, 14, 60, 322, 1773, 10649, 67245, 434029, 2958586, 19786627, 137642461]
+
 def nCr(n, r):
     return comb(n, r, exact=True)
 
@@ -13,11 +15,21 @@ def f1(p):
 
 def e(n):
     embedded_values = [
-        0, 0, 0, 0, 1, 3, 5, 
+        0, 0, 0, 0, 1, 3, 5, 6, 9, 11, 
+        12, 14, 15, 16, 18, 20, 21, 29, 30, 31, 
+        32 
     ]
     if n < len(embedded_values):
         return embedded_values[n]
-    return 64 - n
+    return min(33, 64 - n)
+
+def f2(p):
+    if p < len(n_state_arr_exact):
+        return n_state_arr_exact[p]
+    prod = n_state_arr_exact[-1]
+    for n in range(4 + len(n_state_arr_exact), 4 + p + 1):
+        prod *= e(n)
+    return (2 ** 4) * prod * (2 ** p)
 
 p_arr = list(range(61))
 
@@ -29,10 +41,13 @@ n_state_arr = [1 for _ in range(61)]
 for p in range(61):
     n_state_arr[p] = f1(p)
 
-n_state_arr_exact = [1, 1, 3, 14, 60, 322, 1773, 10649, 67245, 434029, 2958586, 19786627, 137642461]
+n_state_arr2 = [1 for _ in range(61)]
+for p in range(61):
+    n_state_arr2[p] = f2(p)
 
 plt.plot(p_arr, n_state_arr_upper_const, label=r'$2^4 \times 3^{60}$')
 plt.plot(p_arr, n_state_arr, label=r'$2^4 \times {}_{60}\mathrm{C}_p \times 2^p$')
+plt.plot(p_arr, n_state_arr2, label=r'$2^4 \times \prod_{n=4}^{4+p}e(n) \times 2^p$')
 plt.plot(range(len(n_state_arr_exact)), n_state_arr_exact, label='exact')
 plt.yscale('log')
 plt.grid()
