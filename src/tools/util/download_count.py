f33819255e5b5a0ae99633781b8d564d4d38df3a
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-07-26 00:02:46 +0900

    update download count

--- src/tools/util/download_count.py
+++ src/tools/util/download_count.py
@@ -6,9 +6,12 @@ total_download_count = 0
 for item in reversed(r.json()):
     print("tag_name: ",item["tag_name"])
     print("name: ", item["name"])
-    print(item["assets"][0]["url"])
-    print("download count: ", item["assets"][0]["download_count"])
-    total_download_count += int(item["assets"][0]["download_count"])
+    subtotal = 0
+    for i in range(len(item["assets"])):
+        print("download count:", item["assets"][i]["download_count"], " name:", item["assets"][i]["name"])
+        total_download_count += int(item["assets"][i]["download_count"])
+        subtotal += int(item["assets"][i]["download_count"])
+    print('subtotal:', subtotal)
     print("")
 
 print('total:', total_download_count)
