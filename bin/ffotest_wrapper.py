a6abf2a16ad0b60297367b5fd6fd6c6edf5f52de
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-08-15 16:12:37 +0900

    update ffotest script

--- bin/ffotest_wrapper.py
+++ bin/ffotest_wrapper.py
@@ -12,28 +12,25 @@ tasks = [ # is_egaroucid, start, end, n_threads, hash_level, exe, CPU, revision,
     [False, 40, 59, 32, 25, 'versions/edax_4_5_2/wEdax-x86.exe',                                                'Core_i9-13900K', 'x86',            '012_ffo40_59_Core_i9-13900K_edax_x86.txt'],
 
     #60-79
-    [True,  60, 79, 42, 27, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_SIMD.exe',     'Core_i9-13900K', 'x64_SIMD',       '020_ffo60_79_Core_i9-13900K_x64_SIMD.txt'],
+    #[True,  60, 79, 42, 27, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_SIMD.exe',     'Core_i9-13900K', 'x64_SIMD',       '020_ffo60_79_Core_i9-13900K_x64_SIMD.txt'],
 
-    [False, 60, 79, 32, 27, 'versions/edax_4_5_2/wEdax-x64-modern.exe',                                         'Core_i9-13900K', 'x64_modern',     '030_ffo60_79_Core_i9-13900K_edax_x64_modern.txt'],
+    #[False, 60, 79, 32, 27, 'versions/edax_4_5_2/wEdax-x64-modern.exe',                                         'Core_i9-13900K', 'x64_modern',     '030_ffo60_79_Core_i9-13900K_edax_x64_modern.txt'],
 ]
 
 '''
 # Core i9-11900K
 tasks = [ # start, end, n_threads, hash_level, exe, out_file
-    [True,  40, 59, 16, 25, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_AVX512.exe',   '10_ffo40_59_Core_i9-11900K_x64_AVX512.txt'],
-    [True,  40, 59, 16, 25, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_SIMD.exe',     '11_ffo40_59_Core_i9-11900K_x64_SIMD.txt'],
-    [True,  40, 59, 16, 25, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_Generic.exe',  '12_ffo40_59_Core_i9-11900K_x64_Generic.txt'],
-    [True,  40, 59, 16, 25, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x86_Generic.exe',  '13_ffo40_59_Core_i9-11900K_x86_Generic.txt'],
+    #40-59
+    [True,  40, 59, 16, 25, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_AVX512.exe',   'Core_i9-11900K', 'x64_AVX512',         '100_ffo40_59_Core_i9-11900K_x64_AVX512.txt'],
+    [True,  40, 59, 16, 25, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_SIMD.exe',     'Core_i9-11900K', 'x64_SIMD',           '101_ffo40_59_Core_i9-11900K_x64_SIMD.txt'],
+    [True,  40, 59, 16, 25, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_Generic.exe',  'Core_i9-11900K', 'x64_Generic',        '102_ffo40_59_Core_i9-11900K_x64_Generic.txt'],
+    [True,  40, 59, 16, 25, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x86_Generic.exe',  'Core_i9-11900K', 'x86_Generic',        '103_ffo40_59_Core_i9-11900K_x86_Generic.txt'],
 
-    [True,  60, 79, 16, 27, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_AVX512.exe',   '30_ffo60_79_Core_i9-11900K_x64_AVX512.txt'],
-    [True,  60, 79, 16, 27, 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_7_3_0_x64_SIMD.exe',     '31_ffo60_79_Core_i9-11900K_x64_SIMD.txt'],
+    [False, 40, 59, 16, 25, 'versions/edax_4_5_2/wEdax-x64-avx512.exe',                                         'Core_i9-11900K', 'x64_avx512',         '110_ffo40_59_Core_i9-11900K_edax_x64_avx512.txt'],
+    [False, 40, 59, 16, 25, 'versions/edax_4_5_2/wEdax-x64-modern.exe',                                         'Core_i9-11900K', 'x64_modern',         '111_ffo40_59_Core_i9-11900K_edax_x64_modern.txt'],
+    [False, 40, 59, 16, 25, 'versions/edax_4_5_2/wEdax-x64.exe',                                                'Core_i9-11900K', 'x64',                '112_ffo40_59_Core_i9-11900K_edax_x64.txt'],
+    [False, 40, 59, 16, 25, 'versions/edax_4_5_2/wEdax-x86.exe',                                                'Core_i9-11900K', 'x86',                '113_ffo40_59_Core_i9-11900K_edax_x86.txt'],
 ]
-#14: edax x64 AVX512
-#15: edax x64 modern
-#16: edax x64
-#17: edax x86
-#32: edax x64 avx512
-#33: edax x64 modern
 '''
 
 import datetime
@@ -57,10 +54,10 @@ for is_egaroucid, start, end, n_threads, hash_level, exe, cpu, revision, out_fil
     summary = ''
     if is_egaroucid:
         info_line = res.splitlines()[-1].split()
-        tim = info_line[4]
+        tim = info_line[4][:-1]
         n_nodes = info_line[1]
         nps = info_line[6]
-        summary = 'Egaroucid ' + cpu + ' ' + revision + ' ' + tim + ' ' + n_nodes + ' ' + nps + ' ' + out_file + '\n'
+        summary = 'Egaroucid,' + cpu + ',' + revision + ',' + tim + ',' + n_nodes + ',' + nps + ',' + out_file + '\n'
     else:
         info_line = res.splitlines()[-4].replace('(', '').split()
         tim_str = info_line[4]
@@ -68,7 +65,7 @@ for is_egaroucid, start, end, n_threads, hash_level, exe, cpu, revision, out_fil
         tim = str(float(tim_str.split(':')[1]) + tim_min * 60)
         n_nodes = info_line[1]
         nps = info_line[5]
-        summary = 'Edax ' + cpu + ' ' + revision + ' ' + tim + ' ' + n_nodes + ' ' + nps + ' ' + out_file + '\n'
+        summary = 'Edax,' + cpu + ',' + revision + ',' + tim + ',' + n_nodes + ',' + nps + ',' + out_file + '\n'
     with open('ffotest_result/' + out_file, 'w') as f:
         f.write(res)
     with open(summary_file, 'a') as f:
