7d4e1c8fdd55920a8c6449f922beb088cf5b73cd
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-08-15 14:50:47 +0900

    update ffotest cmd

--- bin/ffotest.py
+++ bin/ffotest.py
@@ -1,52 +1,135 @@
+# The FFO endgame test suite
+# http://radagast.se/othello/ffotest.html
+# https://github.com/abulmo/edax-reversi/tree/master/problem
+
 import subprocess
 import sys
 
-answer = '''#40 38  a2
-#41 0   h4
-#42 6   g2
-#43 -12 c7  g3
-#44 -14 d2  b8
-#45 6   b2
-#46 -8  b3
-#47 4   g2
-#48 28  f6
-#49 16  e1
-#50 10  d8
-#51 6   e2  a3
-#52 0   a3
-#53 -2  d8
-#54 -2  c7
-#55 0   g6  g4  b7  e2
-#56 2   h5
-#57 -10 a6
-#58 4   g1
-#59 64  g8  h4  e8'''
+print('usage: python ffotest.py [start=40] [end=59] [n_threads=42] [hash_level=25] [exe=Egaroucid_for_Console.exe]')
 
-answer = answer.splitlines()
+start = 40
+if len(sys.argv) >= 2:
+    start = int(sys.argv[1])
 
-cmd = 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console.exe -l 60 -hash 25 -nobook -solve problem/ffo40-59.txt -thread 40'
-if len(sys.argv) == 2:
-    eval_file = sys.argv[1]
-    print('eval ', eval_file)
-    cmd += ' -eval ' + eval_file
+end = 59
+if len(sys.argv) >= 3:
+    end = int(sys.argv[2])
+
+n_threads = 42
+if len(sys.argv) >= 4:
+    n_threads = int(sys.argv[3])
+
+hash_level = 25
+if len(sys.argv) >= 5:
+    n_threads = int(sys.argv[4])
+
+exe = 'Egaroucid_for_Console.exe'
+if len(sys.argv) >= 6:
+    exe = sys.argv[5]
+
+cmd = 'versions/Egaroucid_for_Console_beta/' + exe + ' -l 60 -hash ' + str(hash_level) + ' -nobook -solve problem/ffo' + str(start) + '-' + str(end) + '.txt -thread ' + str(n_threads)
+
 print(cmd)
 
 egaroucid = subprocess.Popen((cmd).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
+answers = [ # best score, [best move, best move...]
+    [], 
+    [18, ['g8']], #1
+    [10, ['a4']], #2
+    [2, ['d1']], #3
+    [0, ['h8', 'a5']], #4
+    [32, ['g8']], #5
+    [14, ['a1', 'h3']], #6
+    [8, ['a6']], #7
+    [8, ['e1']], #8
+    [-8, ['g7', 'a4']], #9
+    [10, ['b2']], #10
+    [30, ['b3']], #11
+    [-8, ['b7']], #12
+    [14, ['b7']], #13
+    [18, ['a3']], #14
+    [4, ['g3', 'b8']], #15
+    [24, ['f8']], #16
+    [8, ['f8']], #17
+    [-2, ['g2']], #18
+    [8, ['b6']], #19
+    [6, ['h5']], #20
+    [0, ['g5']], #21
+    [2, ['g8']], #22
+    [4, ['a2']], #23
+    [0, ['c3']], #24
+    [0, ['g1', 'a5']], #25
+    [0, ['d8']], #26
+    [-2, ['b7']], #27
+    [0, ['f1', 'b2', 'e1']], #28
+    [10, ['g2']], #29
+    [0, ['g3']], #30
+    [-2, ['g6']], #31
+    [-4, ['g3']], #32
+    [-8, ['e7', 'a3']], #33
+    [-2, ['c2']], #34
+    [0, ['c7']], #35
+    [0, ['b7']], #36
+    [-20, ['g2']], #37
+    [4, ['b2']], #38
+    [64, ['a8', 'b1', 'g1', 'g5', 'g6', 'c8', 'h3', 'e8', 'h4']], #39
+    [38, ['a2']], #40
+    [0, ['h4']], #41
+    [6, ['g2']], #42
+    [-12, ['g3', 'c7']], #43
+    [-14, ['d2', 'b8']], #44
+    [6, ['b2']], #45
+    [-8, ['b3']], #46
+    [4, ['g2']], #47
+    [28, ['f6']], #48
+    [16, ['e1']], #49
+    [10, ['d8']], #50
+    [6, ['e2', 'a3']], #51
+    [0, ['a3']], #52
+    [-2, ['d8']], #53
+    [-2, ['c7']], #54
+    [0, ['g6', 'b7', 'e2', 'g4']], #55
+    [2, ['h5']], #56
+    [-10, ['a6']], #57
+    [4, ['g1']], #58
+    [64, ['h4', 'g8', 'e8']], #59
+    [20, ['c2']], #60
+    [-14, ['h3', 'g1']], #61
+    [28, ['e8']], #62
+    [-2, ['f2']], #63
+    [20, ['b4']], #64
+    [10, ['g1']], #65
+    [30, ['h3']], #66
+    [22, ['h3']], #67
+    [28, ['e8']], #68
+    [0, ['h3']], #69
+    [-24, ['e3']], #70
+    [20, ['d2']], #71
+    [24, ['e1']], #72
+    [-4, ['g4']], #73
+    [-30, ['f1']], #74
+    [14, ['d2']], #75
+    [32, ['a3']], #76
+    [34, ['b7']], #77
+    [8, ['f1']], #78
+    [64, ['d7']], #79
+]
+
 res = ''
 line = egaroucid.stdout.readline().decode().replace('\n', '').replace('\r', '')
 print('#   ' + line)
-for i in range(20):
+for i in range(start, end + 1):
     line = egaroucid.stdout.readline().decode().replace('\n', '').replace('\r', '')
     policy = line.split()[3][:-1]
-    policies = answer[i].split()[2:]
-    if not (policy in policies):
+    correct_policies = answers[i][1]
+    if not (policy in correct_policies):
         line += ' WRONG POLICY'
     score = line.split()[4][:-1]
-    correct_score = answer[i].split()[1]
+    correct_score = answers[i][0]
     if int(score) != int(correct_score):
         line += ' WRONG SCORE'
-    line = '#' + str(40 + i) + ' ' + line
+    line = '#' + str(i) + ' ' + line
     print(line)
     res += line + '\n'
 line = egaroucid.stdout.readline().decode().replace('\n', '').replace('\r', '')
