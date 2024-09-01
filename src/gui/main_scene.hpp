b695ca5a6eff2ca39fab3b8027babdef04b1ce38
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-09-01 17:00:12 +0900

    add shortcuts #286

--- src/gui/main_scene.hpp
+++ src/gui/main_scene.hpp
@@ -464,7 +464,7 @@ private:
             need_start_game_button_calculation();
             pausing_in_pass = false;
         }
-        if ((getData().menu_elements.analyze || KeyA.down()) && !ai_status.ai_thinking && !ai_status.analyzing) {
+        if ((getData().menu_elements.analyze || shortcut_key == U"analyze") && !ai_status.ai_thinking && !ai_status.analyzing) {
             stop_calculating();
             resume_calculating();
             init_analyze();
