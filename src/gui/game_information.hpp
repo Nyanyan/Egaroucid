d2df49a2c6f0f33d8d83d40f6020c832e77b6974
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-09-13 22:02:03 +0900

    adding game information scene #335

--- src/gui/game_information.hpp
+++ src/gui/game_information.hpp
@@ -20,13 +20,17 @@ private:
     Scroll_horizontal_manager scroll_manager_black;
     Scroll_horizontal_manager scroll_manager_white;
     Scroll_manager scroll_manager_memo;
+    int n_lines_memo;
 
 public:
     Game_information_scene(const InitData& init) : IScene{ init } {
         back_button.init(GO_BACK_BUTTON_BACK_SX, GO_BACK_BUTTON_SY, GO_BACK_BUTTON_WIDTH, GO_BACK_BUTTON_HEIGHT, GO_BACK_BUTTON_RADIUS, language.get("common", "back"), 25, getData().fonts.font, getData().colors.white, getData().colors.black);
         edit_button.init(GO_BACK_BUTTON_GO_SX, GO_BACK_BUTTON_SY, GO_BACK_BUTTON_WIDTH, GO_BACK_BUTTON_HEIGHT, GO_BACK_BUTTON_RADIUS, language.get("common", "edit"), 25, getData().fonts.font, getData().colors.white, getData().colors.black);
-        scroll_manager_black.init(X_CENTER - EXPORT_GAME_PLAYER_WIDTH, 80 + EXPORT_GAME_RADIUS * 2, EXPORT_GAME_PLAYER_WIDTH - 10, 10, 20, getData().game_information.black_player_name.size(), 1, X_CENTER - EXPORT_GAME_PLAYER_WIDTH, 80, EXPORT_GAME_PLAYER_WIDTH - 10, EXPORT_GAME_RADIUS * 2 + 10);
-        scroll_manager_memo.init(X_CENTER + 300, 140, 10, 250, 20, 20, 1, X_CENTER - 300, 140, 600 + 10, 250);
+        scroll_manager_black.init(X_CENTER - 300, 80 + 15 * 2, 300 - 10, 10, 20, getData().game_information.black_player_name.size(), 1, X_CENTER - 300, 80, 300 - 10, 15 * 2 + 10);
+        scroll_manager_white.init(X_CENTER + 10, 80 + 15 * 2, 300 - 10, 10, 20, getData().game_information.white_player_name.size(), 1, X_CENTER + 10, 80, 300 - 10, 15 * 2 + 10);
+        n_lines_memo = 0;
+        
+        scroll_manager_memo.init(X_CENTER + 300, 150, 10, 250, 20, n_lines_memo, 1, X_CENTER - 300, 140, 600 + 10, 250);
     }
 
     void update() override {
@@ -36,22 +40,25 @@ public:
         getData().fonts.font(language.get("play", "game_information")).draw(25, Arg::topCenter(X_CENTER, 10), getData().colors.white);
         int sy = 78;
         getData().fonts.font(language.get("in_out", "player_name")).draw(15, Arg::topCenter(X_CENTER, 57), getData().colors.white);
-        Circle(X_CENTER - EXPORT_GAME_PLAYER_WIDTH - EXPORT_GAME_RADIUS - 20, 80 + EXPORT_GAME_RADIUS, EXPORT_GAME_RADIUS).draw(getData().colors.black);
-        Circle(X_CENTER + EXPORT_GAME_PLAYER_WIDTH + EXPORT_GAME_RADIUS + 20, 80 + EXPORT_GAME_RADIUS, EXPORT_GAME_RADIUS).draw(getData().colors.white);
-        
-        Rect black_player_rect{X_CENTER - EXPORT_GAME_PLAYER_WIDTH, 80, EXPORT_GAME_PLAYER_WIDTH - 10, EXPORT_GAME_RADIUS * 2};
+        Circle(X_CENTER - 300 - 15 - 20, 80 + 15, 15).draw(getData().colors.black);
+        Circle(X_CENTER + 300 + 15 + 20, 80 + 15, 15).draw(getData().colors.white);
+        // black player name
+        Rect black_player_rect{X_CENTER - 300, 80, 300 - 10, 15 * 2};
         black_player_rect.drawFrame(1, getData().colors.white);
         scroll_manager_black.draw();
         scroll_manager_black.update();
-        std::cerr << scroll_manager_black.get_strt_idx_int() << std::endl;
-        String black_player_display = get_substr(getData().game_information.black_player_name, scroll_manager_black.get_strt_idx_int(), EXPORT_GAME_PLAYER_WIDTH - 14);
-        getData().fonts.font(black_player_display).draw(15, Arg::leftCenter(X_CENTER - EXPORT_GAME_PLAYER_WIDTH + 2, 80 + EXPORT_GAME_RADIUS), getData().colors.white);
-        
-        Rect white_player_rect{X_CENTER + 10, 80, EXPORT_GAME_PLAYER_WIDTH - 10, EXPORT_GAME_RADIUS * 2};
+        String black_player_display = get_substr(getData().game_information.black_player_name, scroll_manager_black.get_strt_idx_int(), 300 - 14);
+        getData().fonts.font(black_player_display).draw(15, Arg::leftCenter(X_CENTER - 300 + 2, 80 + 15), getData().colors.white);
+        // white player name
+        Rect white_player_rect{X_CENTER + 10, 80, 300 - 10, 15 * 2};
         white_player_rect.drawFrame(1, getData().colors.white);
-        getData().fonts.font(getData().game_information.white_player_name).draw(15, white_player_rect.stretched(-3), getData().colors.white);
-        getData().fonts.font(language.get("in_out", "memo")).draw(15, Arg::topCenter(X_CENTER, 117), getData().colors.white);
-        Rect memo_rect{X_CENTER - 300, 140, 600, 250};
+        scroll_manager_white.draw();
+        scroll_manager_white.update();
+        String white_player_display = get_substr(getData().game_information.white_player_name, scroll_manager_white.get_strt_idx_int(), 300 - 14);
+        getData().fonts.font(white_player_display).draw(15, Arg::leftCenter(X_CENTER + 10 + 2, 80 + 15), getData().colors.white);
+        // memo
+        getData().fonts.font(language.get("in_out", "memo")).draw(15, Arg::topCenter(X_CENTER, 127), getData().colors.white);
+        Rect memo_rect{X_CENTER - 300, 150, 600, 250};
         memo_rect.drawFrame(1, getData().colors.white);
 
         scroll_manager_memo.draw();
