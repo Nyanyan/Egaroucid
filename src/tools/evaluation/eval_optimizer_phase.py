b001fc7fa989c020196a0746c4ff70fc6a3f268d
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-09-25 19:34:28 +0900

    training new evaluation function #341

--- src/tools/evaluation/eval_optimizer_phase.py
+++ src/tools/evaluation/eval_optimizer_phase.py
@@ -3,9 +3,9 @@ import sys
 
 phase = str(sys.argv[1])
 hour = '0'
-minute = '5'
+minute = '6'
 second = '0'
-alpha = '600.0'
+alpha = '700.0'
 n_patience = '1000'
 
 model_dir = './../../../model/nomodel/'
