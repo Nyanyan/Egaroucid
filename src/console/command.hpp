f028eba9f67910cc62232efb3a8fd0ead59974af
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-11-15 20:15:11 +0900

    add endgame search try depth #351

--- src/console/command.hpp
+++ src/console/command.hpp
@@ -177,17 +177,32 @@ void redo(Board_info *board, int remain) {
 #define TIME_MANAGEMENT_REMAINING_MOVES_OFFSET 5
 
 uint64_t calc_time_limit_ply(const Board board, uint64_t remaining_time_msec) {
+    int n_empties = HW2 - board.n_discs();
+
     // try complete search
     // Nodes(depth) = a * exp(b * depth)
     constexpr double complete_const_a = 0.6;
     constexpr double complete_const_b = 0.75;
     constexpr double complete_nps = 120000000.0;
-    double complete_search_depth = log((double)remaining_time_msec / 1000.0 * complete_nps / complete_const_a) / complete_const_b;
+    double complete_use_time = (double)remaining_time_msec * 0.9;
+    double complete_search_depth = log(complete_use_time / 1000.0 * complete_nps / complete_const_a) / complete_const_b;
     std::cerr << "complete search depth " << complete_search_depth << std::endl;
-    int n_empties = HW2 - board.n_discs();
     if (n_empties <= complete_search_depth) {
-        return remaining_time_msec * 0.8;
+        return complete_use_time;
     }
+
+    // try endgame search
+    // Nodes(depth) = a * exp(b * depth)
+    constexpr double endgame_const_a = 0.5;
+    constexpr double endgame_const_b = 0.5;
+    constexpr double endgame_nps = 90000000.0;
+    double endgame_use_time = (double)remaining_time_msec * 0.25;
+    double endgame_search_depth = log(endgame_use_time / 1000.0 * endgame_nps / endgame_const_a) / endgame_const_b;
+    std::cerr << "endgame search depth " << endgame_search_depth << std::endl;
+    if (n_empties <= endgame_search_depth) {
+        return endgame_use_time;
+    }
+
     // midgame search
     int remaining_moves = (n_empties + 1) / 2;
     if (remaining_time_msec > TIME_MANAGEMENT_REMAINING_TIME_OFFSET * remaining_moves) {
