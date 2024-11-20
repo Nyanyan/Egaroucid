4f42db5d7ba3cbf964aa8b9330cd46b2e37b6e2f
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-11-20 15:37:41 +0900

    writing ggs client #367

--- bin/ggs/ggs_client.py
+++ bin/ggs/ggs_client.py
@@ -1,3 +1,12 @@
 
-with open('ggs_id.txt')
-ggs_id = 
+with open('id/ggs_id.txt', 'r') as f:
+    ggs_id = f.read()
+with open('id/ggs_pw.txt', 'r') as f:
+    ggs_pw = f.read()
+with open('id/ggs_server.txt', 'r') as f:
+    ggs_server = f.read()
+with open('id/ggs_port.txt', 'r') as f:
+    ggs_port = f.read()
+print('GGS server', ggs_server, 'port', ggs_port)
+print('GGS ID', ggs_id, 'GGS PW', ggs_pw)
+
