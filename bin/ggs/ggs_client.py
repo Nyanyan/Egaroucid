17fb4a0ed160ef97bf408c406b4e355c98c24014
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-11-21 16:00:12 +0900

    update log

--- bin/ggs/ggs_client.py
+++ bin/ggs/ggs_client.py
@@ -3,6 +3,10 @@ import subprocess
 import datetime
 import time
 
+color_dic = {"black":"\033[30m", "red":"\033[31m", "green":"\033[32m", "yellow":"\033[33m", "blue":"\033[34m", "end":"\033[0m"}
+def print_color(text, color="red"):
+    print(color_dic[color] + text + color_dic["end"])
+
 with open('id/ggs_id.txt', 'r') as f:
     ggs_id = f.read()
 with open('id/ggs_pw.txt', 'r') as f:
@@ -20,7 +24,7 @@ d_today = str(datetime.date.today())
 t_now = str(datetime.datetime.now().time())
 logfile = 'log/' + d_today.replace('-', '') + '_' + t_now.split('.')[0].replace(':', '') + '.txt'
 print('log file', logfile)
-egaroucid_cmd = './../versions/Egaroucid_for_Console_beta/Egaroucid_for_Console.exe -t 8 -quiet -noise -showvalue -noautopass -hash 27 -logfile ' + logfile
+egaroucid_cmd = './../versions/Egaroucid_for_Console_beta/Egaroucid_for_Console.exe -t 8 -quiet -noise -ponder -showvalue -noautopass -hash 27 -logfile ' + logfile
 egaroucid = subprocess.Popen(egaroucid_cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
 
@@ -49,7 +53,7 @@ def ggs_os_ask_game(tl1, tl2, tl3, turn, user):
     tl2_str = fill0(tl2 // 60, 2) + ':' + fill0(tl2 % 60, 2)
     tl3_str = fill0(tl3 // 60, 2) + ':' + fill0(tl3 % 60, 2)
     cmd = 'ts ask 8' + turn_str + ' ' + tl1_str + '/' + tl2_str + '/' + tl3_str + ' ' + user
-    print('[INFO]', 'ask game', cmd)
+    print_color('[INFO] ask game ' + cmd, 'green')
     tn.write((cmd + '\n').encode('utf-8'))
 
 
@@ -65,12 +69,10 @@ def ggs_os_get_info(s):
 def ggs_os_is_game_request(s):
     ss = s.split()
     if len(ss) == 10:
-        return (ss[1] == '+' or ss[1] == '-') and ss[9] == ggs_id
+        return ss[1] == '+' and ss[9] == ggs_id
     return False
 
 def ggs_os_get_received_game_info(s):
-    print('received game info')
-    print(s)
     data = s.split()
     if len(data) == 10:
         game_id = data[2]
@@ -84,10 +86,10 @@ def ggs_os_get_received_game_info(s):
                 tls[i] = 0
         game_type = data[6]
         return game_id, tls[0], tls[1], tls[2], opponent, game_type
-    print('[ERROR]', 'cannot receive game:', s)
+    print_color('[ERROR] cannot receive game : ' + s, 'red')
 
 def ggs_os_accept_game(request_id):
-    print('[INFO]', 'accept game', request_id)
+    print_color('[INFO] accept game : ' + request_id, 'green')
     tn.write(('ts accept ' + request_id + '\n').encode('utf-8'))
 
 
@@ -95,14 +97,14 @@ def ggs_os_accept_game(request_id):
 def ggs_os_is_start_game(s):
     ss = s.split()
     if len(ss) > 3:
-        return ss[2] == 'match'
+        return ss[1] == '+' and ss[2] == 'match'
     return False
 
 def ggs_os_start_game_get_id(s):
     ss = s.split()
     if len(ss) > 4:
         return ss[3]
-    print('[ERROR]', 'cannot receive game id:', s)
+    print_color('[ERROR] cannot receive game id: ' + s, 'red')
 
 
 def ggs_os_is_board_info(s):
@@ -115,7 +117,7 @@ def ggs_os_board_info_get_id(s):
     ss = s.split()
     if len(ss) >= 3:
         return ss[2]
-    print('[ERROR]', 'cannot receive game id:', s)
+    print_color('[ERROR] cannot receive game id: ' + s, 'red')
 
 def ggs_os_get_board(s):
     data = s.splitlines()
@@ -132,7 +134,7 @@ def ggs_os_get_board(s):
                     elif line[2][0] == 'O':
                         me_color = 'O'
                     else:
-                        print('[ERROR]', 'invalid color', datum, line)
+                        print_color('[ERROR] invalid color: ' + datum, 'red')
                     raw_me_remaining_time = line[3].split(',')[0]
                     me_remaining_time = int(raw_me_remaining_time.split(':')[0]) * 60 + int(raw_me_remaining_time.split(':')[1])
     #print('[INFO]', 'me_color', me_color, 'me_remaining_time', me_remaining_time)
@@ -150,7 +152,7 @@ def ggs_os_get_board(s):
     elif raw_player == 'O':
         color_to_move = 'O'
     else:
-        print('[ERROR]', 'cannot recognize player', raw_player)
+        print_color('[ERROR] cannot recognize player: ' + raw_player, 'red')
     #print('[INFO]', 'color_to_move', color_to_move)
 
     raw_board = ''
@@ -207,25 +209,25 @@ def idx_to_coord_str_rev(coord):
 
 def egaroucid_play_move(move):
     cmd = 'play ' + move
-    print('[INFO]', 'egaroucid play', ':', cmd)
+    print_color('[INFO] egaroucid play : ' + cmd, 'green')
     egaroucid.stdin.write((cmd + '\n').encode('utf-8'))
     egaroucid.stdin.flush()
 
 def egaroucid_settime(color, time_limit):
     cmd = 'settime ' + color + ' ' + str(time_limit)
-    print('[INFO]', 'egaroucid settime', ':', cmd)
+    print_color('[INFO] egaroucid settime : ' + cmd, 'green')
     egaroucid.stdin.write((cmd + '\n').encode('utf-8'))
     egaroucid.stdin.flush()
 
 def egaroucid_setboard(board):
     cmd = 'setboard ' + board
-    print('[INFO]', 'egaroucid setboard', ':', cmd)
+    print_color('[INFO] egaroucid setboard : ' + cmd, 'green')
     egaroucid.stdin.write((cmd + '\n').encode('utf-8'))
     egaroucid.stdin.flush()
 
 def egaroucid_get_move_score():
     cmd = 'go'
-    print('[INFO]', 'egaroucid play', ':', cmd)
+    print_color('[INFO] egaroucid play : ' + cmd, 'green')
     egaroucid.stdin.write((cmd + '\n').encode('utf-8'))
     egaroucid.stdin.flush()
     line = egaroucid.stdout.readline().decode().replace('\r', '').replace('\n', '')
@@ -250,7 +252,7 @@ ggs_wait_ready()
 tn.write(b"ts client -\n")
 ggs_wait_ready()
 
-print('[INFO]', 'initialized!')
+print_color('[INFO] Initialized!', 'green')
 
 
 playing_game_id = ''
@@ -264,23 +266,25 @@ while True:
     received_data = ggs_wait_ready()
     os_info = ggs_os_get_info(received_data)
     if ggs_os_is_game_request(os_info):
-        print('[INFO]', 'GGS Game Request', ':', os_info)
+        print_color('[INFO] GGS Game Request : ' + os_info, 'green')
         request_id, tl1, tl2, tl3, opponent, game_type = ggs_os_get_received_game_info(os_info)
         ggs_os_accept_game(request_id)
     elif ggs_os_is_start_game(os_info):
-        print('[INFO]', 'GGS Game Start', ':', os_info)
+        print_color('[INFO] GGS Game Start : ' + os_info, 'green')
         game_id = ggs_os_start_game_get_id(os_info)
         playing_game_id = game_id
     elif ggs_os_is_board_info(os_info):
         game_id = ggs_os_board_info_get_id(os_info)
-        if game_id[:len(playing_game_id)] == playing_game_id:
+        game_id_nosub = game_id
+        if ggs_os_is_synchro(game_id):
+            sub_idx = ggs_os_get_synchro_id(game_id)
+            game_id_nosub = game_id[:len(str(sub_idx))]
+        if game_id_nosub == playing_game_id:
             me_color, me_remaining_time, board, color_to_move = ggs_os_get_board(received_data)
+            print_color('[INFO] got board from GGS : game_id ' + game_id, 'green')
             print('[INFO]', 'got board from GGS', 'game id', game_id, 'egaroucid_color', me_color, 'remaining_time', me_remaining_time, 'board', board, 'color_to_move', color_to_move)
-            print('[INFO]', 'game_id', game_id, 'set board')
             egaroucid_setboard(board)
             sub_idx = 0
-            if ggs_os_is_synchro(game_id):
-                sub_idx = ggs_os_get_synchro_id[game_id]
             latest_boards[sub_idx] = board
             last_board_move_time = time.time()
             if me_color == color_to_move:
