723d5f9fc799e22abdf3fcecf2cd488d8a99ae6a
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-11-25 13:58:33 +0900

    updating evaluation function #371

--- src/engine/evaluate_simd_7_5.hpp
+++ src/engine/evaluate_simd_7_5.hpp
@@ -150,7 +150,70 @@ constexpr Feature_to_coord feature_to_coord[CEIL_N_SYMMETRY_PATTERNS] = {
 };
 
 constexpr Coord_to_feature coord_to_feature[HW2] = {
-
+    {15, {{12, P30}, {14, P38}, {23, P38}, {28, P32}, {29, P39}, {34, P31}, {35, P31}, {39, P39}, {42, P34}, {43, P34}, {47, P39}, {50, P31}, {51, P31}, {55, P39}, {59, P39}}}, // COORD_H8
+    {11, {{ 3, P30}, { 6, P38}, {14, P37}, {23, P37}, {29, P30}, {34, P32}, {39, P38}, {47, P35}, {50, P32}, {55, P38}, {59, P38}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_G8
+    { 9, {{ 6, P37}, {11, P30}, {23, P36}, {34, P33}, {39, P37}, {42, P35}, {50, P33}, {55, P37}, {60, P34}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_F8
+    { 9, {{19, P30}, {26, P35}, {34, P34}, {39, P36}, {42, P36}, {50, P34}, {54, P35}, {55, P36}, {60, P33}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_E8
+    { 9, {{17, P30}, {25, P31}, {34, P35}, {38, P36}, {42, P37}, {50, P35}, {54, P36}, {55, P35}, {60, P36}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_D8
+    { 9, {{ 5, P31}, { 9, P30}, {22, P36}, {34, P36}, {38, P37}, {42, P38}, {50, P36}, {54, P37}, {60, P35}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_C8
+    {11, {{ 1, P30}, { 5, P30}, {13, P31}, {22, P37}, {31, P30}, {34, P37}, {38, P38}, {46, P35}, {50, P37}, {54, P38}, {58, P38}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_B8
+    {15, {{13, P30}, {15, P38}, {22, P38}, {30, P32}, {31, P39}, {33, P31}, {34, P38}, {38, P39}, {41, P34}, {42, P39}, {46, P39}, {49, P31}, {50, P38}, {54, P39}, {58, P39}}}, // COORD_A8
+    {11, {{ 2, P30}, { 4, P30}, {12, P31}, {23, P35}, {29, P31}, {35, P32}, {39, P35}, {47, P32}, {51, P32}, {55, P34}, {59, P37}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_H7
+    {14, {{ 2, P31}, { 3, P31}, { 5, P34}, {23, P34}, {24, P30}, {26, P36}, {28, P33}, {29, P38}, {34, P30}, {35, P30}, {39, P34}, {47, P38}, {55, P33}, {59, P36}, { 0, PNO}}}, // COORD_G7
+    { 9, {{ 2, P32}, {11, P31}, {14, P36}, {23, P33}, {39, P33}, {42, P30}, {47, P34}, {50, P30}, {59, P35}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_F7
+    { 7, {{ 2, P33}, { 6, P36}, {19, P31}, {25, P32}, {42, P31}, {59, P34}, {60, P32}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_E7
+    { 7, {{ 2, P34}, { 5, P32}, {17, P31}, {26, P34}, {42, P32}, {58, P34}, {60, P37}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_D7
+    { 9, {{ 2, P35}, { 9, P31}, {13, P32}, {22, P33}, {38, P33}, {42, P33}, {46, P34}, {50, P39}, {58, P35}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_C7
+    {14, {{ 1, P31}, { 2, P36}, { 6, P34}, {22, P34}, {25, P30}, {27, P36}, {30, P33}, {31, P38}, {33, P30}, {34, P39}, {38, P34}, {46, P38}, {54, P33}, {58, P36}, { 0, PNO}}}, // COORD_B7
+    {11, {{ 2, P37}, { 7, P38}, {15, P37}, {22, P35}, {31, P31}, {33, P32}, {38, P35}, {46, P32}, {49, P32}, {54, P34}, {58, P37}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_A7
+    { 9, {{ 4, P31}, {10, P30}, {23, P32}, {35, P33}, {39, P32}, {43, P35}, {51, P33}, {55, P32}, {63, P35}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_H6
+    { 9, {{ 3, P32}, {10, P31}, {12, P32}, {23, P31}, {39, P31}, {43, P30}, {47, P31}, {51, P30}, {59, P33}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_G6
+    {10, {{10, P32}, {11, P32}, {23, P30}, {25, P33}, {28, P34}, {29, P37}, {47, P37}, {59, P32}, {60, P30}, {63, P39}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_F6
+    { 6, {{ 5, P33}, {10, P33}, {14, P35}, {19, P32}, {47, P33}, {60, P31}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_E6
+    { 6, {{ 6, P35}, {10, P34}, {13, P33}, {17, P32}, {46, P33}, {60, P38}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_D6
+    {10, {{ 9, P32}, {10, P35}, {22, P30}, {26, P33}, {30, P34}, {31, P37}, {46, P37}, {58, P32}, {60, P39}, {61, P30}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_C6
+    { 9, {{ 1, P32}, {10, P36}, {15, P36}, {22, P31}, {38, P31}, {41, P30}, {46, P31}, {49, P30}, {58, P33}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_B6
+    { 9, {{ 7, P37}, {10, P37}, {22, P32}, {33, P33}, {38, P32}, {41, P35}, {49, P33}, {54, P32}, {61, P34}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_A6
+    { 9, {{18, P30}, {24, P31}, {35, P34}, {39, P30}, {43, P36}, {51, P34}, {53, P30}, {55, P31}, {63, P36}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_H5
+    { 7, {{ 3, P33}, { 4, P32}, {18, P31}, {25, P34}, {43, P31}, {59, P31}, {63, P37}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_G5
+    { 6, {{ 5, P35}, {11, P33}, {12, P33}, {18, P32}, {47, P30}, {63, P38}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_F5
+    { 7, {{13, P34}, {18, P33}, {19, P33}, {28, P35}, {29, P36}, {47, P36}, {59, P30}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_E5
+    { 7, {{14, P34}, {17, P33}, {18, P34}, {30, P35}, {31, P36}, {46, P36}, {58, P30}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_D5
+    { 6, {{ 6, P33}, { 9, P33}, {15, P35}, {18, P35}, {46, P30}, {61, P31}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_C5
+    { 7, {{ 1, P33}, { 7, P36}, {18, P36}, {26, P32}, {41, P31}, {58, P31}, {61, P32}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_B5
+    { 9, {{18, P37}, {27, P35}, {33, P34}, {38, P30}, {41, P36}, {49, P34}, {52, P30}, {54, P31}, {61, P33}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_A5
+    { 9, {{16, P30}, {25, P35}, {35, P35}, {37, P30}, {43, P37}, {51, P35}, {53, P31}, {55, P30}, {63, P33}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_H4
+    { 7, {{ 3, P34}, { 5, P36}, {16, P31}, {24, P32}, {43, P32}, {57, P31}, {63, P32}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_G4
+    { 6, {{ 4, P33}, {11, P34}, {13, P35}, {16, P32}, {45, P30}, {63, P31}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_F4
+    { 7, {{12, P34}, {16, P33}, {19, P34}, {30, P36}, {31, P35}, {45, P36}, {57, P30}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_E4
+    { 7, {{15, P34}, {16, P34}, {17, P34}, {28, P36}, {29, P35}, {44, P36}, {56, P30}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_D4
+    { 6, {{ 7, P35}, { 9, P34}, {14, P33}, {16, P35}, {44, P30}, {61, P38}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_C4
+    { 7, {{ 1, P34}, { 6, P32}, {16, P36}, {27, P34}, {41, P32}, {56, P31}, {61, P37}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_B4
+    { 9, {{16, P37}, {26, P31}, {33, P35}, {36, P30}, {41, P37}, {49, P35}, {52, P31}, {54, P30}, {61, P36}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_A4
+    { 9, {{ 5, P37}, { 8, P30}, {21, P32}, {35, P36}, {37, P32}, {43, P38}, {51, P36}, {53, P32}, {63, P34}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_H3
+    { 9, {{ 3, P35}, { 8, P31}, {13, P36}, {21, P31}, {37, P31}, {43, P33}, {45, P31}, {51, P39}, {57, P33}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_G3
+    {10, {{ 8, P32}, {11, P35}, {21, P30}, {24, P33}, {30, P37}, {31, P34}, {45, P37}, {57, P32}, {62, P39}, {63, P30}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_F3
+    { 6, {{ 4, P35}, { 8, P33}, {15, P33}, {19, P35}, {45, P33}, {62, P38}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_E3
+    { 6, {{ 7, P33}, { 8, P34}, {12, P35}, {17, P35}, {44, P33}, {62, P31}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_D3
+    {10, {{ 8, P35}, { 9, P35}, {20, P30}, {27, P33}, {28, P37}, {29, P34}, {44, P37}, {56, P32}, {61, P39}, {62, P30}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_C3
+    { 9, {{ 1, P35}, { 8, P36}, {14, P32}, {20, P31}, {36, P31}, {41, P33}, {44, P31}, {49, P39}, {56, P33}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_B3
+    { 9, {{ 6, P31}, { 8, P37}, {20, P32}, {33, P36}, {36, P32}, {41, P38}, {49, P36}, {52, P32}, {61, P35}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_A3
+    {11, {{ 0, P30}, { 5, P38}, {13, P37}, {21, P35}, {30, P31}, {35, P37}, {37, P35}, {45, P32}, {51, P37}, {53, P34}, {57, P37}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_H2
+    {14, {{ 0, P31}, { 3, P36}, { 4, P34}, {21, P34}, {25, P36}, {27, P30}, {30, P38}, {31, P33}, {32, P30}, {35, P39}, {37, P34}, {45, P38}, {53, P33}, {57, P36}, { 0, PNO}}}, // COORD_G2
+    { 9, {{ 0, P32}, {11, P36}, {15, P32}, {21, P33}, {37, P33}, {40, P30}, {45, P34}, {48, P30}, {57, P35}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_F2
+    { 7, {{ 0, P33}, { 7, P32}, {19, P36}, {24, P34}, {40, P31}, {57, P34}, {62, P37}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_E2
+    { 7, {{ 0, P34}, { 4, P36}, {17, P36}, {27, P32}, {40, P32}, {56, P34}, {62, P32}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_D2
+    { 9, {{ 0, P35}, { 9, P36}, {12, P36}, {20, P33}, {36, P33}, {40, P33}, {44, P34}, {48, P39}, {56, P35}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_C2
+    {14, {{ 0, P36}, { 1, P36}, { 7, P34}, {20, P34}, {24, P36}, {26, P30}, {28, P38}, {29, P33}, {32, P39}, {33, P39}, {36, P34}, {44, P38}, {52, P33}, {56, P36}, { 0, PNO}}}, // COORD_B2
+    {11, {{ 0, P37}, { 6, P30}, {14, P31}, {20, P35}, {28, P31}, {33, P37}, {36, P35}, {44, P32}, {49, P37}, {52, P34}, {56, P37}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_A2
+    {15, {{13, P38}, {15, P30}, {21, P38}, {30, P39}, {31, P32}, {32, P31}, {35, P38}, {37, P39}, {40, P34}, {43, P39}, {45, P39}, {48, P31}, {51, P38}, {53, P39}, {57, P39}}}, // COORD_H1
+    {11, {{ 3, P37}, { 7, P30}, {15, P31}, {21, P37}, {30, P30}, {32, P32}, {37, P38}, {45, P35}, {48, P32}, {53, P38}, {57, P38}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_G1
+    { 9, {{ 7, P31}, {11, P37}, {21, P36}, {32, P33}, {37, P37}, {40, P35}, {48, P33}, {53, P37}, {62, P35}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_F1
+    { 9, {{19, P37}, {27, P31}, {32, P34}, {37, P36}, {40, P36}, {48, P34}, {52, P35}, {53, P36}, {62, P36}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_E1
+    { 9, {{17, P37}, {24, P35}, {32, P35}, {36, P36}, {40, P37}, {48, P35}, {52, P36}, {53, P35}, {62, P33}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_D1
+    { 9, {{ 4, P37}, { 9, P37}, {20, P36}, {32, P36}, {36, P37}, {40, P38}, {48, P36}, {52, P37}, {62, P34}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_C1
+    {11, {{ 1, P37}, { 4, P38}, {12, P37}, {20, P37}, {28, P30}, {32, P37}, {36, P38}, {44, P35}, {48, P37}, {52, P38}, {56, P38}, { 0, PNO}, { 0, PNO}, { 0, PNO}, { 0, PNO}}}, // COORD_B1
+    {15, {{12, P38}, {14, P30}, {20, P38}, {28, P39}, {29, P32}, {32, P38}, {33, P38}, {36, P39}, {40, P39}, {41, P39}, {44, P39}, {48, P38}, {49, P38}, {52, P39}, {56, P39}}}  // COORD_A1
 };
 
 /*
@@ -164,8 +227,6 @@ __m256i eval_move_unflipped_16bit[N_16BIT][N_SIMD_EVAL_FEATURE_GROUP][N_SIMD_EVA
 __m256i eval_simd_offsets_simple[N_SIMD_EVAL_FEATURES_SIMPLE]; // 16bit * 16 * N
 __m256i eval_simd_offsets_comp[N_SIMD_EVAL_FEATURES_COMP * 2]; // 32bit * 8 * N
 
-__m256i eval_surround_mask;
-__m256i eval_surround_shift1879;
 
 /*
     @brief evaluation parameters
@@ -173,7 +234,6 @@ __m256i eval_surround_shift1879;
 // normal
 int16_t pattern_arr[N_PHASES][N_PATTERN_PARAMS];
 int16_t eval_num_arr[N_PHASES][MAX_STONE_NUM];
-int16_t eval_sur0_sur1_arr[N_PHASES][MAX_SURROUND][MAX_SURROUND];
 // move ordering evaluation
 int16_t pattern_move_ordering_end_arr[N_PATTERN_PARAMS_MO_END];
 
@@ -192,8 +252,6 @@ inline bool load_eval_file(const char* file, bool show_log) {
         param_idx += N_PATTERN_PARAMS_RAW;
         std::memcpy(eval_num_arr[phase_idx], &unzipped_params[param_idx], sizeof(short) * MAX_STONE_NUM);
         param_idx += MAX_STONE_NUM;
-        std::memcpy(eval_sur0_sur1_arr[phase_idx], &unzipped_params[param_idx], sizeof(short) * MAX_SURROUND * MAX_SURROUND);
-        param_idx += MAX_SURROUND * MAX_SURROUND;
     }
     // check max value
     for (int phase_idx = 0; phase_idx < N_PHASES; ++phase_idx) {
@@ -347,10 +405,6 @@ inline void pre_calculate_eval_constant() {
         }
         eval_lower_mask = _mm256_set1_epi32(0x0000FFFF);
     }
-    { // calc_surround initialization
-        eval_surround_mask = _mm256_set_epi64x(0x7E7E7E7E7E7E7E7EULL, 0x00FFFFFFFFFFFF00ULL, 0x007E7E7E7E7E7E00ULL, 0x007E7E7E7E7E7E00ULL);
-        eval_surround_shift1879 = _mm256_set_epi64x(1, HW, HW_M1, HW_P1);
-    }
 }
 
 /*
@@ -395,22 +449,6 @@ bool evaluate_init(const std::string file, std::string mo_end_nws_file, bool sho
 bool evaluate_init(bool show_log) {
     return evaluate_init(EXE_DIRECTORY_PATH + "resources/eval.egev2", EXE_DIRECTORY_PATH + "resources/eval_move_ordering_end.egev", show_log);
 }
-/*
-    @brief calculate surround value used in evaluation function
-
-    @param discs                a bitboard representing discs
-    @param empties              a bitboard representing empties
-    @return surround value
-*/
-inline int calc_surround(const uint64_t discs, const uint64_t empties) {
-    __m256i pl = _mm256_set1_epi64x(discs);
-    pl = _mm256_and_si256(pl, eval_surround_mask);
-    pl = _mm256_or_si256(_mm256_sllv_epi64(pl, eval_surround_shift1879), _mm256_srlv_epi64(pl, eval_surround_shift1879));
-    __m128i res = _mm_or_si128(_mm256_castsi256_si128(pl), _mm256_extracti128_si256(pl, 1));
-    res = _mm_or_si128(res, _mm_shuffle_epi32(res, 0x4e));
-    return pop_count_ull(_mm_cvtsi128_si64(res) & empties);
-}
-#define CALC_SURROUND_FUNCTION
 
 /*
     @brief pattern evaluation
@@ -470,12 +508,9 @@ inline int mid_evaluate(Board *board) {
     uint64_t empties;
     phase_idx = search.phase();
     empties = ~(search.board.player | search.board.opponent);
-    sur0 = calc_surround(search.board.player, empties);
-    sur1 = calc_surround(search.board.opponent, empties);
     num0 = pop_count_ull(search.board.player);
     int res = calc_pattern(phase_idx, &search.eval.features[search.eval.feature_idx]) + 
-        eval_num_arr[phase_idx][num0] + 
-        eval_sur0_sur1_arr[phase_idx][sur0][sur1];
+        eval_num_arr[phase_idx][num0];
     res += res >= 0 ? STEP_2 : -STEP_2;
     res /= STEP;
     res = std::clamp(res, -SCORE_MAX, SCORE_MAX);
@@ -493,12 +528,9 @@ inline int mid_evaluate_diff(Search *search) {
     uint64_t empties;
     phase_idx = search->phase();
     empties = ~(search->board.player | search->board.opponent);
-    sur0 = calc_surround(search->board.player, empties);
-    sur1 = calc_surround(search->board.opponent, empties);
     num0 = pop_count_ull(search->board.player);
     int res = calc_pattern(phase_idx, &search->eval.features[search->eval.feature_idx]) + 
-        eval_num_arr[phase_idx][num0] + 
-        eval_sur0_sur1_arr[phase_idx][sur0][sur1];
+        eval_num_arr[phase_idx][num0];
     res += res >= 0 ? STEP_2 : -STEP_2;
     res /= STEP;
     res = std::clamp(res, -SCORE_MAX, SCORE_MAX);
