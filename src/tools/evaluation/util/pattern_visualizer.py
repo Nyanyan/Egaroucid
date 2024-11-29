ea93043ae48ad7a590c7ad9668e2a75d561467de
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-11-29 15:13:44 +0900

    update pattern visualizer #371

--- src/tools/evaluation/util/pattern_visualizer.py
+++ src/tools/evaluation/util/pattern_visualizer.py
@@ -36,57 +36,127 @@ while True:
     #print(s)
     s_arr.append(s)
 
+def yx_to_idx(y, x):
+    return y * 8 + x
 
-print('n_features', len(s_arr))
+def idx_to_yx(yx):
+    return yx // 8, yx % 8
 
-duplication = [[0 for _ in range(8)] for _ in range(8)]
-fig = plt.figure(figsize=(10, 11))
-for idx, s in enumerate(s_arr):
+def rotate_all(yx_list):
+    res = [set([]) for _ in range(8)]
+    for yx in yx_list:
+        y, x = idx_to_yx(yx)
+        res[0].add(yx_to_idx(y, x))
+        res[1].add(yx_to_idx(y, 7 - x))
+        res[2].add(yx_to_idx(7 - y, x))
+        res[3].add(yx_to_idx(7 - y, 7 - x))
+        res[4].add(yx_to_idx(x, y))
+        res[5].add(yx_to_idx(x, 7 - y))
+        res[6].add(yx_to_idx(7 - x, y))
+        res[7].add(yx_to_idx(7 - x, 7 - y))
+    return res
+
+feature_list = []
+pattern_list = []
+for s in s_arr:
     coords = s.split(',')
-    ax = fig.add_subplot(9, 8, idx + 1)
-    ax.set_aspect("equal", adjustable="box")
-    for x in range(9):
-        p = plt.Line2D(xdata=(x, x), ydata=(0, 8), color='k', linewidth=1)
-        ax.add_line(p)
-    for y in range(9):
-        p = plt.Line2D(xdata=(0, 8), ydata=(y, y), color='k', linewidth=1)
-        ax.add_line(p)
-    num = 0
+    yx_list = []
     for coord in coords:
         try:
             x = ord(coord[-2]) - ord('A')
             y = int(coord[-1]) - 1
-            #print(coord, x, y)
-            p = Rectangle(xy=(x, 7 - y), height=1, width=1, facecolor='k')
-            ax.add_patch(p)
-            ax.text(x + 0.5, 7 - y + 0.5, num, color='w', fontsize=7, horizontalalignment='center', verticalalignment='center')
-            num += 1
-            duplication[y][x] += 1
-            '''
-            duplication[7 - y][x] += 1
-            duplication[y][7 - x] += 1
-            duplication[7 - y][7 - x] += 1
-            duplication[x][y] += 1
-            duplication[7 - x][y] += 1
-            duplication[x][7 - y] += 1
-            duplication[7 - x][7 - y] += 1
-            '''
+            yx_list.append(yx_to_idx(y, x))
         except:
             pass
+    feature_list.append(yx_list)
+    rotated_lists = rotate_all(yx_list)
+    ignore_this_pattern = False
+    for rotated in rotated_lists:
+        for pattern in pattern_list:
+            if rotated == set(pattern):
+                ignore_this_pattern = True
+    if not ignore_this_pattern:
+        pattern_list.append(yx_list)
+
+print('n_features', len(feature_list))
+print('n_patterns', len(pattern_list))
+
+# feature image
+fig = plt.figure(figsize=(10, 10))
+for idx, yx_list in enumerate(feature_list):
+    ax = fig.add_subplot((len(feature_list) + 7) // 8, 8, idx + 1)
+    ax.set_aspect("equal", adjustable="box")
+    for x in range(9):
+        p = plt.Line2D(xdata=(x, x), ydata=(0, 8), color='k', linewidth= 2 if x == 0 or x == 8 else 1)
+        ax.add_line(p)
+    for y in range(9):
+        p = plt.Line2D(xdata=(0, 8), ydata=(y, y), color='k', linewidth= 2 if y == 0 or y == 8 else 1)
+        ax.add_line(p)
+    num = 0
+    for yx in yx_list:
+        y, x = idx_to_yx(yx)
+        p = Rectangle(xy=(x, 7 - y), height=1, width=1, facecolor='k')
+        ax.add_patch(p)
+        ax.text(x + 0.5, 7 - y + 0.5, num, color='w', fontsize=7, horizontalalignment='center', verticalalignment='center')
+        num += 1
     ax.set_xlim((0, 8))
     ax.set_ylim((0, 8))
     ax.axis("off")
+plt.tight_layout()
+plt.show()
+plt.clf()
 
+# pattern image
+fig = plt.figure(figsize=(10, 10))
+for idx, yx_list in enumerate(pattern_list):
+    ax = fig.add_subplot((len(pattern_list) + 3) // 4, 4, idx + 1)
+    ax.set_aspect("equal", adjustable="box")
+    for x in range(9):
+        p = plt.Line2D(xdata=(x, x), ydata=(0, 8), color='k', linewidth= 3 if x == 0 or x == 8 else 1)
+        ax.add_line(p)
+    for y in range(9):
+        p = plt.Line2D(xdata=(0, 8), ydata=(y, y), color='k', linewidth= 3 if y == 0 or y == 8 else 1)
+        ax.add_line(p)
+    num = 0
+    for yx in yx_list:
+        y, x = idx_to_yx(yx)
+        p = Rectangle(xy=(x, 7 - y), height=1, width=1, facecolor='k')
+        ax.add_patch(p)
+        #ax.text(x + 0.5, 7 - y + 0.5, num, color='w', fontsize=7, horizontalalignment='center', verticalalignment='center')
+        num += 1
+    ax.set_xlim((0, 8))
+    ax.set_ylim((0, 8))
+    ax.axis("off")
+plt.tight_layout()
+plt.show()
+plt.clf()
+
+# heatmap
+fig = plt.figure(figsize=(10, 10))
+duplication = [[0 for _ in range(8)] for _ in range(8)]
+for idx, yx_list in enumerate(feature_list):
+    for yx in yx_list:
+        y, x = idx_to_yx(yx)
+        duplication[y][x] += 1
+max_dup = 0
+min_dup = 10000
 for arr in duplication:
+    max_dup = max(max_dup, max(arr))
+    min_dup = min(min_dup, min(arr))
     print(arr)
-ax = fig.add_subplot(9, 8, len(s_arr) + 1)
+ax = fig.add_subplot(1, 1, 1)
 ax.set_aspect("equal", adjustable="box")
 heatmap = ax.pcolor(np.flipud(np.array(duplication)), cmap=plt.cm.Blues)
 for y in range(8):
     for x in range(8):
-        ax.text(x + 0.5, 7 - y + 0.5, duplication[y][x], color='w', fontsize=7, horizontalalignment='center', verticalalignment='center')
+        if duplication[y][x] < (max_dup + min_dup) // 2:
+            color = 'black'
+        else:
+            color = 'white'
+        ax.text(x + 0.5, 7 - y + 0.5, duplication[y][x], color=color, fontsize=30, horizontalalignment='center', verticalalignment='center')
 ax.set_xlim((0, 8))
 ax.set_ylim((0, 8))
 ax.axis("off")
 plt.tight_layout()
 plt.show()
+plt.clf()
