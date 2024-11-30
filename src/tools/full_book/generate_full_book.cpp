b7cccae4627c902a1e44c6837149c5333656fb5f
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-11-30 12:29:45 +0900

    updating full book generation #371

--- src/tools/full_book/generate_full_book.cpp
+++ src/tools/full_book/generate_full_book.cpp
@@ -10,70 +10,6 @@ void full_book_init(){
     book_init("data/empty_book.egbk3", true);
 }
 
-void board_to_line_print_transcript(std::vector<int> &transcript){
-    for (int &move: transcript){
-        std::cout << idx_to_coord(move);
-    }
-    std::cout << std::endl;
-}
-
-void board_to_line(Board board, const int depth, const int error_per_move, int remaining_error, std::vector<int> &transcript){
-    if (board.n_discs() >= depth + 4){
-        board_to_line_print_transcript(transcript);
-        return;
-    }
-    bool move_found = false;
-    uint64_t legal = board.get_legal();
-    Flip flip;
-    Book_elem parent_elem = book.get(board);
-    for (uint8_t cell = first_bit(&legal); legal; cell = next_bit(&legal)){
-        calc_flip(&flip, &board, cell);
-        board.move_board(&flip);
-        transcript.emplace_back(cell);
-        bool passed = false;
-        bool is_end = false;
-        if (board.get_legal() == 0){
-            passed = true;
-            board.pass();
-            if (board.get_legal() == 0){
-                passed = false;
-                is_end = true;
-                board.pass();
-            }
-        }
-
-            if (!is_end){
-                if (book.contain(&board)){
-                    Book_elem book_elem = book.get(board);
-                    int val = -book_elem.value;
-                    if (passed){
-                        val *= -1;
-                    }
-                    int error = parent_elem.value - val;
-                    int n_remaining_error = remaining_error - error;
-                    if (error <= error_per_move && n_remaining_error >= 0){
-                        move_found = true;
-                    }
-                    if (!book_elem.seen){
-                        //book.flag_book_elem(board);
-                        if (error <= error_per_move && n_remaining_error >= 0){
-                            board_to_line(board, depth, error_per_move, n_remaining_error, transcript);
-                        }
-                    }
-                }
-            }
-
-        if (passed){
-            board.pass();
-        }
-        transcript.pop_back();
-        board.undo_board(&flip);
-    }
-    if (!move_found){
-        board_to_line_print_transcript(transcript);
-    }
-}
-
 std::unordered_map<Board, int, Book_hash> data;
 
 void load_data(std::string data_dir) {
@@ -104,6 +40,12 @@ void load_data(std::string data_dir) {
     std::cerr << data.size() << " data found" << std::endl;
 }
 
+void generate_full_book(Board board, int depth, int level) {
+    if (depth == 0) {
+        if ()
+    }
+}
+
 int main(int argc, char* argv[]){
     if (argc < 4){
         std::cerr << "input [depth] [level] [data_dir]" << std::endl;
@@ -117,4 +59,5 @@ int main(int argc, char* argv[]){
     Board board;
     board.reset();
     generate_full_book(board, depth, level);
+    book.negamax_book();
 }
