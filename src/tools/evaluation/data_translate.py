7d8671164a93ccd25155991a989a2d022efde7c3
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-11-30 16:35:12 +0900

    adding new first11 book data #371

--- src/tools/evaluation/data_translate.py
+++ src/tools/evaluation/data_translate.py
@@ -94,13 +94,16 @@ N_PHASES = 60
 board_sub_dir_nums = [
     18, 19, 20, 21, 24, 25, 28, 29, 30, 31, # old data (without records27)
     34, 35, # mid-endgame data 1
-    36, 37, # book data
+    #36, # first11 book (old)
+    37, # book data
     38, # test data
     39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 57, 60, 61, 62, 63, # mid-endgame data 2
     65, 66, 67, 68, 69, 70, 71, 72, 73, 74, # Egaroucid 7.4.0 1000000 data
     77,  # random 18 discs (GGS)
-    78, 79 # random 11 & 12 (bug fixed)
+    78, 79, # random 11 & 12 (bug fixed)
+    80 # new first11 book
 ]
+board_sub_dir_nums = [80]
 board_sub_dir_nums.sort()
 #'''
 
@@ -215,6 +218,8 @@ board_n_moves['77'] = [14, 59] # random 18 discs
 
 board_n_moves['78'] = [12, 59] # random11 all cut 5999816 games bug fixed (records27)
 board_n_moves['79'] = [12, 59] # random12 all cut 7799640 games bug fixed (records75)
+
+board_n_moves['80'] = [0, 11] # new first11 book book_size 23259291
 
 
 procs = []
