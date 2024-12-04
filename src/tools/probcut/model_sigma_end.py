322c6e6a65cd1642d311a4ccc72ec2f4c3127b9d
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-12-04 14:52:46 +0900

    updating mpc

--- src/tools/probcut/model_sigma_end.py
+++ src/tools/probcut/model_sigma_end.py
@@ -18,6 +18,18 @@ import math
 #data_files = ['data/20241128_1_7_5/probcut_end0.txt']
 data_files = ['data/20241130_1_7_5/probcut_end0.txt', 'data/20241130_1_7_5/probcut_end1.txt', 'data/20241130_1_7_5/probcut_end2.txt']
 
+
+probcut_mid_param = [0.93740805837003, -7.340323137961951, 1.1401695320187872, 0.700567733735339, 2.662003673678691, 3.0554301965778063, 2.0942574977708674]
+
+def mid_f(wxy, probcut_a, probcut_b, probcut_c, probcut_d, probcut_e, probcut_f, probcut_g):
+    w, x, y = wxy
+    w = w / 64 # n_discs
+    x = x / 60 # depth_short
+    y = y / 60 # depth_long (== 64 - n_discs)
+    res = probcut_a * w + probcut_b * x + probcut_c * y
+    res = probcut_d * res * res * res + probcut_e * res * res + probcut_f * res + probcut_g
+    return res
+
 data = [[[] for _ in range(61)] for _ in range(65)] # n_discs, depth, error (exact - predict)
 for data_file in data_files:
     try:
@@ -40,7 +52,7 @@ weight_mean = []
 
 
 for n_discs in range(len(data)):
-    if n_discs >= 64 - 13:
+    if n_discs >= 64 - 12:
         continue
     for depth in range(3, len(data[n_discs])):
         if len(data[n_discs][depth]) >= 3:
@@ -53,6 +65,8 @@ for n_discs in range(len(data)):
             for elem in data[n_discs][depth]:
                 sd += (elem - mean) ** 2
             sd = math.sqrt(sd / len(data[n_discs][depth]))
+            
+
             print('n_discs', n_discs, 'depth', depth, 'mean', mean, 'sd', sd, 'n_data', len(data[n_discs][depth]))
             x_n_discs_sd.append(n_discs)
             y_depth_sd.append(depth)
@@ -64,14 +78,12 @@ for n_discs in range(len(data)):
             z_mean.append(mean)
             weight_mean.append(1 / len(data[n_discs][depth]))
 
-'''
-for n_discs in range(15):
-    for depth in range(min(15, 15 - n_discs)):
+for n_discs in range(1):
+    for depth in range(1):
         x_n_discs_sd.append(n_discs)
         y_depth_sd.append(depth)
-        z_sd.append(6.5 - n_discs / 60 * 1.0 - depth * 0.05)
+        z_sd.append(10.0 - n_discs / 60 * 1.0 - depth * 0.05)
         weight_sd.append(0.01)
-'''
 
 def f(xy, probcut_a, probcut_b, probcut_c, probcut_d, probcut_e, probcut_f):
     x, y = xy
@@ -93,6 +105,18 @@ def plot_fit_result(x, y, z, params):
     ax.plot(x, y, z, ms=3, marker="o",linestyle='None')
     mx, my = np.meshgrid(range(65), range(16))
     ax.plot_wireframe(mx, my, f_max((mx, my), *params), rstride=5, cstride=5)
+    # midgame MPC
+    x_mid_mpc = []
+    y_mid_mpc = []
+    for xx in range(64 - 13):
+        for yy in range(15):
+            x_mid_mpc.append(xx)
+            y_mid_mpc.append(yy)
+    z_mid_mpc = []
+    for xx, yy in zip(x_mid_mpc, y_mid_mpc):
+        zz = mid_f([xx, yy, 64 - xx], *probcut_mid_param)
+        z_mid_mpc.append(zz)
+    ax.plot(x_mid_mpc, y_mid_mpc, z_mid_mpc, ms=1, marker="o",linestyle='None')
     ax.set_xlabel('n_discs(=64-long_depth)')
     ax.set_ylabel('search_depth')
     ax.set_zlabel('error')
