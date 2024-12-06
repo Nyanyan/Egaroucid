e7ea085178ba520f8981c4a2db3609eb60b4fa81
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-12-06 14:46:21 +0900

    update log

--- src/engine/ai.hpp
+++ src/engine/ai.hpp
@@ -429,9 +429,11 @@ void iterative_deepening_search_time_limit(Board board, int alpha, int beta, boo
             } else{
                 if (main_mpc_level < MPC_100_LEVEL) {
                     ++main_mpc_level;
-                } else{
-                    if (show_log) {
-                        std::cerr << "completely searched" << std::endl;
+                } else {
+                    if (search_success) {
+                        if (show_log) {
+                            std::cerr << "completely searched" << std::endl;
+                        }
                     }
                     break;
                 }
