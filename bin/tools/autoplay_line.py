a2abe2523c0514bca0bbe6fef837cdf2138688ff
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-12-12 00:17:08 +0900

    update script

--- bin/tools/autoplay_line.py
+++ bin/tools/autoplay_line.py
@@ -6,7 +6,7 @@ import sys
 line_dr = './../problem/etc/first12_all_shuffled'
 out_dr = './../transcript/first12_all_shuffled_after800'
 
-exe = './../versions/Egaroucid_for_Console_beta/Egaroucid_for_Console.exe'
+exe = './../versions/Egaroucid_for_Console_beta/Egaroucid_for_Console_clang.exe'
 
 
 IDX_START = int(sys.argv[1])
