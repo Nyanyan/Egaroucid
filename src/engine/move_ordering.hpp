41e2babe24417039ae7903e842ed552ab44eb7d6
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-12-13 12:44:17 +0900

    refactoring #391

--- src/engine/move_ordering.hpp
+++ src/engine/move_ordering.hpp
@@ -97,7 +97,7 @@ constexpr int MOVE_ORDERING_VALUE_OFFSET_BETA = 8;
 constexpr int MOVE_ORDERING_NWS_VALUE_OFFSET_ALPHA = 16;
 constexpr int MOVE_ORDERING_NWS_VALUE_OFFSET_BETA = 6;
 
-constexpr int MOVE_ORDERING_MPC_LEVEL = MPC_88_LEVEL;
+//constexpr int MOVE_ORDERING_MPC_LEVEL = MPC_88_LEVEL;
 
 int nega_alpha_eval1(Search *search, int alpha, int beta, bool skipped);
 inline int nega_alpha_eval1_move_ordering_mid(Search *search, int alpha, int beta, bool skipped);
@@ -128,14 +128,14 @@ inline void move_ordering_init() {
     @return number of legal moves on corners
 */
 #if USE_BUILTIN_POPCOUNT
-    inline int get_corner_n_moves(uint64_t legal) {
-        return pop_count_ull(legal & 0x8100000000000081ULL);
-    }
+inline int get_corner_n_moves(uint64_t legal) {
+    return pop_count_ull(legal & 0x8100000000000081ULL);
+}
 #else
-    inline int get_corner_n_moves(uint64_t legal) {
-        int res = (int)((legal & 0b10000001ULL) + ((legal >> 56) & 0b10000001ULL));
-        return (res & 0b11) + (res >> 7);
-    }
+inline int get_corner_n_moves(uint64_t legal) {
+    int res = (int)((legal & 0b10000001ULL) + ((legal >> 56) & 0b10000001ULL));
+    return (res & 0b11) + (res >> 7);
+}
 #endif
 
 /*
@@ -175,29 +175,27 @@ inline int get_n_moves_cornerX2(uint64_t legal) {
     @param empties              a bitboard representing empty squares
     @return potential mobility
 */
-#ifdef CALC_SURROUND_FUNCTION
-    #define get_potential_mobility(discs, empties) calc_surround(discs, empties)
-#elif USE_SIMD
-    inline int get_potential_mobility(uint64_t discs, uint64_t empties) {
-        __m256i pl = _mm256_set1_epi64x(discs);
-        pl = _mm256_and_si256(pl, eval_surround_mask);
-        pl = _mm256_or_si256(_mm256_sllv_epi64(pl, eval_surround_shift1879), _mm256_srlv_epi64(pl, eval_surround_shift1879));
-        __m128i res = _mm_or_si128(_mm256_castsi256_si128(pl), _mm256_extracti128_si256(pl, 1));
-        res = _mm_or_si128(res, _mm_shuffle_epi32(res, 0x4e));
-        return pop_count_ull(_mm_cvtsi128_si64(res) & empties);
-    }
+#if USE_SIMD
+inline int get_potential_mobility(uint64_t discs, uint64_t empties) {
+    __m256i pl = _mm256_set1_epi64x(discs);
+    pl = _mm256_and_si256(pl, eval_surround_mask);
+    pl = _mm256_or_si256(_mm256_sllv_epi64(pl, eval_surround_shift1879), _mm256_srlv_epi64(pl, eval_surround_shift1879));
+    __m128i res = _mm_or_si128(_mm256_castsi256_si128(pl), _mm256_extracti128_si256(pl, 1));
+    res = _mm_or_si128(res, _mm_shuffle_epi32(res, 0x4e));
+    return pop_count_ull(_mm_cvtsi128_si64(res) & empties);
+}
 #else
-    inline int get_potential_mobility(uint64_t discs, uint64_t empties) {
-        uint64_t hmask = discs & 0x7E7E7E7E7E7E7E7EULL;
-        uint64_t vmask = discs & 0x00FFFFFFFFFFFF00ULL;
-        uint64_t hvmask = discs & 0x007E7E7E7E7E7E00ULL;
-        uint64_t res = 
-            (hmask << 1) | (hmask >> 1) | 
-            (vmask << HW) | (vmask >> HW) | 
-            (hvmask << HW_M1) | (hvmask >> HW_M1) | 
-            (hvmask << HW_P1) | (hvmask >> HW_P1);
-        return pop_count_ull(empties & res);
-    }
+inline int get_potential_mobility(uint64_t discs, uint64_t empties) {
+    uint64_t hmask = discs & 0x7E7E7E7E7E7E7E7EULL;
+    uint64_t vmask = discs & 0x00FFFFFFFFFFFF00ULL;
+    uint64_t hvmask = discs & 0x007E7E7E7E7E7E00ULL;
+    uint64_t res = 
+        (hmask << 1) | (hmask >> 1) | 
+        (vmask << HW) | (vmask >> HW) | 
+        (hvmask << HW_M1) | (hvmask >> HW_M1) | 
+        (hvmask << HW_P1) | (hvmask >> HW_P1);
+    return pop_count_ull(empties & res);
+}
 #endif
 
 /*
@@ -390,27 +388,27 @@ inline void move_list_evaluate(Search *search, std::vector<Flip_value> &move_lis
     }
     */
     for (Flip_value &flip_value: move_list) {
-        #if USE_MID_ETC
-            if (flip_value.flip.flip) {
-                if (flip_value.flip.pos == moves[0]) {
-                    flip_value.value = W_1ST_MOVE;
-                } else if (flip_value.flip.pos == moves[1]) {
-                    flip_value.value = W_2ND_MOVE;
-                } else{
-                    move_evaluate(search, &flip_value, eval_alpha, eval_beta, eval_depth, searching);
-                }
-            } else{
-                flip_value.value = -INF;
-            }
-        #else
+#if USE_MID_ETC
+        if (flip_value.flip.flip) {
             if (flip_value.flip.pos == moves[0]) {
                 flip_value.value = W_1ST_MOVE;
             } else if (flip_value.flip.pos == moves[1]) {
                 flip_value.value = W_2ND_MOVE;
-            } else{
+            } else {
                 move_evaluate(search, &flip_value, eval_alpha, eval_beta, eval_depth, searching);
             }
-        #endif
+        } else{
+            flip_value.value = -INF;
+        }
+#else
+        if (flip_value.flip.pos == moves[0]) {
+            flip_value.value = W_1ST_MOVE;
+        } else if (flip_value.flip.pos == moves[1]) {
+            flip_value.value = W_2ND_MOVE;
+        } else{
+            move_evaluate(search, &flip_value, eval_alpha, eval_beta, eval_depth, searching);
+        }
+#endif
     }
 }
 
@@ -432,19 +430,8 @@ inline void move_list_evaluate_nws(Search *search, std::vector<Flip_value> &move
     const int eval_beta = -std::max(-SCORE_MAX, alpha - MOVE_ORDERING_NWS_VALUE_OFFSET_ALPHA);
     int eval_depth = depth >> 4;
     for (Flip_value &flip_value: move_list) {
-        #if USE_MID_ETC
-            if (flip_value.flip.flip) {
-                if (flip_value.flip.pos == moves[0]) {
-                    flip_value.value = W_1ST_MOVE;
-                } else if (flip_value.flip.pos == moves[1]) {
-                    flip_value.value = W_2ND_MOVE;
-                } else{
-                    move_evaluate_nws(search, &flip_value, eval_alpha, eval_beta, eval_depth, searching);
-                }
-            } else{
-                flip_value.value = -INF;
-            }
-        #else
+#if USE_MID_ETC
+        if (flip_value.flip.flip) {
             if (flip_value.flip.pos == moves[0]) {
                 flip_value.value = W_1ST_MOVE;
             } else if (flip_value.flip.pos == moves[1]) {
@@ -452,7 +439,18 @@ inline void move_list_evaluate_nws(Search *search, std::vector<Flip_value> &move
             } else{
                 move_evaluate_nws(search, &flip_value, eval_alpha, eval_beta, eval_depth, searching);
             }
-        #endif
+        } else{
+            flip_value.value = -INF;
+        }
+#else
+        if (flip_value.flip.pos == moves[0]) {
+            flip_value.value = W_1ST_MOVE;
+        } else if (flip_value.flip.pos == moves[1]) {
+            flip_value.value = W_2ND_MOVE;
+        } else{
+            move_evaluate_nws(search, &flip_value, eval_alpha, eval_beta, eval_depth, searching);
+        }
+#endif
     }
 }
 
@@ -500,115 +498,115 @@ inline void move_list_sort(std::vector<Flip_value> &move_list) {
     @brief Parameter tuning for move ordering
 */
 #if TUNE_MOVE_ORDERING
-    #include "ai.hpp"
-    inline Search_result tree_search_legal(Board board, int depth, uint_fast8_t mpc_level, bool show_log, uint64_t use_legal, bool use_multi_thread, uint64_t time_limit);
+#include "ai.hpp"
+inline Search_result tree_search_legal(Board board, int depth, uint_fast8_t mpc_level, bool show_log, uint64_t use_legal, bool use_multi_thread, uint64_t time_limit);
 
-    Board get_board(std::string board_str) {
-        board_str.erase(std::remove_if(board_str.begin(), board_str.end(), ::isspace), board_str.end());
-        Board new_board;
-        int player = BLACK;
-        new_board.player = 0ULL;
-        new_board.opponent = 0ULL;
-        if (board_str.length() != HW2 + 1) {
-            std::cerr << "[ERROR] invalid argument" << std::endl;
-            return new_board;
-        }
-        for (int i = 0; i < HW2; ++i) {
-            if (board_str[i] == 'B' || board_str[i] == 'b' || board_str[i] == 'X' || board_str[i] == 'x' || board_str[i] == '0' || board_str[i] == '*')
-                new_board.player |= 1ULL << (HW2_M1 - i);
-            else if (board_str[i] == 'W' || board_str[i] == 'w' || board_str[i] == 'O' || board_str[i] == 'o' || board_str[i] == '1')
-                new_board.opponent |= 1ULL << (HW2_M1 - i);
-        }
-        if (board_str[HW2] == 'B' || board_str[HW2] == 'b' || board_str[HW2] == 'X' || board_str[HW2] == 'x' || board_str[HW2] == '0' || board_str[HW2] == '*')
-            player = BLACK;
-        else if (board_str[HW2] == 'W' || board_str[HW2] == 'w' || board_str[HW2] == 'O' || board_str[HW2] == 'o' || board_str[HW2] == '1')
-            player = WHITE;
-        else{
-            std::cerr << "[ERROR] invalid player argument" << std::endl;
-            return new_board;
-        }
-        if (player == WHITE)
-            std::swap(new_board.player, new_board.opponent);
+Board get_board(std::string board_str) {
+    board_str.erase(std::remove_if(board_str.begin(), board_str.end(), ::isspace), board_str.end());
+    Board new_board;
+    int player = BLACK;
+    new_board.player = 0ULL;
+    new_board.opponent = 0ULL;
+    if (board_str.length() != HW2 + 1) {
+        std::cerr << "[ERROR] invalid argument" << std::endl;
         return new_board;
     }
-
-    uint64_t n_nodes_test(int level, std::vector<Board> testcase_arr) {
-        uint64_t n_nodes = 0;
-        for (Board &board: testcase_arr) {
-            int depth;
-            bool is_mid_search;
-            uint_fast8_t mpc_level;
-            get_level(level, board.n_discs() - 4, &is_mid_search, &depth, &mpc_level);
-            transposition_table.init();
-            Search_result result = tree_search_legal(board, depth, mpc_level, false, board.get_legal(), true, TIME_LIMIT_INF);
-            n_nodes += result.nodes;
-        }
-        transposition_table.reset_importance();
-        return n_nodes;
+    for (int i = 0; i < HW2; ++i) {
+        if (board_str[i] == 'B' || board_str[i] == 'b' || board_str[i] == 'X' || board_str[i] == 'x' || board_str[i] == '0' || board_str[i] == '*')
+            new_board.player |= 1ULL << (HW2_M1 - i);
+        else if (board_str[i] == 'W' || board_str[i] == 'w' || board_str[i] == 'O' || board_str[i] == 'o' || board_str[i] == '1')
+            new_board.opponent |= 1ULL << (HW2_M1 - i);
     }
+    if (board_str[HW2] == 'B' || board_str[HW2] == 'b' || board_str[HW2] == 'X' || board_str[HW2] == 'x' || board_str[HW2] == '0' || board_str[HW2] == '*')
+        player = BLACK;
+    else if (board_str[HW2] == 'W' || board_str[HW2] == 'w' || board_str[HW2] == 'O' || board_str[HW2] == 'o' || board_str[HW2] == '1')
+        player = WHITE;
+    else{
+        std::cerr << "[ERROR] invalid player argument" << std::endl;
+        return new_board;
+    }
+    if (player == WHITE)
+        std::swap(new_board.player, new_board.opponent);
+    return new_board;
+}
 
-    void tune_move_ordering(int level) {
-        std::cout << "please input testcase file" << std::endl;
-        std::string file;
-        std::cin >> file;
-        std::ifstream ifs(file);
-        if (ifs.fail()) {
-            std::cerr << "[ERROR] [FATAL] problem file " << file << " not found" << std::endl;
-            return;
-        }
-        std::vector<Board> testcase_arr;
-        std::string line;
-        while (std::getline(ifs, line)) {
-            testcase_arr.emplace_back(get_board(line));
-        }
-        std::cerr << testcase_arr.size() << " testcases loaded" << std::endl;
-        int minute = 10;
-        std::cout << "please input timelimit (minute)" << std::endl;
-        std::cin >> minute;
-        uint64_t tl = 60ULL * 1000ULL * minute; // 10 min
-        uint64_t min_n_nodes = n_nodes_test(level, testcase_arr);
-        double min_percentage = 100.0;
-        uint64_t first_n_nodes = min_n_nodes;
-        std::cerr << "min_n_nodes " << min_n_nodes << std::endl;
-        int n_updated = 0;
-        int n_try = 0;
-        uint64_t strt = tim();
-        while (tim() - strt < tl) {
-            // update parameter randomly
-            int idx = myrandrange(MOVE_ORDERING_PARAM_START, MOVE_ORDERING_PARAM_END + 1); // midgame search
-            int delta = myrandrange(-5, 6);
-            while (delta == 0)
-                delta = myrandrange(-5, 6);
-            if (move_ordering_param_array[idx] + delta < 0)
-                continue;
-            move_ordering_param_array[idx] += delta;
-            uint64_t n_nodes = n_nodes_test(level, testcase_arr);
-            double percentage = 100.0 * n_nodes / first_n_nodes;
+uint64_t n_nodes_test(int level, std::vector<Board> testcase_arr) {
+    uint64_t n_nodes = 0;
+    for (Board &board: testcase_arr) {
+        int depth;
+        bool is_mid_search;
+        uint_fast8_t mpc_level;
+        get_level(level, board.n_discs() - 4, &is_mid_search, &depth, &mpc_level);
+        transposition_table.init();
+        Search_result result = tree_search_legal(board, depth, mpc_level, false, board.get_legal(), true, TIME_LIMIT_INF);
+        n_nodes += result.nodes;
+    }
+    transposition_table.reset_importance();
+    return n_nodes;
+}
 
-            // simulated annealing
-            constexpr double start_temp = 0.1; // percent
-            constexpr double end_temp = 0.0001; // percent
-            double temp = start_temp + (end_temp - start_temp) * (tim() - strt) / tl;
-            double prob = exp((min_percentage - percentage) / temp);
-            if (prob > myrandom()) {
-                min_n_nodes = n_nodes;
-                min_percentage = percentage;
-                ++n_updated;
-            } else{
-                move_ordering_param_array[idx] -= delta;
-            }
-            ++n_try;
+void tune_move_ordering(int level) {
+    std::cout << "please input testcase file" << std::endl;
+    std::string file;
+    std::cin >> file;
+    std::ifstream ifs(file);
+    if (ifs.fail()) {
+        std::cerr << "[ERROR] [FATAL] problem file " << file << " not found" << std::endl;
+        return;
+    }
+    std::vector<Board> testcase_arr;
+    std::string line;
+    while (std::getline(ifs, line)) {
+        testcase_arr.emplace_back(get_board(line));
+    }
+    std::cerr << testcase_arr.size() << " testcases loaded" << std::endl;
+    int minute = 10;
+    std::cout << "please input timelimit (minute)" << std::endl;
+    std::cin >> minute;
+    uint64_t tl = 60ULL * 1000ULL * minute; // 10 min
+    uint64_t min_n_nodes = n_nodes_test(level, testcase_arr);
+    double min_percentage = 100.0;
+    uint64_t first_n_nodes = min_n_nodes;
+    std::cerr << "min_n_nodes " << min_n_nodes << std::endl;
+    int n_updated = 0;
+    int n_try = 0;
+    uint64_t strt = tim();
+    while (tim() - strt < tl) {
+        // update parameter randomly
+        int idx = myrandrange(MOVE_ORDERING_PARAM_START, MOVE_ORDERING_PARAM_END + 1); // midgame search
+        int delta = myrandrange(-5, 6);
+        while (delta == 0)
+            delta = myrandrange(-5, 6);
+        if (move_ordering_param_array[idx] + delta < 0)
+            continue;
+        move_ordering_param_array[idx] += delta;
+        uint64_t n_nodes = n_nodes_test(level, testcase_arr);
+        double percentage = 100.0 * n_nodes / first_n_nodes;
 
-            std::cerr << "try " << n_try << " updated " << n_updated << " min_n_nodes " << min_n_nodes << " n_nodes " << n_nodes << " " << min_percentage << "% " << tim() - strt << " ms ";
-            for (int i = 0; i < N_MOVE_ORDERING_PARAM; ++i) {
-                std::cerr << ", " << move_ordering_param_array[i];
-            }
-            std::cerr << std::endl;
+        // simulated annealing
+        constexpr double start_temp = 0.1; // percent
+        constexpr double end_temp = 0.0001; // percent
+        double temp = start_temp + (end_temp - start_temp) * (tim() - strt) / tl;
+        double prob = exp((min_percentage - percentage) / temp);
+        if (prob > myrandom()) {
+            min_n_nodes = n_nodes;
+            min_percentage = percentage;
+            ++n_updated;
+        } else{
+            move_ordering_param_array[idx] -= delta;
         }
-        std::cout << "done " << min_percentage << "% ";
+        ++n_try;
+
+        std::cerr << "try " << n_try << " updated " << n_updated << " min_n_nodes " << min_n_nodes << " n_nodes " << n_nodes << " " << min_percentage << "% " << tim() - strt << " ms ";
         for (int i = 0; i < N_MOVE_ORDERING_PARAM; ++i) {
-            std::cout << ", " << move_ordering_param_array[i];
+            std::cerr << ", " << move_ordering_param_array[i];
         }
-        std::cout << std::endl;
+        std::cerr << std::endl;
     }
+    std::cout << "done " << min_percentage << "% ";
+    for (int i = 0; i < N_MOVE_ORDERING_PARAM; ++i) {
+        std::cout << ", " << move_ordering_param_array[i];
+    }
+    std::cout << std::endl;
+}
 #endif
