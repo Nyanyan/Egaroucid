efb2c345d81f2d2f270b852effdbbfe63901116d
Takuto Yamana <nyanyama3141pi2@yahoo.co.jp>
2024-12-14 22:49:02 +0900

    update citation update script

--- src/tools/release_script/update_citation.py
+++ src/tools/release_script/update_citation.py
@@ -3,12 +3,15 @@ import datetime
 
 if len(sys.argv) < 2:
     print('please input [version] [date]')
+    exit(1)
 version = sys.argv[1]
 
 if len(sys.argv) == 3:
     date = sys.argv[2]
 else:
-    date = datetime.date.today()
+    date = str(datetime.date.today())
+
+print(version, date)
 
 citation_file = './../../../CITATION.cff'
 
