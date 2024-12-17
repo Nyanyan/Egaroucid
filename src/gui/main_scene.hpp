03d9a2c35fe5f5e982775d14828950fd57731906
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2024-12-17 12:34:48 +0900

    update color #387

--- src/gui/main_scene.hpp
+++ src/gui/main_scene.hpp
@@ -1399,15 +1399,14 @@ private:
             int sx = BOARD_SX + ((HW2_M1 - cell) % HW) * BOARD_CELL_SIZE;
             int sy = BOARD_SY + ((HW2_M1 - cell) / HW) * BOARD_CELL_SIZE;
             //Color cell_color = HSV{160.0, 0.76 - 0.24 * ai_status.local_strategy[cell], 0.60 - 0.30 * ai_status.local_strategy[cell]};
-            Color cell_color = HSV{160.0 + 10.0 * ai_status.local_strategy[cell], 0.76 - 0.20 * ai_status.local_strategy[cell], 0.60 - 0.20 * ai_status.local_strategy[cell]};
+            //Color cell_color = HSV{160.0 + 10.0 * ai_status.local_strategy[cell], 0.76 - 0.20 * ai_status.local_strategy[cell], 0.60 - 0.20 * ai_status.local_strategy[cell]};
             // normal green = HSV{160.0, 0.76, 0.60}
-            /*
+            Color cell_color;
             if (ai_status.local_strategy[cell] > 0) { // black
                 cell_color = ColorF{ 52.0 / 255.0, 152.0 / 255.0, 219.0 / 255.0, ai_status.local_strategy[cell] };
             } else { // white
                 cell_color = ColorF{ 241.0 / 255.0, 196.0 / 255.0, 15.0 / 255.0, -ai_status.local_strategy[cell] };
             }
-            */
             Rect{ sx, sy,  BOARD_CELL_SIZE, BOARD_CELL_SIZE}.draw(cell_color);
         }
     }
