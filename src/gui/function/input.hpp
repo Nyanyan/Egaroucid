edeed2723e23b50d49fc9ba95ea949de3fbc45a0
Takuto Yamana <nyanyama3141pi2@yahoo.co.jp>
2024-12-23 09:54:54 +0900

    add ggf parser #410

--- src/gui/function/input.hpp
+++ src/gui/function/input.hpp
@@ -17,61 +17,61 @@ std::vector<History_elem> import_transcript_processing(std::vector<History_elem>
     String transcript_str = Unicode::Widen(transcript).replace(U"\r", U"").replace(U"\n", U"").replace(U" ", U"");
     if (transcript_str.size() % 2 != 0 && transcript_str.size() >= 120) {
         *failed = true;
-    } else {
-        int y, x;
-        uint64_t legal;
-        Flip flip;
-        History_elem history_elem;
-        int player = strt_elem.player;
-        //history_elem.set(h_bd, player, GRAPH_IGNORE_VALUE, -1, -1, -1, "");
-        //n_history.emplace_back(history_elem);
-        bool passed = false;
-        for (int i = 0; i < (int)transcript_str.size(); i += 2) {
-            if (is_pass_like_str(transcript_str.narrow().substr(i, 2)) && passed) {
-                continue;
-            }
-            x = (int)transcript_str[i] - (int)'a';
+        return n_history;
+    }
+    int y, x;
+    uint64_t legal;
+    Flip flip;
+    History_elem history_elem;
+    int player = strt_elem.player;
+    //history_elem.set(h_bd, player, GRAPH_IGNORE_VALUE, -1, -1, -1, "");
+    //n_history.emplace_back(history_elem);
+    bool passed = false;
+    for (int i = 0; i < (int)transcript_str.size(); i += 2) {
+        if (is_pass_like_str(transcript_str.narrow().substr(i, 2)) && passed) {
+            continue;
+        }
+        x = (int)transcript_str[i] - (int)'a';
+        if (x < 0 || HW <= x) {
+            x = (int)transcript_str[i] - (int)'A';
             if (x < 0 || HW <= x) {
-                x = (int)transcript_str[i] - (int)'A';
-                if (x < 0 || HW <= x) {
-                    *failed = true;
-                    break;
-                }
-            }
-            y = (int)transcript_str[i + 1] - (int)'1';
-            if (y < 0 || HW <= y) {
                 *failed = true;
                 break;
             }
-            y = HW_M1 - y;
-            x = HW_M1 - x;
-            legal = h_bd.get_legal();
-            if (1 & (legal >> (y * HW + x))) {
-                calc_flip(&flip, &h_bd, y * HW + x);
-                h_bd.move_board(&flip);
+        }
+        y = (int)transcript_str[i + 1] - (int)'1';
+        if (y < 0 || HW <= y) {
+            *failed = true;
+            break;
+        }
+        y = HW_M1 - y;
+        x = HW_M1 - x;
+        legal = h_bd.get_legal();
+        if (1 & (legal >> (y * HW + x))) {
+            calc_flip(&flip, &h_bd, y * HW + x);
+            h_bd.move_board(&flip);
+            player ^= 1;
+            passed = false;
+            if (h_bd.get_legal() == 0ULL) {
+                h_bd.pass();
                 player ^= 1;
-                passed = false;
+                passed = true;
                 if (h_bd.get_legal() == 0ULL) {
                     h_bd.pass();
                     player ^= 1;
-                    passed = true;
-                    if (h_bd.get_legal() == 0ULL) {
-                        h_bd.pass();
-                        player ^= 1;
-                        if (i != transcript_str.size() - 2) {
-                            *failed = true;
-                            break;
-                        }
+                    if (i != transcript_str.size() - 2) {
+                        *failed = true;
+                        break;
                     }
                 }
-            } else {
-                *failed = true;
-                break;
             }
-            n_history.back().next_policy = y * HW + x;
-            history_elem.set(h_bd, player, GRAPH_IGNORE_VALUE, -1, y * HW + x, -1, "");
-            n_history.emplace_back(history_elem);
+        } else {
+            *failed = true;
+            break;
         }
+        n_history.back().next_policy = y * HW + x;
+        history_elem.set(h_bd, player, GRAPH_IGNORE_VALUE, -1, y * HW + x, -1, "");
+        n_history.emplace_back(history_elem);
     }
     return n_history;
 }
@@ -114,4 +114,51 @@ std::pair<Board, int> import_board_processing(std::string board_str, bool *faile
         }
     }
     return std::make_pair(board, player);
+}
+
+
+
+
+std::vector<History_elem> import_ggf_processing(std::string ggf, bool* failed) {
+    std::vector<History_elem> n_history;
+    String ggf_str = Unicode::Widen(ggf).replace(U"\r", U"").replace(U"\n", U"").replace(U" ", U"");
+    int board_start_idx = ggf_str.indexOf(U"BO[8");
+    if (board_start_idx == std::string::npos) {
+        *failed = true;
+        return n_history;
+    }
+    board_start_idx += 4;
+    if (ggf_str.size() < board_start_idx + 65) {
+        *failed = true;
+        return n_history;
+    }
+    std::string start_board_str = ggf_str.substr(board_start_idx, 65).narrow();
+    std::cerr << "start board " << start_board_str << std::endl;
+    std::pair<Board, player> board_player = import_board_processing(start_board_str, failed);
+    if (*failed) {
+        return n_history;
+    }
+    History_elem start_board;
+    start_board.board = board_player.first;
+    start_board.player = board_player.second;
+    n_history.emplace_back(start_board);
+    std::string transcript;
+    int offset = board_start_idx + 65;
+    int coord_start_idx;
+    while (true) {
+        coord_start_idx = ggf_str.indexOf(U"B[", offset);
+        if (coord_start_idx == std::string::npos) {
+            coord_start_idx = ggf_str.indexOf(U"W[", offset);
+            if (coord_start_idx == std::string::npos) {
+                break;
+            }
+        }
+        coord_start_idx += 2;
+        std::string coord = ggf_str.substr(coord_start_idx, 2).narrow();
+        transcript += coord;
+        std::cerr << coord << std::endl;
+        offset = coord_start_idx + 2;
+    }
+    n_history = import_transcript_processing(n_history, start_board, transcript, failed);
+    return n_history;
 }
