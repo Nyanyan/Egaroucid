55917db13c1a9a85ddfdd95d519bc46bdb2a6156
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-02-15 14:37:44 +0900

    update eval #435

--- src/tools/evaluation/evaluation_definition.hpp
+++ src/tools/evaluation/evaluation_definition.hpp
@@ -14,5 +14,5 @@
 //#include "evaluation_definition_20250214_1_cell_weight_60phase.hpp"
 
 
-#include "evaluation_definition_20240301_4_move_ordering_end_nws.hpp"
-//#include "evaluation_definition_20241125_1_7_5.hpp"
+//#include "evaluation_definition_20240301_4_move_ordering_end_nws.hpp"
+#include "evaluation_definition_20241125_1_7_5.hpp"
