5e18a990a5aaaecea07ae870c3bc5f114d42e7ac
Takuto Yamana <nyanyama3141pi2@yahoo.co.jp>
2025-02-18 02:28:06 +0900

    add script #437

---
+++ bin/tools/solverandom.py
@@ -0,0 +1,36 @@
+import subprocess
+from tqdm import trange
+import sys
+import os
+
+n_random_moves = int(sys.argv[1])
+IDX_START = int(sys.argv[2])
+IDX_END = int(sys.argv[3])
+
+LEVEL = 13
+N_BOARDS_PER_FILE = 10000
+N_THREAD = 15
+
+def fill0(n, r):
+    res = str(n)
+    return '0' * (r - len(res)) + res
+
+exe = './../Egaroucid_for_Console_clang.exe'
+
+#cmd = exe + ' -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD) + ' -selfplay ' + str(N_BOARDS_PER_FILE) + ' ' + str(n_random_moves)
+cmd = exe + ' -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD) + ' -solverandom ' + str(N_BOARDS_PER_FILE) + ' ' + str(n_random_moves)
+print(cmd)
+
+for idx in range(IDX_START, IDX_END + 1):
+    print(fill0(idx, 7))
+    dr = './../transcript/' + str(n_random_moves)
+    try:
+        os.mkdir(dr)
+    except:
+        pass
+    with open(dr + '/' + fill0(idx, 7) + '.txt', 'w') as f:
+        egaroucid = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        for i in trange(N_BOARDS_PER_FILE):
+            line = egaroucid.stdout.readline().decode().replace('\r', '').replace('\n', '') + '\n'
+            f.write(line)
+        egaroucid.kill()
