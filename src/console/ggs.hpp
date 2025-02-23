e71fff0867af98c8b9ae94126222b7a2b551989a
Takuto Yamana <nyanyama3141pi2@yahoo.co.jp>
2025-02-23 14:41:41 +0900

    writing ggs #439

--- src/console/ggs.hpp
+++ src/console/ggs.hpp
@@ -10,6 +10,12 @@
 #pragma once
 #include "./../engine/engine_all.hpp"
 #include "option.hpp"
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#define GGS_URL "skatgame.net"
+#define GGS_PORT 5000
 
 void ggs_client(Options *options) {
 
