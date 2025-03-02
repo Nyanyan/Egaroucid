fafe987ccc16bc53b4eb07a7cf23521671c2e552
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-03-02 23:21:46 +0900

    update script

--- bin/tools/solverandom.py
+++ bin/tools/solverandom.py
@@ -7,15 +7,16 @@ n_random_moves = int(sys.argv[1])
 IDX_START = int(sys.argv[2])
 IDX_END = int(sys.argv[3])
 
-LEVEL = 13
+LEVEL = int(sys.argv[4])
 N_BOARDS_PER_FILE = 10000
-N_THREAD = 15
+N_THREAD = 31
 
 def fill0(n, r):
     res = str(n)
     return '0' * (r - len(res)) + res
 
 exe = './../Egaroucid_for_Console_clang.exe'
+#exe = './../versions/Egaroucid_for_Console_7_5_1_Windows_SIMD/Egaroucid_for_Console_7_5_1_SIMD.exe'
 
 #cmd = exe + ' -nobook -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD) + ' -selfplay ' + str(N_BOARDS_PER_FILE) + ' ' + str(n_random_moves)
 cmd = exe + ' -l ' + str(LEVEL) + ' -thread ' + str(N_THREAD) + ' -solverandom ' + str(N_BOARDS_PER_FILE) + ' ' + str(n_random_moves)
