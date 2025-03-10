edd48ab12b130a86d0b47594ff96e65dcbd1ffc4
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-03-10 13:08:44 +0900

    update script #438

--- bin/battle.py
+++ bin/battle.py
@@ -11,7 +11,8 @@ N_SET_GAMES = 25000
 # name, cmd
 player_info = [
     #['beta', 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console.exe -quiet -nobook -t 32'],
-    ['latest',  'Egaroucid_for_Console.exe -quiet -nobook -t 32'],
+    #['latest',  'Egaroucid_for_Console.exe -quiet -nobook -t 32'],
+    ['clang',  'Egaroucid_for_Console_clang.exe -quiet -nobook -t 32'],
     ['7.5.0', 'versions/Egaroucid_for_Console_7_5_0_Windows_SIMD/Egaroucid_for_Console_7_5_0_SIMD.exe -quiet -nobook -t 32'],
     ['7.4.0', 'versions/Egaroucid_for_Console_7_4_0_Windows_x64_SIMD/Egaroucid_for_Console_7_4_0_x64_SIMD.exe -quiet -nobook -t 32'],
     ['7.3.0', 'versions/Egaroucid_for_Console_7_3_0_Windows_x64_SIMD/Egaroucid_for_Console_7_3_0_x64_SIMD.exe -quiet -nobook -t 32'],
