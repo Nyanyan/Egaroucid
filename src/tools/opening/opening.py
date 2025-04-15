24754d573d51d069970f534720c32db1bd39358c
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-04-15 11:19:54 +0900

    update opening.py #452

--- src/tools/opening/opening.py
+++ src/tools/opening/opening.py
@@ -6,28 +6,42 @@ with open('data/openings_japanese.txt', 'r', encoding='utf-8') as f:
         if len(datum.replace(' ', '').replace('　', '')):
             if datum.replace(' ', '').replace('　', '')[:2] == '//':
                 continue
-            n_spaces = 0
-            for i in range(100):
-                if datum[i] != ' ' and datum[i] != '　':
-                    n_spaces = i
-                    break
             datum = datum.replace(' ', '').replace('　', '')
             name, record = datum.split('=')
-            print(n_spaces, name, record)
-            data.append([n_spaces, name, record])
+            record = record.lower()
+            print(name, record)
+            data.append([name, record, []])
 
-data.sort(key=lambda x: x[2])
+data.sort(key=lambda x: len(x[1]))
+
+for i in range(len(data)):
+    name, record, _ = data[i]
+    for cname, crecord, _ in data:
+        if name != cname and record == crecord[:len(record)]:
+            data[i][2].append(cname)
+
+for i in range(len(data)):
+    name, record, children = data[i]
+    n_children = set(children)
+    for child in children:
+        for cname, crecord, cchildren in data:
+            if child == cname:
+                n_children -= set(cchildren)
+    n_children = list(n_children)
+    data[i][2] = n_children
+    if record == 'F5F6':
+        print(name, record, children, n_children)
+    #print(name, record, n_children)
+
 
 joseki = {}
 joseki_many = {}
 
-for n_spaces, name, record in data:
+for name, record, children in data:
     o = othello()
     o.check_legal()
     for i in range(0, len(record), 2):
-        x = ord(record[i]) - ord('A')
-        if x >= hw:
-            x = ord(record[i]) - ord('a')
+        x = ord(record[i]) - ord('a')
         y = int(record[i + 1]) - 1
         o.move(y, x)
         if not o.check_legal():
@@ -42,15 +56,23 @@ for n_spaces, name, record in data:
                     s += '1'
                 else:
                     s += '.'
-        flag = not (s in joseki_many)
-        if (not flag):
-            if joseki_many[s][0] == n_spaces:
-                joseki_many[s].append(name)
+        if not s in joseki_many:
+            joseki_many[s] = [name]
         else:
-            joseki_many[s] = [n_spaces, name]
-    #if not (s in joseki.keys()):
-    #    joseki[s] = name
+            joseki_many[s].append(name)
     joseki[s] = name
+
+for board in joseki_many.keys():
+    joseki_children = set(joseki_many[board])
+    for child in joseki_many[board]:
+        for name, record, children in data:
+            if name == child:
+                joseki_children -= set(children)
+    #if board == '...........................10......000..........................':
+    #    print(board, joseki_many[board], joseki_children)
+    joseki_many[board] = list(joseki_children)
+
+print(joseki_many['...........................10......000..........................'])
 
 print(len(joseki))
 print(len(joseki_many))
@@ -59,4 +81,4 @@ with open('output/openings.txt', 'w', encoding='utf-8') as f:
         f.write(board + ' ' + joseki[board] + '\n')
 with open('output/openings_fork.txt', 'w', encoding='utf-8') as f:
     for board in joseki_many.keys():
-        f.write(board + ' ' + '|'.join(joseki_many[board][1:]) + '\n')
+        f.write(board + ' ' + '|'.join(joseki_many[board]) + '\n')
