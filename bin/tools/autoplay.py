f5e261fda3876594640fb74650d0335975c54de7
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-04-17 11:32:57 +0900

    update autoplay #452

--- bin/tools/autoplay.py
+++ bin/tools/autoplay.py
@@ -7,9 +7,9 @@ n_random_moves = int(sys.argv[1])
 IDX_START = int(sys.argv[2])
 IDX_END = int(sys.argv[3])
 
-LEVEL = 11
+LEVEL = 30
 N_GAMES_PER_FILE = 10000
-N_THREAD = 15
+N_THREAD = 31
 
 def fill0(n, r):
     res = str(n)
