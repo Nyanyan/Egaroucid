18bce35ff93dd184e8ec8dc608481661b67c9a39
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-06-04 18:10:59 +0900

    update battle script

--- bin/battle.py
+++ bin/battle.py
@@ -17,7 +17,7 @@ shuffle(openings)
 # name, cmd
 player_info = [
     #['7.5-2', 'versions/Egaroucid_for_Console_7_5_0_Windows_SIMD/Egaroucid_for_Console_7_5_0_SIMD.exe -quiet -nobook'],
-    ['beta', 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console.exe -quiet -nobook'],
+    #['beta', 'versions/Egaroucid_for_Console_beta/Egaroucid_for_Console.exe -quiet -nobook'],
     ['latest',  'Egaroucid_for_Console.exe -quiet -nobook'],
     #['clang',  'Egaroucid_for_Console_clang.exe -quiet -nobook'],
     ['7.6.0', 'versions/Egaroucid_for_Console_7_6_0_Windows_SIMD/Egaroucid_for_Console_7_6_0_SIMD.exe -quiet -nobook'],
