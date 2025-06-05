076ca89cd41458af9eff78d7c3fa3168f2a901db
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-06-05 18:23:21 +0900

    updte presearch #469

--- src/engine/ai.hpp
+++ src/engine/ai.hpp
@@ -730,8 +730,8 @@ Search_result ai_time_limit(Board board, bool use_book, int book_acc_level, bool
     if (time_limit > 10000ULL && n_empties >= 35) { // additional search
         bool need_request_more_time = false;
         bool get_values_searching = true;
-        uint64_t get_values_tl = 2000ULL;
-        uint64_t until_align_levels_tl = 4000ULL;
+        uint64_t get_values_tl = 3000ULL;
+        uint64_t until_align_levels_tl = 5000ULL;
         uint64_t min_ai_common_tl = 3000ULL;
         if (show_log) {
             std::cerr << "getting values tl " << get_values_tl << std::endl;
