e25067f85ef0201902ee4100215a62060b5af500
Takuto Yamana <nyanyama3141pi2@yahoo.co.jp>
2025-12-17 21:38:53 +0900

    refactoring #494

--- src/engine/endsearch_last_generic.hpp
+++ src/engine/endsearch_last_generic.hpp
@@ -12,7 +12,8 @@
 #include "search.hpp"
 #include "endsearch_common.hpp"
 
-constexpr uint_fast8_t d7_n_right_zeros_arr[HW2] = {
+// max(0, x + y - 7)
+constexpr uint_fast8_t d7_n_right_zeros[HW2] = {
     0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 1,
     0, 0, 0, 0, 0, 0, 1, 2,
@@ -23,7 +24,8 @@ constexpr uint_fast8_t d7_n_right_zeros_arr[HW2] = {
     0, 1, 2, 3, 4, 5, 6, 7
 };
 
-constexpr uint_fast8_t d7_n_left_zeros_arr[HW2] = {
+// max(0, -(x + y - 7))
+constexpr uint_fast8_t d7_n_left_zeros[HW2] = {
     7, 6, 5, 4, 3, 2, 1, 0,
     6, 5, 4, 3, 2, 1, 0, 0,
     5, 4, 3, 2, 1, 0, 0, 0,
@@ -34,7 +36,8 @@ constexpr uint_fast8_t d7_n_left_zeros_arr[HW2] = {
     0, 0, 0, 0, 0, 0, 0, 0
 };
 
-constexpr uint_fast8_t d9_n_right_zeros_arr[HW2] = {
+// max(0, x - y)
+constexpr uint_fast8_t d9_n_right_zeros[HW2] = {
     0, 1, 2, 3, 4, 5, 6, 7,
     0, 0, 1, 2, 3, 4, 5, 6,
     0, 0, 0, 1, 2, 3, 4, 5,
@@ -45,7 +48,8 @@ constexpr uint_fast8_t d9_n_right_zeros_arr[HW2] = {
     0, 0, 0, 0, 0, 0, 0, 0
 };
 
-constexpr uint_fast8_t d9_n_left_zeros_arr[HW2] = {
+// max(0, -(x - y))
+constexpr uint_fast8_t d9_n_left_zeros[HW2] = {
     0, 0, 0, 0, 0, 0, 0, 0,
     1, 0, 0, 0, 0, 0, 0, 0,
     2, 1, 0, 0, 0, 0, 0, 0,
@@ -116,14 +120,10 @@ inline int last1(Search *search, uint64_t player, int alpha, uint_fast8_t p0) {
                 score = score2;
             if (score > alpha) {
 #if LAST_FLIP_PASS_OPT
-                // int d7_n_right_zeros = y + x - 7;
-                // int d9_n_right_zeros = x - y;
                 n_flip = 
                     (n_flip_both >> 8) + // hv (calculated)
-                    // N_LAST_FLIP[((0xff << std::max(0, d7_n_right_zeros)) & (0xff >> std::max(0, -d7_n_right_zeros))) ^ d7][x] + // d7
-                    // N_LAST_FLIP[((0xff << std::max(0, d9_n_right_zeros)) & (0xff >> std::max(0, -d9_n_right_zeros))) ^ d9][x]; // d9
-                    N_LAST_FLIP[((0xff << d7_n_right_zeros_arr[p0]) & (0xff >> d7_n_left_zeros_arr[p0])) ^ d7][x] + // d7
-                    N_LAST_FLIP[((0xff << d9_n_right_zeros_arr[p0]) & (0xff >> d9_n_left_zeros_arr[p0])) ^ d9][x]; // d9
+                    N_LAST_FLIP[((0xff << d7_n_right_zeros[p0]) & (0xff >> d7_n_left_zeros[p0])) ^ d7][x] + // d7
+                    N_LAST_FLIP[((0xff << d9_n_right_zeros[p0]) & (0xff >> d9_n_left_zeros[p0])) ^ d9][x]; // d9
 #else
                 n_flip = count_last_flip(~player, p0);
 #endif
