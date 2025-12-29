5e7504c5161fefd65ca88d1ec9fb4555851090cb
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-12-29 19:39:47 +0900

    update release script #516

--- src/tools/release_script/generate_release.py
+++ src/tools/release_script/generate_release.py
@@ -33,6 +33,7 @@ def copy_common_files(correct_task_type, dst_dir_root):
         common_file_in_dir = COMMON_FILES_IN_DIR + '/' + filename
         common_file_dst_dir = dst_dir_root + '/' + dst_dir
         if os.path.isfile(common_file_in_dir):
+            os.makedirs(common_file_dst_dir, exist_ok=True) # ensure parent directory exists
             shutil.copy2(common_file_in_dir, common_file_dst_dir) # copy common special files
         else:
             shutil.copytree(common_file_in_dir, common_file_dst_dir, dirs_exist_ok=True) # copy common special files
@@ -80,10 +81,10 @@ for installer_exe in installer_exes:
     print(installer_exe)
     shutil.copy2(installer_exe, INSTALLER_DST_INSTALLER_DIR) # copy main executable
 shutil.copy2(INSTALLER_IN_SETUP_FILE, INSTALLER_DST_INSTALLER_DIR) # copy setup file
-with open(INSTALLER_DST_INSTALLER_DIR + '/egaroucid_setup.iss', 'r', encoding='cp932') as f:
+with open(INSTALLER_DST_INSTALLER_DIR + '/egaroucid_setup.iss', 'r', encoding='utf-8') as f:
     installer_setup = f.read()
 installer_setup = installer_setup.replace('REPLACE_VERSION_DOT', VERSION_DOT).replace('REPLACE_VERSION_UNDERBAR', VERSION_UNDERBAR) # replace version information
-with open(INSTALLER_DST_INSTALLER_DIR + '/egaroucid_setup.iss', 'w', encoding='cp932') as f:
+with open(INSTALLER_DST_INSTALLER_DIR + '/egaroucid_setup.iss', 'w', encoding='utf-8') as f:
     f.write(installer_setup) # rewrite setup file
 shutil.copytree(INSTALLER_IN_FILES_DIR, INSTALLER_DST_FILES_DIR, dirs_exist_ok=True) # copy files
 copy_common_files('installer', INSTALLER_DST_FILES_DIR) # copy common special files
