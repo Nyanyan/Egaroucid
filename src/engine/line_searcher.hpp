d3b36a0304b4d84d00dd4bfc2935542132940be0
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2026-03-10 12:05:45 +0900

    move line searcher

--- src/engine/line_searcher.hpp
+++ src/engine/line_searcher.hpp
@@ -1,49 +1,15 @@
-﻿/*
+/*
 	Egaroucid Project
 
-	@file Egaroucid_for_Console.cpp
-		Main file for Console application
+	@file line_searcher.cpp
+		Search specific lines
 	@date 2021-2026
 	@author Takuto Yamana
 	@license GPL-3.0-or-later
 */
-#include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <unordered_map>
-#include "engine/engine_all.hpp"
-#include "console/console_all.hpp"
 
-
-void init_console(Options options, std::string binary_path) {
-    int thread_size = std::max(0, options.n_threads - 1);
-    thread_pool.resize(thread_size);
-    if (options.show_log)
-        std::cerr << "thread size = " << thread_size + 1 << std::endl;
-    bit_init();
-    mobility_init();
-    flip_init();
-    last_flip_init();
-    endsearch_init();
-    #if USE_MPC_PRE_CALCULATION
-        mpc_init();
-    #endif
-    move_ordering_init();
-    #if USE_CHANGEABLE_HASH_LEVEL
-        hash_resize(DEFAULT_HASH_LEVEL, options.hash_level, options.binary_path, options.show_log);
-    #else
-        hash_tt_init(options.binary_path, options.show_log);
-    #endif
-    stability_init();
-    std::string mo_end_file = binary_path + "resources/eval_move_ordering_end.egev"; // filename fixed
-    //std::string mo_mid_file = binary_path + "resources/eval_move_ordering_mid.egev"; // filename fixed
-    if (!evaluate_init(options.eval_file, mo_end_file, options.show_log))
-        std::exit(0);
-    if (!options.nobook)
-        book_init(options.book_file, options.show_log);
-    if (options.show_log)
-        std::cerr << "initialized" << std::endl;
-}
+#pragma once
+#include "ai.hpp"
 
 void search_lines(Board &board, int player, int depth, int score, int black_score_min, int black_score_max, int black_max_loss, int white_max_loss, std::vector<int> &line, int last_move_player, std::vector<int> &last_move_cells, std::unordered_map<Board, std::vector<std::pair<int, int>>, Book_hash> &ok_board_memo, int level = 21) {
     if (depth <= 0) {
@@ -157,48 +123,8 @@ void search_lines(Board &board, int player, int depth, int score, int black_scor
     }
 }
 
-int main(int argc, char* argv[]) {
-    State state;
-    std::string binary_path = get_binary_path();
-    std::vector<Commandline_option> commandline_options = get_commandline_options(argc, argv);
-    Options options = get_options(commandline_options, binary_path);
-    std::ofstream ofs;
-    if (options.log_to_file) {
-        ofs.open(options.log_file);
-        if (!ofs) {
-            std::cout << "[ERROR] can't open log file " << options.log_file << std::endl;
-            std::exit(1);
-        }
-        std::cerr.rdbuf(ofs.rdbuf());
-    }
-    print_special_commandline_options(commandline_options);
-    init_console(options, binary_path);
-    execute_special_tasks(options); // tuning etc.
-    execute_special_commandline_tasks(commandline_options, &options, &state); // solve problems etc.
-    Board_info board;
-    init_board(&board, &options, &state);
 
-    // while (true) {
-    //     Board b;
-    //     b.player = myrand_ull();
-    //     b.opponent = ~b.player;
-    //     uint_fast8_t empty_cell = myrand_uint() & 63;
-    //     b.player &= ~(1ULL << empty_cell);
-    //     b.opponent &= ~(1ULL << empty_cell);
-    //     Flip flip;
-    //     calc_flip(&flip, &b, empty_cell);
-    //     int n_flipped_correct = pop_count_ull(flip.flip);
-    //     int n_flipped_last = count_last_flip(b.player, empty_cell);
-    //     std::cerr << n_flipped_correct << " " << n_flipped_last << std::endl;
-    //     b.print();
-    //     if (n_flipped_correct != n_flipped_last) {
-    //         std::cerr << "ERROR" << std::endl;
-    //         std::cerr << n_flipped_correct << " " << n_flipped_last << std::endl;
-    //         b.print();
-    //     }
-    // }
-
-    
+void search_lines_demo() {
     // std::string initial_line = "f5d6c3d3c4f4f6"; // stephenson
     std::string initial_line = "f5f6e6d6e7"; // tobidashi
     // std::string initial_line = "f5d6c3d3c4f4f6g5e3f3g6e2h5c5g4g3f2e1"; // 1993
@@ -273,34 +199,4 @@ int main(int argc, char* argv[]) {
     }
     std::cout << "done!" << std::endl;
     std::cerr << "done!" << std::endl;
-    return 0;
-
-
-    while (true) {
-        if (options.gtp) {
-            if (options.ponder) {
-                state.ponder_searching = true;
-                state.ponder_future = std::async(std::launch::async, ai_ponder, board.board, options.show_log, THREAD_ID_NONE, &state.ponder_searching);
-            }
-            gtp_check_command(&board, &state, &options);
-        }else {
-            if (!options.quiet && !options.noboard) {
-                print_board_info(&board, &state, &options);
-                std::cout << std::endl;
-                //std::cerr << "val " << mid_evaluate(&board.board) << std::endl;
-            }
-            if (!execute_special_tasks_loop(&board, &state, &options)) {
-                if (options.ponder) {
-                    if (board.board.n_discs() > 4) {
-                        state.ponder_searching = true;
-                        state.ponder_future = std::async(std::launch::async, ai_ponder, board.board, options.show_log, THREAD_ID_NONE, &state.ponder_searching);
-                    } //else {
-                    //    transposition_table.reset_importance();
-                    //}
-                }
-                check_command(&board, &state, &options);
-            }
-        }
-    }
-    return 0;
 }
