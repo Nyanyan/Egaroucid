fce85a61d7d201199588f1d772b464786455057c
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2026-03-31 23:02:09 +0900

    Fix midtest.py CLI arguments and paths

--- bin/midtest.py
+++ bin/midtest.py
@@ -1,6 +1,7 @@
+import os
 import subprocess
 import sys
-import os
+
 
 def fill0(n, r):
     n = str(n)
@@ -9,42 +10,67 @@ def fill0(n, r):
         n = '0' + n
     return n
 
-exe = 'Egaroucid_for_Console.exe'
-# exe = 'Egaroucid_for_Console_20250717_before.exe'
-# exe = 'versions/Egaroucid_for_Console_7_5_1_Windows_SIMD/Egaroucid_for_Console_7_5_1_SIMD.exe'
-# exe = 'versions/Egaroucid_for_Console_7_6_0_Windows_SIMD/Egaroucid_for_Console_7_6_0_SIMD.exe'
-
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-if not os.path.isabs(exe):
-    exe = os.path.join(script_dir, exe)
-
-cmd_version = exe + ' -v'
-# print(cmd_version)
-version = subprocess.run((cmd_version).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE).stdout.decode()
 
 def strip_newlines(s):
     while s.endswith('\n') or s.endswith('\r'):
         s = s[:-1]
     return s
 
+
+level = 23
+n_threads = 32
+hash_level = 25
+exe = 'Egaroucid_for_Console.exe'
+problem_file = 'problem/midgame_test.txt'
+
+try:
+    if len(sys.argv) >= 2:
+        level = int(sys.argv[1])
+    if len(sys.argv) >= 3:
+        n_threads = int(sys.argv[2])
+    if len(sys.argv) >= 4:
+        hash_level = int(sys.argv[3])
+    if len(sys.argv) >= 5:
+        exe = sys.argv[4]
+    if len(sys.argv) >= 6:
+        problem_file = sys.argv[5]
+except Exception:
+    print('usage: python midtest.py [level=23] [n_threads=32] [hash_level=25] [exe=Egaroucid_for_Console.exe] [problem=problem/midgame_test.txt]')
+    exit()
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+if not os.path.isabs(exe):
+    exe = os.path.join(script_dir, exe)
+if not os.path.isabs(problem_file):
+    problem_file = os.path.join(script_dir, problem_file)
+
+cmd_version = [exe, '-v']
+version = subprocess.run(cmd_version, stdin=subprocess.PIPE, stdout=subprocess.PIPE).stdout.decode()
 version = strip_newlines(version)
 print(version)
 
-cmd = exe + ' -l 23 -nobook -thread 32 -solve ' + os.path.join(script_dir, 'problem/midgame_test.txt')
+cmd = [
+    exe,
+    '-l',
+    str(level),
+    '-nobook',
+    '-thread',
+    str(n_threads),
+    '-hash',
+    str(hash_level),
+    '-solve',
+    problem_file,
+]
 
-print(cmd.replace(script_dir, 'script_dir'))
-egaroucid = subprocess.Popen((cmd).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+print(' '.join(cmd).replace(script_dir, 'script_dir'))
+egaroucid = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
-res = ''
-line = egaroucid.stdout.readline().decode().replace('\n', '').replace('\r', '')
+line = strip_newlines(egaroucid.stdout.readline().decode())
 print('#   ' + line)
 for i in range(32):
-    line = egaroucid.stdout.readline().decode().replace('\n', '').replace('\r', '')
-    policy = line.split()[3][:-1]
+    line = strip_newlines(egaroucid.stdout.readline().decode())
     line = '#' + fill0(i, 2) + ' ' + line
     print(line)
-    res += line + '\n'
-line = egaroucid.stdout.readline().decode().replace('\n', '').replace('\r', '')
+line = strip_newlines(egaroucid.stdout.readline().decode())
 print(line)
 egaroucid.kill()
