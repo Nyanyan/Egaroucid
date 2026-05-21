5abbdc2e80ea249e3ea0085a253a0e82d1ffa5ef
Benson Muite <bkmgit@users.noreply.github.com>
2026-05-21 20:34:11 +0300

    Use conditional build for 32 bit systems
    Allow for integer conversions.

--- src/engine/hash.hpp
+++ src/engine/hash.hpp
@@ -15,7 +15,11 @@
 /*
     @brief definition of maximum hash level
 */
-constexpr int N_HASH_LEVEL = 33; // level 32 is maximim because hash is 32 bit
+#ifdef HAS_32_BIT_OS
+constexpr int N_HASH_LEVEL = 32; // level 32 is maximim because hash is 32 bit
+#else
+constexpr int N_HASH_LEVEL = 33;
+#endif
 constexpr int DEFAULT_HASH_LEVEL = 25;
 
 #if USE_CHANGEABLE_HASH_LEVEL
@@ -35,6 +39,43 @@ uint32_t hash_rand_opponent[4][65536];
 
     2 ^ hash_level will be tha size of hash
 */
+
+#ifdef HAS_32_BIT_OS
+constexpr size_t hash_sizes[N_HASH_LEVEL] = {
+    1,
+    2,
+    4,
+    8,
+    16,
+    32,
+    64,
+    128,
+    256,
+    512,
+    1024,
+    2048,
+    4096,
+    8192,
+    16384,
+    32768,
+    65536,
+    131072,
+    262144,
+    524288,
+    1048576,
+    2097152,
+    4194304,
+    8388608,
+    16777216,
+    33554432,
+    67108864,
+    134217728,
+    268435456,
+    536870912,
+    1073741824,
+    2147483648
+};
+#else
 constexpr size_t hash_sizes[N_HASH_LEVEL] = {
     1,
     2,
@@ -70,6 +111,7 @@ constexpr size_t hash_sizes[N_HASH_LEVEL] = {
     2147483648,
     4294967296
 };
+#endif
 
 /*
     @brief initialize hash array randomly
