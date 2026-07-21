8a98e5ae64d5e53124b6b2c136022911f15736ab
Takuto Yamana <nyanyama3141pi2@yahoo.co.jp>
2026-07-21 12:09:08 +0900

    update gui version

--- src/gui/function/const/info.hpp
+++ src/gui/function/const/info.hpp
@@ -13,7 +13,7 @@
 #include "./../../../engine/setting.hpp"
 #include "settings.hpp"
 
-#define EGAROUCID_GUI_VERSION "2 20260718_Issue627"
+#define EGAROUCID_GUI_VERSION "3 beta"
 #if USE_BETA_VERSION
     #if GUI_PORTABLE_MODE
         const String EGAROUCID_VERSION = Unicode::Widen(EGAROUCID_ENGINE_VERSION + (std::string)"." + EGAROUCID_GUI_VERSION + (std::string)" " + EGAROUCID_ENGINE_ENV_VERSION + (std::string)" Portable" + (std::string)" beta");
