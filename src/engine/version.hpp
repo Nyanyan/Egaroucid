f01bf865a52412cc294939064f61ee5985ffa845
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2026-09-25 12:31:50 +0900

    update version

--- src/engine/version.hpp
+++ src/engine/version.hpp
@@ -14,8 +14,8 @@
 /*
     @brief version settings
 */
-#define EGAROUCID_ENGINE_VERSION "7.8"
-#define USE_BETA_VERSION false
+#define EGAROUCID_ENGINE_VERSION "7.9"
+#define USE_BETA_VERSION true
 
 
 // OS
